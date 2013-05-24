7a484286829e1a117be7d6608254efe65e27009d
Will Norris <will@willnorris.com>
2013-05-24 13:56:48 -0700

    gofmt

--- users_test.go
+++ users_test.go
@@ -7,111 +7,111 @@
 package github
 
 import (
-  "encoding/json"
-  "fmt"
-  "net/http"
-  "reflect"
-  "testing"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
 )
 
 func TestUsersService_Get_authenticatedUser(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "GET" {
-      t.Errorf("Request method = %v, want %v", r.Method, "GET")
-    }
-    fmt.Fprint(w, `{"id":1}`)
-  })
+	mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			t.Errorf("Request method = %v, want %v", r.Method, "GET")
+		}
+		fmt.Fprint(w, `{"id":1}`)
+	})
 
-  user, err := client.Users.Get("")
-  if err != nil {
-    t.Errorf("Users.Get returned error: %v", err)
-  }
+	user, err := client.Users.Get("")
+	if err != nil {
+		t.Errorf("Users.Get returned error: %v", err)
+	}
 
-  want := &User{ID: 1}
-  if !reflect.DeepEqual(user, want) {
-    t.Errorf("Users.Get returned %+v, want %+v", user, want)
-  }
+	want := &User{ID: 1}
+	if !reflect.DeepEqual(user, want) {
+		t.Errorf("Users.Get returned %+v, want %+v", user, want)
+	}
 }
 
 func TestUsersService_Get_specifiedUser(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  mux.HandleFunc("/users/u", func(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "GET" {
-      t.Errorf("Request method = %v, want %v", r.Method, "GET")
-    }
-    fmt.Fprint(w, `{"id":1}`)
-  })
+	mux.HandleFunc("/users/u", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			t.Errorf("Request method = %v, want %v", r.Method, "GET")
+		}
+		fmt.Fprint(w, `{"id":1}`)
+	})
 
-  user, err := client.Users.Get("u")
-  if err != nil {
-    t.Errorf("Users.Get returned error: %v", err)
-  }
+	user, err := client.Users.Get("u")
+	if err != nil {
+		t.Errorf("Users.Get returned error: %v", err)
+	}
 
-  want := &User{ID: 1}
-  if !reflect.DeepEqual(user, want) {
-    t.Errorf("Users.Get returned %+v, want %+v", user, want)
-  }
+	want := &User{ID: 1}
+	if !reflect.DeepEqual(user, want) {
+		t.Errorf("Users.Get returned %+v, want %+v", user, want)
+	}
 }
 
 func TestUsersService_Edit(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  input := &User{Name: "n"}
+	input := &User{Name: "n"}
 
-  mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-    v := new(User)
-    json.NewDecoder(r.Body).Decode(v)
+	mux.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		v := new(User)
+		json.NewDecoder(r.Body).Decode(v)
 
-    if r.Method != "PATCH" {
-      t.Errorf("Request method = %v, want %v", r.Method, "GET")
-    }
-    if !reflect.DeepEqual(v, input) {
-      t.Errorf("Request body = %+v, want %+v", v, input)
-    }
+		if r.Method != "PATCH" {
+			t.Errorf("Request method = %v, want %v", r.Method, "GET")
+		}
+		if !reflect.DeepEqual(v, input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
 
-    fmt.Fprint(w, `{"id":1}`)
-  })
+		fmt.Fprint(w, `{"id":1}`)
+	})
 
-  user, err := client.Users.Edit(input)
-  if err != nil {
-    t.Errorf("Users.Edit returned error: %v", err)
-  }
+	user, err := client.Users.Edit(input)
+	if err != nil {
+		t.Errorf("Users.Edit returned error: %v", err)
+	}
 
-  want := &User{ID: 1}
-  if !reflect.DeepEqual(user, want) {
-    t.Errorf("Users.Edit returned %+v, want %+v", user, want)
-  }
+	want := &User{ID: 1}
+	if !reflect.DeepEqual(user, want) {
+		t.Errorf("Users.Edit returned %+v, want %+v", user, want)
+	}
 }
 
 func TestUsersService_List(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-    var v string
-    if r.Method != "GET" {
-      t.Errorf("Request method = %v, want %v", r.Method, "GET")
-    }
-    if v = r.FormValue("since"); v != "1" {
-      t.Errorf("Request since parameter = %v, want %v", v, "1")
-    }
-    fmt.Fprint(w, `[{"id":2}]`)
-  })
+	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		var v string
+		if r.Method != "GET" {
+			t.Errorf("Request method = %v, want %v", r.Method, "GET")
+		}
+		if v = r.FormValue("since"); v != "1" {
+			t.Errorf("Request since parameter = %v, want %v", v, "1")
+		}
+		fmt.Fprint(w, `[{"id":2}]`)
+	})
 
-  opt := &UserListOptions{1}
-  users, err := client.Users.List(opt)
-  if err != nil {
-    t.Errorf("Users.Get returned error: %v", err)
-  }
+	opt := &UserListOptions{1}
+	users, err := client.Users.List(opt)
+	if err != nil {
+		t.Errorf("Users.Get returned error: %v", err)
+	}
 
-  want := []User{User{ID: 2}}
-  if !reflect.DeepEqual(users, want) {
-    t.Errorf("Users.List returned %+v, want %+v", users, want)
-  }
+	want := []User{User{ID: 2}}
+	if !reflect.DeepEqual(users, want) {
+		t.Errorf("Users.List returned %+v, want %+v", users, want)
+	}
 }
