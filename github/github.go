1213e5ab3e1e1470469f14748b5b9a14c6cd3505
jlaportebot <jlaportebot@users.noreply.github.com>
2013-06-27 10:08:41 -0700

    Update boilerplate to reflect shared copyright
    Since the library has received external contributions, Google is no
    longer the sole copyright holder.  Update the boilerplate at the top of
    each file to reflect that, and add AUTHORS and CONTRIBUTORS files to
    track this information.

--- github/github.go
+++ github/github.go
@@ -1,8 +1,7 @@
-// Copyright 2013 Google. All rights reserved.
+// Copyright 2013 The go-github AUTHORS. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file or at
-// https://developers.google.com/open-source/licenses/bsd
+// license that can be found in the LICENSE file.
 
 /*
 Package github provides a client for using the GitHub API.
