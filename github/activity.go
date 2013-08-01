8ee61256c3621f78c14ba3f2aca977eff1a321e7
Billy Lynch <wlynch92@gmail.com>
2013-08-01 11:08:36 -0400

    Restructured Activity Service and Event files.

--- github/activity.go
+++ github/activity.go
@@ -5,60 +5,10 @@
 
 package github
 
-import (
-	"fmt"
-	"net/url"
-	"strconv"
-)
-
 // ActivityService handles communication with the activity related
 // methods of the GitHub API.
 //
 // GitHub API docs: http://developer.github.com/v3/users/
 type ActivityService struct {
-	client *Client
-}
-
-// ActivityListStarredOptions specifies the optional parameters to the
-// ActivityService.ListStarred method.
-type ActivityListStarredOptions struct {
-	// How to sort the repository list.  Possible values are: created, updated,
-	// pushed, full_name.  Default is "full_name".
-	Sort string
-
-	// Direction in which to sort repositories.  Possible values are: asc, desc.
-	// Default is "asc" when sort is "full_name", otherwise default is "desc".
-	Direction string
-
-	// For paginated result sets, page of results to retrieve.
-	Page int
-}
-
-// ListStarred lists all the repos starred by a user.  Passing the empty string
-// will list the starred repositories for the authenticated user.
-//
-// GitHub API docs: http://developer.github.com/v3/activity/starring/#list-repositories-being-starred
-func (s *ActivityService) ListStarred(user string, opt *ActivityListStarredOptions) ([]Repository, error) {
-	var u string
-	if user != "" {
-		u = fmt.Sprintf("users/%v/starred", user)
-	} else {
-		u = "user/starred"
-	}
-	if opt != nil {
-		params := url.Values{
-			"sort":      []string{opt.Sort},
-			"direction": []string{opt.Direction},
-			"page":      []string{strconv.Itoa(opt.Page)},
-		}
-		u += "?" + params.Encode()
-	}
-	req, err := s.client.NewRequest("GET", u, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	repos := new([]Repository)
-	_, err = s.client.Do(req, repos)
-	return *repos, err
+    client *Client
 }
--- github/github.go
+++ github/github.go
@@ -87,7 +87,6 @@ type Client struct {
 
 	// Services used for talking to different parts of the API
 
-	Events        *EventsService
 	Issues        *IssuesService
 	Organizations *OrganizationsService
 	PullRequests  *PullRequestsService
@@ -116,7 +115,6 @@ func NewClient(httpClient *http.Client) *Client {
 	baseURL, _ := url.Parse(defaultBaseURL)
 
 	c := &Client{client: httpClient, BaseURL: baseURL, UserAgent: userAgent}
-	c.Events = &EventsService{client: c}
 	c.Issues = &IssuesService{client: c}
 	c.Organizations = &OrganizationsService{client: c}
 	c.PullRequests = &PullRequestsService{client: c}
