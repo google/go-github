b4b4710d3de55c8c3ea5c45edf55ed65482210b8
jlaportebot <jlaportebot@users.noreply.github.com>
2013-08-01 14:40:14 -0700

    fix example to handle returned Response

--- examples/example.go
+++ examples/example.go
@@ -17,14 +17,14 @@ func main() {
 	fmt.Println("Recently updated repositories owned by user willnorris:")
 
 	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
-	repos, err := client.Repositories.List("willnorris", opt)
+	repos, _, err := client.Repositories.List("willnorris", opt)
 	if err != nil {
 		fmt.Printf("error: %v\n\n", err)
 	} else {
 		fmt.Printf("%#v\n\n", repos)
 	}
 
-	rate, err := client.RateLimit()
+	rate, _, err := client.RateLimit()
 	if err != nil {
 		fmt.Printf("Error fetching rate limit: %#v\n\n", err)
 	} else {
