57576dc161b98ded73132625f24850824953e755
jlaportebot <jlaportebot@users.noreply.github.com>
2013-08-01 12:08:47 -0400

    Moved timestamp implementation into its own file.
    This was done so that timestamp_test.go would have a corresponding file that
    could easily be found. It did not make sense to move timestamp_test.go into
    github_test.go if we are trying to move to a more specific source structure.

--- github/github.go
+++ github/github.go
@@ -393,29 +393,3 @@ func cloneRequest(r *http.Request) *http.Request {
 	}
 	return r2
 }
-
-// Timestamp represents a time that can be unmarshalled from a JSON string
-// formatted as either an RFC3339 or Unix timestamp. This is necessary for some
-// fields since the GitHub API is inconsistent in how it represents times. All
-// exported methods of time.Time can be called on Timestamp.
-type Timestamp struct {
-	time.Time
-}
-
-// UnmarshalJSON implements the json.Unmarshaler interface.
-// Time is expected in RFC3339 or Unix format.
-func (t *Timestamp) UnmarshalJSON(data []byte) (err error) {
-	str := string(data)
-	i, err := strconv.ParseInt(str, 10, 64)
-	if err == nil {
-		(*t).Time = time.Unix(i, 0)
-	} else {
-		(*t).Time, err = time.Parse(`"`+time.RFC3339+`"`, str)
-	}
-	return
-}
-
-// Equal reports whether t and u are equal based on time.Equal
-func (t Timestamp) Equal(u Timestamp) bool {
-	return t.Time.Equal(u.Time)
-}
