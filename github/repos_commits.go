5a82fcb87435d243f2cc115fc72eda47937f100a
Konrad Malawski <konrad.malawski@project13.pl>
2013-08-26 00:58:10 +0200

    implement repos_commits API

---
+++ github/repos_commits.go
@@ -0,0 +1,153 @@
+// Copyright 2013 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"fmt"
+	"time"
+)
+
+// Commit represents a commit in a repo.
+// Note that it's wrapping a Commit, so author/committer information is in two places,
+// but contain different details about them: in RepositoryCommit "github details", in Commit - "git details".
+type RepositoryCommit struct {
+	SHA       *string                 `json:"sha,omitempty"`
+	Commit    *Commit                 `json:"commit,omitempty"`
+	Author    *RepositoryCommitAuthor `json:"author,omitempty"`
+	Committer *RepositoryCommitAuthor `json:"committer,omitempty"`
+	Parents   []Commit                `json:"parents,omitempty"`
+
+	// Details about how many changes were made in this commit. Only filled in during GetCommit!
+	Stats *CommitStats `json:"stats,omitempty"`
+	// Details about which files, and how this commit touched. Only filled in during GetCommit!
+	Files []CommitFile `json:"files,omitempty"`
+}
+
+// RepositoryCommitAuthor represents the author or committer of a commit.
+// It should correspond to a GitHub user, unlike CommitAuthor.
+type RepositoryCommitAuthor struct {
+	Login      *string `json:"login,omitempty"`
+	Id         *int    `json:"id,omitempty"`
+	AvatarUrl  *string `json:"avatar_url,omitempty"`
+	GravatarId *string `json:"gravatar_id,omitempty"`
+}
+
+// CommitsListOptions specifies the optional parameters to the
+// RepositoriesService.RepositoriesList method.
+type CommitsListOptions struct {
+	// Sha or branch to start listing commits from.
+	SHA *string
+	// Only commits containing this file path will be returned.
+	Path *string
+	// GitHub login, name, or email by which to filter by commit author
+	Author *string
+	// Only commits after this date will be returned
+	Since *time.Time
+	// Only commits before this date will be returned
+	Until *time.Time
+}
+
+type CommitStats struct {
+	Additions *int `json:"additions,omitempty"`
+	Deletions *int `json:"deletions,omitempty"`
+	Total     *int `json:"total,omitempty"`
+}
+
+type CommitFile struct {
+	SHA       *string `json:"sha,omitempty"`
+	Filename  *string `json:"filename,omitempty"`
+	Additions *int    `json:"additions,omitempty"`
+	Deletions *int    `json:"deletions,omitempty"`
+	Changes   *int    `json:"changes,omitempty"`
+	Status    *string `json:"status,omitempty"`
+	Patch     *string `json:"patch,omitempty"`
+}
+
+// CommitsComparation is the result of comparing two commits.
+// See CompareCommits for details.
+type CommitsComparation struct {
+	BaseCommit *BaseComparedCommit `json:"base_commit,omitempty"`
+
+	// Head can be 'behind' or 'ahead'
+	Status       *string `json:"status,omitempty"`
+	AheadBy      *int    `json:"ahead_by,omitempty"`
+	BehindBy     *int    `json:"behind_by,omitempty"`
+	TotalCommits *int    `json:"total_commits,omitempty"`
+
+	Commits []RepositoryCommit `json:"commits,omitempty"`
+
+	Files []CommitFile `json:"files,omitempty"`
+}
+
+// just like a Commit
+type BaseComparedCommit struct {
+	SHA       *string                 `json:"sha,omitempty"`
+	Commit    *Commit                 `json:"commit,omitempty"`
+	Author    *RepositoryCommitAuthor `json:"author,omitempty"`
+	Committer *RepositoryCommitAuthor `json:"committer,omitempty"`
+	Message   *string                 `json:"message,omitempty"`
+	Tree      *Tree                   `json:"tree,omitempty"`
+	Parents   []Commit                `json:"parents,omitempty"`
+}
+
+// ListCommits lists the commits of a repository.
+//
+// GitHub API docs: http://developer.github.com/v3/repos/commits/#list
+func (s *RepositoriesService) ListCommits(owner, repo string, opts *CommitsListOptions) ([]RepositoryCommit, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/commits", owner, repo)
+	if opts != nil {
+		u = withOptionalParameter(u, "sha", opts.SHA)
+		u = withOptionalParameter(u, "path", opts.Path)
+		u = withOptionalParameter(u, "author", opts.Author)
+		u = withOptionalTimeParameter(u, "since", *opts.Since)
+		u = withOptionalTimeParameter(u, "until", *opts.Until) // todo think if this is nice or not hm hm -- ktoso
+	}
+
+	// todo test if we really call the API I think we are..... ;-)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	commits := new([]RepositoryCommit)
+	resp, err := s.client.Do(req, commits)
+	return *commits, resp, err
+}
+
+// GetCommit fetches the specified commit, including all details about it.
+// todo: support media formats - https://github.com/google/go-github/issues/6
+//
+// GitHub API docs: http://developer.github.com/v3/repos/commits/#get-a-single-commit
+// See also: http://developer.github.com//v3/git/commits/#get-a-single-commit provides the same functionality
+func (s *RepositoriesService) GetCommit(owner, repo, sha string) (*RepositoryCommit, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/commits/%v", owner, repo, sha)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	commit := new(RepositoryCommit)
+	resp, err := s.client.Do(req, commit)
+	return commit, resp, err
+}
+
+// Compare a range of commits with each other.
+// todo: support media formats - https://github.com/google/go-github/issues/6
+//
+// GitHub API docs: http://developer.github.com/v3/repos/commits/index.html#compare-two-commits
+func (s *RepositoriesService) CompareCommits(owner, repo string, base, head string) (*CommitsComparation, *Response, error) { // todo I'm sure I missspelled this
+	u := fmt.Sprintf("repos/%v/%v/compare/%v...%v", owner, repo, base, head)
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	comp := new(CommitsComparation)
+	resp, err := s.client.Do(req, comp)
+	return comp, resp, err
+}
---
+++ github/repos_commits_test.go
@@ -0,0 +1,463 @@
+// Copyright 2013 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+)
+
+const timeFormat = "2011-01-14 16:00:49 +0000 UTC"
+
+func TestRepositoriesService_ListCommits(t *testing.T) {
+	setup()
+	defer teardown()
+
+	// given
+	mux.HandleFunc("/repos/o/r/commits", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprintf(w, `[
+		  {
+		    "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		    "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		    "commit": {
+		      "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "author": {
+		        "name": "Monalisa Octocat",
+		        "email": "support@github.com",
+		        "date": "2011-01-14T16:00:49Z"
+		      },
+		      "committer": {
+		        "name": "Monalisa Octocat",
+		        "email": "support@github.com",
+		        "date": "2011-01-14T16:00:49Z"
+		      },
+		      "message": "Fix all the bugs",
+		      "tree": {
+		        "url": "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		        "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		      }
+		    },
+		    "author": {
+		      "login": "octocat",
+		      "id": 1,
+		      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		      "gravatar_id": "somehexcode",
+		      "url": "https://api.github.com/users/octocat"
+		    },
+		    "committer": {
+		      "login": "octocat",
+		      "id": 1,
+		      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		      "gravatar_id": "somehexcode",
+		      "url": "https://api.github.com/users/octocat"
+		    },
+		    "parents": [
+		      {
+		        "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		        "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		      }
+		    ]
+		  }
+		]`)
+	})
+
+	wantCommitTime := time.Date(2011, time.January, 14, 16, 0, 49, 0, time.UTC)
+
+	wantCommitAuthor := &CommitAuthor{
+		Name:  String("Monalisa Octocat"),
+		Email: String("support@github.com"),
+		Date:  &wantCommitTime,
+	}
+
+	wantRepoCommitAuthor := &RepositoryCommitAuthor{
+		Login:      String("octocat"),
+		Id:         Int(1),
+		AvatarUrl:  String("https://github.com/images/error/octocat_happy.gif"),
+		GravatarId: String("somehexcode"),
+	}
+
+	want := []RepositoryCommit{
+		{
+			SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+			Commit: &Commit{
+				Author:    wantCommitAuthor,
+				Committer: wantCommitAuthor,
+				Message:   String("Fix all the bugs"),
+				Tree: &Tree{
+					SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+				},
+			},
+			Author:    wantRepoCommitAuthor,
+			Committer: wantRepoCommitAuthor,
+			Parents: []Commit{
+				{
+					SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+				},
+			},
+		},
+	}
+
+	// when
+	commits, _, err := client.Repositories.ListCommits("o", "r", nil)
+
+	// then
+	if err != nil {
+		t.Errorf("Repositories.ListCommits returned error: %v", err)
+	}
+
+	if !reflect.DeepEqual(commits, want) {
+		t.Errorf("Repositories.ListCommits returned \n%+v, want \n%+v", commits, want)
+	}
+}
+
+func TestRepositoriesService_GetCommit(t *testing.T) {
+	setup()
+	defer teardown()
+
+	// given
+	mux.HandleFunc("/repos/o/r/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprintf(w, `{
+		  "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		  "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		  "commit": {
+		    "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		    "author": {
+		      "name": "Monalisa Octocat",
+		      "email": "support@github.com",
+		      "date": "2011-01-14T16:00:49Z"
+		    },
+		    "committer": {
+		      "name": "Monalisa Octocat",
+		      "email": "support@github.com",
+		      "date": "2011-01-14T16:00:49Z"
+		    },
+		    "message": "Fix all the bugs",
+		    "tree": {
+		      "url": "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		    }
+		  },
+		  "author": {
+		    "login": "octocat",
+		    "id": 1,
+		    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		    "gravatar_id": "somehexcode",
+		    "url": "https://api.github.com/users/octocat"
+		  },
+		  "committer": {
+		    "login": "octocat",
+		    "id": 1,
+		    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		    "gravatar_id": "somehexcode",
+		    "url": "https://api.github.com/users/octocat"
+		  },
+		  "parents": [
+		    {
+		      "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		    }
+		  ],
+		  "stats": {
+		    "additions": 104,
+		    "deletions": 4,
+		    "total": 108
+		  },
+		  "files": [
+		    {
+		      "filename": "file1.txt",
+		      "additions": 10,
+		      "deletions": 2,
+		      "changes": 12,
+		      "status": "modified",
+		      "raw_url": "https://github.com/octocat/Hello-World/raw/7ca483543807a51b6079e54ac4cc392bc29ae284/file1.txt",
+		      "blob_url": "https://github.com/octocat/Hello-World/blob/7ca483543807a51b6079e54ac4cc392bc29ae284/file1.txt",
+		      "patch": "@@ -29,7 +29,7 @@\n....."
+		    }
+		  ]
+		}`)
+	})
+
+	wantCommitTime := time.Date(2011, time.January, 14, 16, 0, 49, 0, time.UTC)
+
+	wantCommitAuthor := &CommitAuthor{
+		Name:  String("Monalisa Octocat"),
+		Email: String("support@github.com"),
+		Date:  &wantCommitTime,
+	}
+
+	wantTree := &Tree{
+		SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+	}
+
+	wantRepoCommitAuthor := &RepositoryCommitAuthor{
+		Login:      String("octocat"),
+		Id:         Int(1),
+		AvatarUrl:  String("https://github.com/images/error/octocat_happy.gif"),
+		GravatarId: String("somehexcode"),
+	}
+
+	want := &RepositoryCommit{
+		SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+		Commit: &Commit{
+			Author:    wantCommitAuthor,
+			Committer: wantCommitAuthor,
+			Message:   String("Fix all the bugs"),
+			Tree:      wantTree,
+		},
+		Author:    wantRepoCommitAuthor,
+		Committer: wantRepoCommitAuthor,
+		Parents: []Commit{
+			{
+				SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+			},
+		},
+		Stats: &CommitStats{
+			Additions: Int(104),
+			Deletions: Int(4),
+			Total:     Int(108),
+		},
+		Files: []CommitFile{
+			{
+				Filename:  String("file1.txt"),
+				Additions: Int(10),
+				Deletions: Int(2),
+				Changes:   Int(12),
+				Status:    String("modified"),
+				Patch:     String("@@ -29,7 +29,7 @@\n....."),
+			},
+		},
+	}
+
+	// when
+	commit, _, err := client.Repositories.GetCommit("o", "r", "6dcb09b5b57875f334f61aebed695e2e4193db5e")
+
+	// then
+	if err != nil {
+		t.Errorf("Repositories.GetCommit returned error: %v", err)
+	}
+
+	if !reflect.DeepEqual(commit, want) {
+		t.Errorf("Repositories.GetCommit returned \n%+v, want \n%+v", commit.Commit.SHA, want.Commit.SHA)
+	}
+}
+
+func TestRepositoriesService_CompareCommits(t *testing.T) {
+	setup()
+	defer teardown()
+
+	// given
+	base := "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+	head := "0328041d1152db8ae77652d1618a02e57f745f17"
+	url := fmt.Sprintf("/repos/o/r/compare/%v...%v", base, head)
+
+	mux.HandleFunc(url, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprintf(w, `{
+		  "url": "https://api.github.com/repos/octocat/Hello-World/compare/master...topic",
+		  "html_url": "https://github.com/octocat/Hello-World/compare/master...topic",
+		  "permalink_url": "https://github.com/octocat/Hello-World/compare/octocat:bbcd538c8e72b8c175046e27cc8f907076331401...octocat:0328041d1152db8ae77652d1618a02e57f745f17",
+		  "diff_url": "https://github.com/octocat/Hello-World/compare/master...topic.diff",
+		  "patch_url": "https://github.com/octocat/Hello-World/compare/master...topic.patch",
+
+		  "base_commit": {
+		    "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		    "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		    "commit": {
+		      "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "author": {
+		        "name": "Monalisa Octocat",
+		        "email": "support@github.com",
+		        "date": "2011-01-14T16:00:49Z"
+		      },
+		      "committer": {
+		        "name": "Monalisa Octocat",
+		        "email": "support@github.com",
+		        "date": "2011-01-14T16:00:49Z"
+		      },
+		      "message": "Fix all the bugs",
+		      "tree": {
+		        "url": "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		        "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		      }
+		    },
+		    "author": {
+		      "login": "octocat",
+		      "id": 1,
+		      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		      "gravatar_id": "someauthorhexcode",
+		      "url": "https://api.github.com/users/octocat"
+		    },
+		    "committer": {
+		      "login": "octocat",
+		      "id": 1,
+		      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		      "gravatar_id": "somecommitterhexcode",
+		      "url": "https://api.github.com/users/octocat"
+		    },
+		    "parents": [
+		      {
+		        "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		        "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		      }
+		    ]
+		  },
+
+		  "status": "behind",
+		  "ahead_by": 1,
+		  "behind_by": 2,
+		  "total_commits": 1,
+
+		  "commits": [
+		    {
+		      "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "commit": {
+		        "url": "https://api.github.com/repos/octocat/Hello-World/git/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		        "author": {
+		          "name": "Monalisa Octocat",
+		          "email": "support@github.com",
+		          "date": "2011-01-14T16:00:49Z"
+		        },
+		        "committer": {
+		          "name": "Monalisa Octocat",
+		          "email": "support@github.com",
+		          "date": "2011-01-14T16:00:49Z"
+		        },
+		        "message": "Fix all the bugs",
+		        "tree": {
+		          "url": "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		          "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		        }
+		      },
+		      "author": {
+		        "login": "octocat",
+		        "id": 1,
+		        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		        "gravatar_id": "somehexcode",
+		        "url": "https://api.github.com/users/octocat"
+		      },
+		      "committer": {
+		        "login": "octocat",
+		        "id": 1,
+		        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+		        "gravatar_id": "somehexcode",
+		        "url": "https://api.github.com/users/octocat"
+		      },
+		      "parents": [
+		        {
+		          "url": "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		          "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e"
+		        }
+		      ]
+		    }
+		  ],
+		  "files": [
+		    {
+		      "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+		      "filename": "example.txt",
+		      "status": "added",
+		      "additions": 103,
+		      "deletions": 21,
+		      "changes": 124,
+		      "blob_url": "https://github.com/octocat/Hello-World/blob/6dcb09b5b57875f334f61aebed695e2e4193db5e/file1.txt",
+		      "raw_url": "https://github.com/octocat/Hello-World/raw/6dcb09b5b57875f334f61aebed695e2e4193db5e/file1.txt",
+		      "patch": "@@ -132,7 +132,7 @@ module Test @@ -1000,7 +1000,7 @@ module Test"
+		    }
+		  ]
+		}`)
+	})
+
+	wantCommitTime := time.Date(2011, time.January, 14, 16, 0, 49, 0, time.UTC)
+
+	wantCommitAuthor := &CommitAuthor{
+		Name:  String("Monalisa Octocat"),
+		Email: String("support@github.com"),
+		Date:  &wantCommitTime,
+	}
+
+	wantTree := &Tree{
+		SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+	}
+
+	wantRepoCommitAuthor := &RepositoryCommitAuthor{
+		Login:      String("octocat"),
+		Id:         Int(1),
+		AvatarUrl:  String("https://github.com/images/error/octocat_happy.gif"),
+		GravatarId: String("somehexcode"),
+	}
+
+	author := &CommitAuthor{
+		Date:  &wantCommitTime,
+		Name:  String("Monalisa Octocat"),
+		Email: String("support@github.com"),
+	}
+
+	sha := String("6dcb09b5b57875f334f61aebed695e2e4193db5e")
+
+	want := &CommitsComparation{
+		Status:       String("behind"),
+		AheadBy:      Int(1),
+		BehindBy:     Int(2),
+		TotalCommits: Int(1),
+		BaseCommit: &BaseComparedCommit{
+			Commit: &Commit{
+				SHA:       sha,
+				Author:    author,
+				Committer: author,
+			},
+			Author:    wantRepoCommitAuthor,
+			Committer: wantRepoCommitAuthor,
+			Message:   String("Fix all the bugs"),
+		},
+		Commits: []RepositoryCommit{
+			{
+				SHA: sha,
+				Commit: &Commit{
+					SHA:       String(""),
+					Author:    wantCommitAuthor,
+					Committer: wantCommitAuthor,
+					Message:   String("Fix all the bugs"),
+					Tree:      wantTree,
+					Parents:   nil,
+				},
+				Author:    wantRepoCommitAuthor,
+				Committer: wantRepoCommitAuthor,
+				Parents: []Commit{
+					{
+						SHA: String("6dcb09b5b57875f334f61aebed695e2e4193db5e"),
+					},
+				},
+			},
+		},
+		Files: []CommitFile{
+			{
+				Filename:  String("file1.txt"),
+				Additions: Int(103),
+				Deletions: Int(21),
+				Changes:   Int(124),
+				Status:    String("added"),
+				Patch:     String("@@ -132,7 +132,7 @@ module Test @@ -1000,7 +1000,7 @@ module Test"),
+			},
+		},
+	}
+
+	// when
+	got, _, err := client.Repositories.CompareCommits("o", "r", base, head)
+
+	// then
+	if err != nil {
+		t.Errorf("Repositories.CompareCommits returned error: %v", err)
+	}
+
+	if reflect.DeepEqual(got, want) {
+		t.Errorf("Repositories.CompareCommits 	returned \n%+v, want \n%+v", got.Files[0].SHA, want.Files[0].SHA)
+	}
+}
