af59185d440b56706351bd0bdf92205310b079b7
Amey Sakhadeo <me@ameyms.com>
2013-10-24 22:11:04 +0530

    Incorporate changes from  willnorris`s code review feedback

--- github/activity_watching.go
+++ github/activity_watching.go
@@ -2,16 +2,18 @@ package github
 
 import "fmt"
 
-// Lists watchers of a particular repo
+// ListWatchers lists watchers of a particular repo
 // GitHub API Docs: http://developer.github.com/v3/activity/watching/#list-watchers
 func (s *ActivityService) ListWatchers(owner, repo string) ([]User, *Response, error) {
 	url := fmt.Sprintf("repos/%s/%s/subscribers", owner, repo)
 
 	req, err := s.client.NewRequest("GET", url, nil)
+        if err != nil {
+          return nil, nil, err
+        }
+
 	watchers := new([]User)
-
 	resp, err := s.client.Do(req, watchers)
-
 	if err != nil {
 		return nil, resp, err
 	}
