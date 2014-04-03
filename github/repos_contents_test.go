25c33dc9f269b5bc2a67baf02c01a9a7c7a92d37
Ondrej Kupka <ondra.cap@gmail.com>
2014-04-03 17:08:07 +0200

    Add support for RepositoriesService GetContent
    Signed-off-by: Ondrej Kupka <ondra.cap@gmail.com>

--- github/repos_contents_test.go
+++ github/repos_contents_test.go
@@ -24,9 +24,75 @@ func TestRepositoriesService_GetReadme(t *testing.T) {
 	if err != nil {
 		t.Errorf("Repositories.GetReadme returned error: %v", err)
 	}
-	want := &RepositoryContent{Type: String("file"), Name: String("README.md"), Size: Int(5362), Encoding: String("base64"), Path: String("README.md")}
+	want := &RepositoryContent{
+		Type:     String("file"),
+		Encoding: String("base64"),
+		Size:     Int(5362),
+		Name:     String("README.md"),
+		Path:     String("README.md"),
+	}
 	if !reflect.DeepEqual(readme, want) {
 		t.Errorf("Repositories.GetReadme returned %+v, want %+v", readme, want)
+	}
+}
+
+func TestRepositoriesService_GetContents(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc("/repos/o/r/contents/package.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{})
+		fmt.Fprint(w, `{
+		  "type": "file",
+		  "encoding": "base64",
+		  "size": 5362,
+		  "name": "package.json",
+		  "path": "package.json"
+		}`)
+	})
+	content, _, err := client.Repositories.GetContents("o", "r", "package.json", "")
+	if err != nil {
+		t.Errorf("Repositories.GetContents returned error: %v", err)
+	}
+	want := &RepositoryContent{
+		Type:     String("file"),
+		Encoding: String("base64"),
+		Size:     Int(5362),
+		Name:     String("package.json"),
+		Path:     String("package.json"),
+	}
+	if !reflect.DeepEqual(content, want) {
+		t.Errorf("Repositories.GetContents returned %+v, want %+v", content, want)
+	}
+}
+
+func TestRepositoriesService_GetContentsAtCustomRef(t *testing.T) {
+	setup()
+	defer teardown()
+	mux.HandleFunc("/repos/o/r/contents/package.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{"ref": "develop"})
+		fmt.Fprint(w, `{
+		  "type": "file",
+		  "encoding": "base64",
+		  "size": 5362,
+		  "name": "package.json",
+		  "path": "package.json"
+		}`)
+	})
+	content, _, err := client.Repositories.GetContents("o", "r", "package.json", "develop")
+	if err != nil {
+		t.Errorf("Repositories.GetContents returned error: %v", err)
+	}
+	want := &RepositoryContent{
+		Type:     String("file"),
+		Encoding: String("base64"),
+		Size:     Int(5362),
+		Name:     String("package.json"),
+		Path:     String("package.json"),
+	}
+	if !reflect.DeepEqual(content, want) {
+		t.Errorf("Repositories.GetContents returned %+v, want %+v", content, want)
 	}
 }
 
