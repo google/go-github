9f2a72abf206d7732b7cf04945309292ebada42a
Will Norris <willnorris@google.com>
2014-04-10 11:29:03 -0700

    remove Users.EditKey method
    User keys can no longer be updated.  Instead, they should be deleted and
    re-added.

--- github/users_keys_test.go
+++ github/users_keys_test.go
@@ -107,35 +107,6 @@ func TestUsersService_CreateKey(t *testing.T) {
 	}
 }
 
-func TestUsersService_EditKey(t *testing.T) {
-	setup()
-	defer teardown()
-
-	input := &Key{Key: String("k"), Title: String("t")}
-
-	mux.HandleFunc("/user/keys/1", func(w http.ResponseWriter, r *http.Request) {
-		v := new(Key)
-		json.NewDecoder(r.Body).Decode(v)
-
-		testMethod(t, r, "PATCH")
-		if !reflect.DeepEqual(v, input) {
-			t.Errorf("Request body = %+v, want %+v", v, input)
-		}
-
-		fmt.Fprint(w, `{"id":1}`)
-	})
-
-	key, _, err := client.Users.EditKey(1, input)
-	if err != nil {
-		t.Errorf("Users.EditKey returned error: %v", err)
-	}
-
-	want := &Key{ID: Int(1)}
-	if !reflect.DeepEqual(key, want) {
-		t.Errorf("Users.EditKey returned %+v, want %+v", key, want)
-	}
-}
-
 func TestUsersService_DeleteKey(t *testing.T) {
 	setup()
 	defer teardown()
