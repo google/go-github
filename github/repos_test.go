1e12d6a1a2ff1df760d7b8e2a20cb629ab38e300
jlaportebot <jlaportebot@users.noreply.github.com>
2015-08-04 15:08:44 -0700

    include license preview header when listing repos

--- github/repos_test.go
+++ github/repos_test.go
@@ -19,6 +19,7 @@ func TestRepositoriesService_List_authenticatedUser(t *testing.T) {
 
 	mux.HandleFunc("/user/repos", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
+		testHeader(t, r, "Accept", mediaTypeLicensesPreview)
 		fmt.Fprint(w, `[{"id":1},{"id":2}]`)
 	})
 
@@ -39,6 +40,7 @@ func TestRepositoriesService_List_specifiedUser(t *testing.T) {
 
 	mux.HandleFunc("/users/u/repos", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
+		testHeader(t, r, "Accept", mediaTypeLicensesPreview)
 		testFormValues(t, r, values{
 			"type":      "owner",
 			"sort":      "created",
@@ -71,6 +73,7 @@ func TestRepositoriesService_ListByOrg(t *testing.T) {
 
 	mux.HandleFunc("/orgs/o/repos", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
+		testHeader(t, r, "Accept", mediaTypeLicensesPreview)
 		testFormValues(t, r, values{
 			"type": "forks",
 			"page": "2",
