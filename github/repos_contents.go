9d46db9335491c3b013098f083246e1127c08b8b
jlaportebot <jlaportebot@users.noreply.github.com>
2016-01-23 11:26:36 -0800

    RepositoryContentFileOptions: specify .Content as unencoded Fixes #263.

--- github/repos_contents.go
+++ github/repos_contents.go
@@ -43,7 +43,7 @@ type RepositoryContentResponse struct {
 // RepositoryContentFileOptions specifies optional parameters for CreateFile, UpdateFile, and DeleteFile.
 type RepositoryContentFileOptions struct {
 	Message   *string       `json:"message,omitempty"`
-	Content   []byte        `json:"content,omitempty"`
+	Content   []byte        `json:"content,omitempty"` // unencoded
 	SHA       *string       `json:"sha,omitempty"`
 	Branch    *string       `json:"branch,omitempty"`
 	Author    *CommitAuthor `json:"author,omitempty"`
