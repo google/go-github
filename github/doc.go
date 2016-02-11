121939088e5f54f654c25e4fd711e871444383cf
Dmitri Shuralyov <shurcooL@gmail.com>
2016-02-10 23:43:07 -0800

    Minor Rate, RateLimits documentation fixes.
    Rate is now a method, not a field.
    
    Mention RateLimits rather than RateLimit since the latter is deprecated.
    
    Use Go convention for marking deprecated identifiers. See
    https://github.com/golang/blog/commit/257114af91a0defb1fc2c16c7f4ae2429b8a4e0f.

--- github/doc.go
+++ github/doc.go
@@ -59,10 +59,10 @@ limited to 60 requests per hour, while authenticated clients can make up to
 that are not issued on behalf of a user, use the
 UnauthenticatedRateLimitedTransport.
 
-The Rate field on a client tracks the rate limit information based on the most
+The Rate method on a client returns the rate limit information based on the most
 recent API call.  This is updated on every call, but may be out of date if it's
 been some time since the last API call and other clients have made subsequent
-requests since then.  You can always call RateLimit() directly to get the most
+requests since then.  You can always call RateLimits() directly to get the most
 up-to-date rate limit data for the client.
 
 Learn more about GitHub rate limiting at
