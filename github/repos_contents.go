44e82c5a7abba7d29f280e21d1e5a0a4fae9c35e
Piotr Zurek <p.zurek@gmail.com>
2016-03-21 17:04:17 +1300

    Fixes path escaping for Repositories.GetContents
    Fixes #308.
    Fixes #309.
    
    Change-Id: I71d1422c90f32cc6e6834ed428a311a2b2b88778

--- github/repos_contents.go
+++ github/repos_contents.go
@@ -128,9 +128,7 @@ func (s *RepositoriesService) DownloadContents(owner, repo, filepath string, opt
 //
 // GitHub API docs: http://developer.github.com/v3/repos/contents/#get-contents
 func (s *RepositoriesService) GetContents(owner, repo, path string, opt *RepositoryContentGetOptions) (fileContent *RepositoryContent, directoryContent []*RepositoryContent, resp *Response, err error) {
-	// escape characters not allowed in URL path.  This actually escapes a
-	// lot more, but seems to be harmless.
-	escapedPath := url.QueryEscape(path)
+	escapedPath := (&url.URL{Path: path}).String()
 	u := fmt.Sprintf("repos/%s/%s/contents/%s", owner, repo, escapedPath)
 	u, err = addOptions(u, opt)
 	if err != nil {
