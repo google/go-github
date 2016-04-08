c048c3943f18dd5d9a3a770266e47dd5780ff095
jlaportebot <jlaportebot@users.noreply.github.com>
2016-04-08 10:35:47 -0700

    add support for Deployment and DeploymentStatus API enhancements
    Fixes #328.
    
    Change-Id: I2f93fb89eb46e9d341e9a9aada8de1b3339d1ffd

--- github/github.go
+++ github/github.go
@@ -65,6 +65,9 @@ const (
 
 	// https://help.github.com/enterprise/2.4/admin/guides/migrations/exporting-the-github-com-organization-s-repositories/
 	mediaTypeMigrationsPreview = "application/vnd.github.wyandotte-preview+json"
+
+	// https://developer.github.com/changes/2016-04-06-deployment-and-deployment-status-enhancements/
+	mediaTypeDeploymentStatusPreview = "application/vnd.github.ant-man-preview+json"
 )
 
 // A Client manages communication with the GitHub API.
