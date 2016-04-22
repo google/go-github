1d56631ea80b4d3f9711eb63bb4559926e5722dd
Dmitri Shuralyov <shurcooL@gmail.com>
2016-04-21 22:44:51 -0700

    tests/integration: Turn into valid, empty Go package.
    This describes the package in a way that can be seen via godoc,
    and makes it so that the following command succeeds without
    "no buildable Go source files" error:
    
    	go build github.com/google/go-github/...

---
+++ tests/integration/doc.go
@@ -0,0 +1,6 @@
+// Package tests contains integration tests.
+//
+// These tests call the live GitHub API, and therefore require a little more
+// setup to run.  See https://github.com/google/go-github/tree/master/tests/integration
+// for more information.
+package tests
--- tests/integration/github_test.go
+++ tests/integration/github_test.go
@@ -3,9 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// These tests call the live GitHub API, and therefore require a little more
-// setup to run.  See https://github.com/google/go-github/tree/master/tests/integration
-// for more information
+// +build integration
 
 package tests
 
