216bef3fc8958556eff6a921c6689469c963b8e5
Dmitri Shuralyov <shurcooL@gmail.com>
2016-05-03 22:25:14 -0700

    Simplify Bool, Int, String helpers.
    Code simplification suggestion by @gmlewis.

--- github/github.go
+++ github/github.go
@@ -689,24 +689,12 @@ func cloneRequest(r *http.Request) *http.Request {
 
 // Bool is a helper routine that allocates a new bool value
 // to store v and returns a pointer to it.
-func Bool(v bool) *bool {
-	p := new(bool)
-	*p = v
-	return p
-}
+func Bool(v bool) *bool { return &v }
 
 // Int is a helper routine that allocates a new int value
 // to store v and returns a pointer to it.
-func Int(v int) *int {
-	p := new(int)
-	*p = v
-	return p
-}
+func Int(v int) *int { return &v }
 
 // String is a helper routine that allocates a new string value
 // to store v and returns a pointer to it.
-func String(v string) *string {
-	p := new(string)
-	*p = v
-	return p
-}
+func String(v string) *string { return &v }
