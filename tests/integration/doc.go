92234e567776832de23193278353150f1d9605a5
Will Norris <willnorris@google.com>
2016-07-01 08:59:49 -0700

    add missing license headers in a few files

--- tests/integration/doc.go
+++ tests/integration/doc.go
@@ -1,3 +1,8 @@
+// Copyright 2016 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package tests contains integration tests.
 //
 // These tests call the live GitHub API, and therefore require a little more
