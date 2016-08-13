5d21f36880d50311d5791778b7d420ff713797d3
Juan Basso <jrbasso@gmail.com>
2016-08-12 20:13:39 -0400

    Adding missing field to search results
    Added the field incomplete_results. It is docummented on https://developer.github.com/v3/search/#timeouts-and-incomplete-results

--- github/search.go
+++ github/search.go
@@ -40,8 +40,9 @@ type SearchOptions struct {
 
 // RepositoriesSearchResult represents the result of a repositories search.
 type RepositoriesSearchResult struct {
-	Total        *int         `json:"total_count,omitempty"`
-	Repositories []Repository `json:"items,omitempty"`
+	Total             *int         `json:"total_count,omitempty"`
+	IncompleteResults *bool        `json:"incomplete_results,omitempty"`
+	Repositories      []Repository `json:"items,omitempty"`
 }
 
 // Repositories searches repositories via various criteria.
@@ -55,8 +56,9 @@ func (s *SearchService) Repositories(query string, opt *SearchOptions) (*Reposit
 
 // IssuesSearchResult represents the result of an issues search.
 type IssuesSearchResult struct {
-	Total  *int    `json:"total_count,omitempty"`
-	Issues []Issue `json:"items,omitempty"`
+	Total             *int    `json:"total_count,omitempty"`
+	IncompleteResults *bool   `json:"incomplete_results,omitempty"`
+	Issues            []Issue `json:"items,omitempty"`
 }
 
 // Issues searches issues via various criteria.
@@ -70,8 +72,9 @@ func (s *SearchService) Issues(query string, opt *SearchOptions) (*IssuesSearchR
 
 // UsersSearchResult represents the result of an issues search.
 type UsersSearchResult struct {
-	Total *int   `json:"total_count,omitempty"`
-	Users []User `json:"items,omitempty"`
+	Total             *int   `json:"total_count,omitempty"`
+	IncompleteResults *bool  `json:"incomplete_results,omitempty"`
+	Users             []User `json:"items,omitempty"`
 }
 
 // Users searches users via various criteria.
@@ -104,8 +107,9 @@ func (tm TextMatch) String() string {
 
 // CodeSearchResult represents the result of an code search.
 type CodeSearchResult struct {
-	Total       *int         `json:"total_count,omitempty"`
-	CodeResults []CodeResult `json:"items,omitempty"`
+	Total             *int         `json:"total_count,omitempty"`
+	IncompleteResults *bool        `json:"incomplete_results,omitempty"`
+	CodeResults       []CodeResult `json:"items,omitempty"`
 }
 
 // CodeResult represents a single search result.
