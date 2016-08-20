0733f281f3b46b88b477ae6838b7bd2230fe36f9
Fabrice <fabrice.vaillant@student.ecp.fr>
2016-08-20 22:57:13 +0200

    Adds the paths api

--- github/repo_traffic.go
+++ github/repo_traffic.go
@@ -14,6 +14,14 @@ type Referrer struct {
 	Uniques  *int    `json:"uniques,omitempty"`
 }
 
+// Path represent a referrer information
+type Path struct {
+	Path    *string `json:"path,omitempty"`
+	Title   *string `json:"title,omitempty"`
+	Count   *int    `json:"count,omitempty"`
+	Uniques *int    `json:"uniques,omitempty"`
+}
+
 // ListReferrers list the top 10 referrers over the last 14 days.
 //
 // GitHub API docs: https://developer.github.com/v3/repos/traffic/#list-referrers
@@ -39,4 +47,31 @@ func (s *RepositoriesService) ListReferrers(owner, repo string, opt *ListOptions
 	}
 
 	return referrers, resp, err
+}
+
+// ListPaths list the top 10 popular content over the last 14 days.
+//
+// GitHub API docs: https://developer.github.com/v3/repos/traffic/#list-paths
+func (s *RepositoriesService) ListPaths(owner, repo string, opt *ListOptions) ([]*Path, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/traffic/popular/paths", owner, repo)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// TODO: remove custom Accept header when this API fully launches.
+	req.Header.Set("Accept", mediaTypeTrafficPreview)
+
+	var paths []*Path
+	resp, err := s.client.Do(req, &paths)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return paths, resp, err
 }
