54943985871d3b59053804af48b8347a3c232938
Ronak Jain <ronakjain@outlook.in>
2016-11-03 09:39:17 +0530

    removed authorization preview header
    Fixes #464.
    Closes #465.
    
    Change-Id: I9bde509ef6bf9382c48344e58d4a77da4b33e4a3

--- github/github.go
+++ github/github.go
@@ -79,9 +79,6 @@ const (
 	// https://developer.github.com/changes/2016-06-14-repository-invitations/
 	mediaTypeRepositoryInvitationsPreview = "application/vnd.github.swamp-thing-preview+json"
 
-	// https://developer.github.com/changes/2016-04-21-oauth-authorizations-grants-api-preview/
-	mediaTypeOAuthGrantAuthorizationsPreview = "application/vnd.github.damage-preview+json"
-
 	// https://developer.github.com/changes/2016-07-06-github-pages-preiew-api/
 	mediaTypePagesPreview = "application/vnd.github.mister-fantastic-preview+json"
 
