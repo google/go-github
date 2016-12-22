ff537e13bb907aafdfa203cf8d14d7677d1823ec
Dmitri Shuralyov <shurcooL@gmail.com>
2016-12-22 12:41:28 -0500

    Fixup for 627bfa78, revert pullRequestBody to pointer.
    Keeping it a pointer for now for consistency and because I don't yet
    have evidence it's equally performant (it might not be).
    
    Also because this is consistent with the rest of the codebase.

--- github/pulls.go
+++ github/pulls.go
@@ -301,7 +301,7 @@ type pullRequestMergeRequest struct {
 func (s *PullRequestsService) Merge(owner string, repo string, number int, commitMessage string, options *PullRequestOptions) (*PullRequestMergeResult, *Response, error) {
 	u := fmt.Sprintf("repos/%v/%v/pulls/%d/merge", owner, repo, number)
 
-	pullRequestBody := pullRequestMergeRequest{CommitMessage: commitMessage}
+	pullRequestBody := &pullRequestMergeRequest{CommitMessage: commitMessage}
 	if options != nil {
 		pullRequestBody.CommitTitle = options.CommitTitle
 		pullRequestBody.MergeMethod = options.MergeMethod
