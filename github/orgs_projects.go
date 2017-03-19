56756e005f485ec2dd559a3fc9e8dc4c3dca6a5b
Shawn Catanzarite <me@shawncatz.com>
2017-03-19 13:23:20 -0700

    forgot to go fmt

--- github/orgs_projects.go
+++ github/orgs_projects.go
@@ -6,64 +6,64 @@
 package github
 
 import (
-  "context"
-  "fmt"
+	"context"
+	"fmt"
 )
 
 // ListProjectsOptions specifies the optional parameters to the
 // OrganizationsService.ListProjects method.
 type ListProjectsOptions struct {
-  // Indicates the state of the projects to return. Can be either open, closed, or all. Default: open
-  State string `url:"state,omitempty"`
+	// Indicates the state of the projects to return. Can be either open, closed, or all. Default: open
+	State string `url:"state,omitempty"`
 
-  ListOptions
+	ListOptions
 }
 
 // ListProjects lists the projects for an organization.
 //
 // GitHub API docs: https://developer.github.com/v3/projects/#list-organization-projects
 func (s *OrganizationsService) ListProjects(ctx context.Context, org string, opt *ListProjectsOptions) ([]*Project, *Response, error) {
-  u := fmt.Sprintf("repos/%v/projects", org)
-  u, err := addOptions(u, opt)
-  if err != nil {
-    return nil, nil, err
-  }
+	u := fmt.Sprintf("repos/%v/projects", org)
+	u, err := addOptions(u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
 
-  req, err := s.client.NewRequest("GET", u, nil)
-  if err != nil {
-    return nil, nil, err
-  }
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
 
-  // TODO: remove custom Accept header when this API fully launches.
-  req.Header.Set("Accept", mediaTypeProjectsPreview)
+	// TODO: remove custom Accept header when this API fully launches.
+	req.Header.Set("Accept", mediaTypeProjectsPreview)
 
-  projects := []*Project{}
-  resp, err := s.client.Do(ctx, req, &projects)
-  if err != nil {
-    return nil, resp, err
-  }
+	projects := []*Project{}
+	resp, err := s.client.Do(ctx, req, &projects)
+	if err != nil {
+		return nil, resp, err
+	}
 
-  return projects, resp, nil
+	return projects, resp, nil
 }
 
 // CreateProject creates a GitHub Project for the specified organization.
 //
 // GitHub API docs: https://developer.github.com/v3/projects/#create-an-organization-project
 func (s *OrganizationsService) CreateProject(ctx context.Context, org string, opt *ProjectOptions) (*Project, *Response, error) {
-  u := fmt.Sprintf("repos/%v/projects", org)
-  req, err := s.client.NewRequest("POST", u, opt)
-  if err != nil {
-    return nil, nil, err
-  }
+	u := fmt.Sprintf("repos/%v/projects", org)
+	req, err := s.client.NewRequest("POST", u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
 
-  // TODO: remove custom Accept header when this API fully launches.
-  req.Header.Set("Accept", mediaTypeProjectsPreview)
+	// TODO: remove custom Accept header when this API fully launches.
+	req.Header.Set("Accept", mediaTypeProjectsPreview)
 
-  project := &Project{}
-  resp, err := s.client.Do(ctx, req, project)
-  if err != nil {
-    return nil, resp, err
-  }
+	project := &Project{}
+	resp, err := s.client.Do(ctx, req, project)
+	if err != nil {
+		return nil, resp, err
+	}
 
-  return project, resp, nil
+	return project, resp, nil
 }
--- github/orgs_projects_test.go
+++ github/orgs_projects_test.go
@@ -6,63 +6,63 @@
 package github
 
 import (
-  "context"
-  "encoding/json"
-  "fmt"
-  "net/http"
-  "reflect"
-  "testing"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+	"testing"
 )
 
 func TestOrganizationsService_ListProjects(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  mux.HandleFunc("/repos/o/projects", func(w http.ResponseWriter, r *http.Request) {
-    testMethod(t, r, "GET")
-    testHeader(t, r, "Accept", mediaTypeProjectsPreview)
-    testFormValues(t, r, values{"state": "open", "page": "2"})
-    fmt.Fprint(w, `[{"id":1}]`)
-  })
+	mux.HandleFunc("/repos/o/projects", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testHeader(t, r, "Accept", mediaTypeProjectsPreview)
+		testFormValues(t, r, values{"state": "open", "page": "2"})
+		fmt.Fprint(w, `[{"id":1}]`)
+	})
 
-  opt := &ListProjectsOptions{State: "open", ListOptions: ListOptions{Page: 2}}
-  projects, _, err := client.Organizations.ListProjects(context.Background(), "o", opt)
-  if err != nil {
-    t.Errorf("Organizations.ListProjects returned error: %v", err)
-  }
+	opt := &ListProjectsOptions{State: "open", ListOptions: ListOptions{Page: 2}}
+	projects, _, err := client.Organizations.ListProjects(context.Background(), "o", opt)
+	if err != nil {
+		t.Errorf("Organizations.ListProjects returned error: %v", err)
+	}
 
-  want := []*Project{{ID: Int(1)}}
-  if !reflect.DeepEqual(projects, want) {
-    t.Errorf("Organizations.ListProjects returned %+v, want %+v", projects, want)
-  }
+	want := []*Project{{ID: Int(1)}}
+	if !reflect.DeepEqual(projects, want) {
+		t.Errorf("Organizations.ListProjects returned %+v, want %+v", projects, want)
+	}
 }
 
 func TestOrganizationsService_CreateProject(t *testing.T) {
-  setup()
-  defer teardown()
+	setup()
+	defer teardown()
 
-  input := &ProjectOptions{Name: "Project Name", Body: "Project body."}
+	input := &ProjectOptions{Name: "Project Name", Body: "Project body."}
 
-  mux.HandleFunc("/repos/o/projects", func(w http.ResponseWriter, r *http.Request) {
-    testMethod(t, r, "POST")
-    testHeader(t, r, "Accept", mediaTypeProjectsPreview)
+	mux.HandleFunc("/repos/o/projects", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testHeader(t, r, "Accept", mediaTypeProjectsPreview)
 
-    v := &ProjectOptions{}
-    json.NewDecoder(r.Body).Decode(v)
-    if !reflect.DeepEqual(v, input) {
-      t.Errorf("Request body = %+v, want %+v", v, input)
-    }
+		v := &ProjectOptions{}
+		json.NewDecoder(r.Body).Decode(v)
+		if !reflect.DeepEqual(v, input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
 
-    fmt.Fprint(w, `{"id":1}`)
-  })
+		fmt.Fprint(w, `{"id":1}`)
+	})
 
-  project, _, err := client.Organizations.CreateProject(context.Background(), "o", input)
-  if err != nil {
-    t.Errorf("Organizations.CreateProject returned error: %v", err)
-  }
+	project, _, err := client.Organizations.CreateProject(context.Background(), "o", input)
+	if err != nil {
+		t.Errorf("Organizations.CreateProject returned error: %v", err)
+	}
 
-  want := &Project{ID: Int(1)}
-  if !reflect.DeepEqual(project, want) {
-    t.Errorf("Organizations.CreateProject returned %+v, want %+v", project, want)
-  }
+	want := &Project{ID: Int(1)}
+	if !reflect.DeepEqual(project, want) {
+		t.Errorf("Organizations.CreateProject returned %+v, want %+v", project, want)
+	}
 }
