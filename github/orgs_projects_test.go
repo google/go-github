e5dee88b4524ccbd90f1cafdab122146b16c2f13
Shawn Catanzarite <me@shawncatz.com>
2017-03-20 14:00:22 -0700

    update a few more copyright lines

--- github/orgs_projects_test.go
+++ github/orgs_projects_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The go-github AUTHORS. All rights reserved.
+// Copyright 2017 The go-github AUTHORS. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
