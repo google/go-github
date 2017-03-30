783dcb9d7edb9d9c71cf5e722d5dc8e2c8f0ef8e
jlaportebot <jlaportebot@users.noreply.github.com>
2017-03-30 18:50:55 -0400

    Return nil error explicitly in RepositoriesService.GetContents. (#608)
    This is simpler and more readable.
    
    Helps #536.

--- github/repos_contents.go
+++ github/repos_contents.go
@@ -158,11 +158,11 @@ func (s *RepositoriesService) GetContents(ctx context.Context, owner, repo, path
 	}
 	fileUnmarshalError := json.Unmarshal(rawJSON, &fileContent)
 	if fileUnmarshalError == nil {
-		return fileContent, nil, resp, fileUnmarshalError
+		return fileContent, nil, resp, nil
 	}
 	directoryUnmarshalError := json.Unmarshal(rawJSON, &directoryContent)
 	if directoryUnmarshalError == nil {
-		return nil, directoryContent, resp, directoryUnmarshalError
+		return nil, directoryContent, resp, nil
 	}
 	return nil, nil, resp, fmt.Errorf("unmarshalling failed for both file and directory content: %s and %s ", fileUnmarshalError, directoryUnmarshalError)
 }
