7487bd72a49fb49d8bd0cf4475766a191c86ff18
Sahil Dua <sahildua2305@gmail.com>
2017-05-07 17:53:18 +0200

    Add accessors for type Metric in CommunityHealthMetrics

--- github/github-accessors.go
+++ github/github-accessors.go
@@ -2572,6 +2572,38 @@ func (m *MembershipEvent) GetScope() string {
 	return *m.Scope
 }
 
+// GetHTMLURL returns the HTMLURL field if it's non-nil, zero value otherwise.
+func (m *Metric) GetHTMLURL() string {
+	if m == nil || m.HTMLURL == nil {
+		return ""
+	}
+	return *m.HTMLURL
+}
+
+// GetKey returns the Key field if it's non-nil, zero value otherwise.
+func (m *Metric) GetKey() string {
+	if m == nil || m.Key == nil {
+		return ""
+	}
+	return *m.Key
+}
+
+// GetName returns the Name field if it's non-nil, zero value otherwise.
+func (m *Metric) GetName() string {
+	if m == nil || m.Name == nil {
+		return ""
+	}
+	return *m.Name
+}
+
+// GetURL returns the URL field if it's non-nil, zero value otherwise.
+func (m *Metric) GetURL() string {
+	if m == nil || m.URL == nil {
+		return ""
+	}
+	return *m.URL
+}
+
 // GetCreatedAt returns the CreatedAt field if it's non-nil, zero value otherwise.
 func (m *Migration) GetCreatedAt() string {
 	if m == nil || m.CreatedAt == nil {
