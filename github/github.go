de79463459e9b17e188e30a67b70efea67c2a22e
Varadarajan Aravamudhan <varadaraajan@gmail.com>
2017-05-15 23:44:12 +0530

    fixup! #638: changes to media type

--- github/github.go
+++ github/github.go
@@ -92,9 +92,6 @@ const (
 	// https://developer.github.com/changes/2017-01-05-commit-search-api/
 	mediaTypeCommitSearchPreview = "application/vnd.github.cloak-preview+json"
 
-	// https://developer.github.com/changes/2016-12-14-reviews-api/
-	mediaTypePullRequestReviewsPreview = "application/vnd.github.black-cat-preview+json"
-
 	// https://developer.github.com/changes/2017-02-28-user-blocking-apis-and-webhook/
 	mediaTypeBlockUsersPreview = "application/vnd.github.giant-sentry-fist-preview+json"
 )
