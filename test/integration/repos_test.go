4ab751a53f04b451247ed3cbedd431a827de16a2
Dmitri Shuralyov <shurcooL@gmail.com>
2017-07-18 15:06:04 -0400

    test/integration: Update for API changes to fix build.
    Update code with the latest breaking API changes so that it builds
    without errors.
    
    All tests other than TestRepositories_EditBranches are passing.
    To fix that test, it requires someone with expertise in protection
    requests, and is out of scope of this change.

--- test/integration/repos_test.go
+++ test/integration/repos_test.go
@@ -8,6 +8,7 @@
 package integration
 
 import (
+	"context"
 	"net/http"
 	"reflect"
 	"testing"
@@ -21,7 +22,7 @@ func TestRepositories_CRUD(t *testing.T) {
 	}
 
 	// get authenticated user
-	me, _, err := client.Users.Get("")
+	me, _, err := client.Users.Get(context.Background(), "")
 	if err != nil {
 		t.Fatalf("Users.Get('') returned error: %v", err)
 	}
@@ -34,19 +35,19 @@ func TestRepositories_CRUD(t *testing.T) {
 	// update the repository description
 	repo.Description = github.String("description")
 	repo.DefaultBranch = nil // FIXME: this shouldn't be necessary
-	_, _, err = client.Repositories.Edit(*repo.Owner.Login, *repo.Name, repo)
+	_, _, err = client.Repositories.Edit(context.Background(), *repo.Owner.Login, *repo.Name, repo)
 	if err != nil {
 		t.Fatalf("Repositories.Edit() returned error: %v", err)
 	}
 
 	// delete the repository
-	_, err = client.Repositories.Delete(*repo.Owner.Login, *repo.Name)
+	_, err = client.Repositories.Delete(context.Background(), *repo.Owner.Login, *repo.Name)
 	if err != nil {
 		t.Fatalf("Repositories.Delete() returned error: %v", err)
 	}
 
 	// verify that the repository was deleted
-	_, resp, err := client.Repositories.Get(*repo.Owner.Login, *repo.Name)
+	_, resp, err := client.Repositories.Get(context.Background(), *repo.Owner.Login, *repo.Name)
 	if err == nil {
 		t.Fatalf("Test repository still exists after deleting it.")
 	}
@@ -57,7 +58,7 @@ func TestRepositories_CRUD(t *testing.T) {
 
 func TestRepositories_BranchesTags(t *testing.T) {
 	// branches
-	branches, _, err := client.Repositories.ListBranches("git", "git", nil)
+	branches, _, err := client.Repositories.ListBranches(context.Background(), "git", "git", nil)
 	if err != nil {
 		t.Fatalf("Repositories.ListBranches() returned error: %v", err)
 	}
@@ -66,13 +67,13 @@ func TestRepositories_BranchesTags(t *testing.T) {
 		t.Fatalf("Repositories.ListBranches('git', 'git') returned no branches")
 	}
 
-	_, _, err = client.Repositories.GetBranch("git", "git", *branches[0].Name)
+	_, _, err = client.Repositories.GetBranch(context.Background(), "git", "git", *branches[0].Name)
 	if err != nil {
 		t.Fatalf("Repositories.GetBranch() returned error: %v", err)
 	}
 
 	// tags
-	tags, _, err := client.Repositories.ListTags("git", "git", nil)
+	tags, _, err := client.Repositories.ListTags(context.Background(), "git", "git", nil)
 	if err != nil {
 		t.Fatalf("Repositories.ListTags() returned error: %v", err)
 	}
@@ -82,24 +83,13 @@ func TestRepositories_BranchesTags(t *testing.T) {
 	}
 }
 
-func TestRepositories_ServiceHooks(t *testing.T) {
-	hooks, _, err := client.Repositories.ListServiceHooks()
-	if err != nil {
-		t.Fatalf("Repositories.ListServiceHooks() returned error: %v", err)
-	}
-
-	if len(hooks) == 0 {
-		t.Fatalf("Repositories.ListServiceHooks() returned no hooks")
-	}
-}
-
 func TestRepositories_EditBranches(t *testing.T) {
 	if !checkAuth("TestRepositories_EditBranches") {
 		return
 	}
 
 	// get authenticated user
-	me, _, err := client.Users.Get("")
+	me, _, err := client.Users.Get(context.Background(), "")
 	if err != nil {
 		t.Fatalf("Users.Get('') returned error: %v", err)
 	}
@@ -109,7 +99,7 @@ func TestRepositories_EditBranches(t *testing.T) {
 		t.Fatalf("createRandomTestRepository returned error: %v", err)
 	}
 
-	branch, _, err := client.Repositories.GetBranch(*repo.Owner.Login, *repo.Name, "master")
+	branch, _, err := client.Repositories.GetBranch(context.Background(), *repo.Owner.Login, *repo.Name, "master")
 	if err != nil {
 		t.Fatalf("Repositories.GetBranch() returned error: %v", err)
 	}
@@ -118,34 +108,47 @@ func TestRepositories_EditBranches(t *testing.T) {
 		t.Fatalf("Branch %v of repo %v is already protected", "master", *repo.Name)
 	}
 
+	// TODO: This test fails with 422 Validation Failed [{Resource: Field: Code: Message:}].
+	//       Someone familiar with protection requests needs to come up with
+	//       a valid protection request that doesn't give 422 error.
 	protectionRequest := &github.ProtectionRequest{
 		RequiredStatusChecks: &github.RequiredStatusChecks{
-			IncludeAdmins: true,
-			Strict:        true,
-			Contexts:      []string{"continuous-integration"},
+			Strict:   true,
+			Contexts: []string{"continuous-integration"},
 		},
-		RequiredPullRequestReviews: &github.RequiredPullRequestReviews{
-			IncludeAdmins: true,
+		RequiredPullRequestReviews: &github.PullRequestReviewsEnforcementRequest{
+			DismissalRestrictionsRequest: &github.DismissalRestrictionsRequest{
+				Users: []string{},
+				Teams: []string{},
+			},
+			DismissStaleReviews: true,
 		},
+		EnforceAdmins: true,
 		// TODO: Only organization repositories can have users and team restrictions.
 		//       In order to be able to test these Restrictions, need to add support
 		//       for creating temporary organization repositories.
 		Restrictions: nil,
 	}
 
-	protection, _, err := client.Repositories.UpdateBranchProtection(*repo.Owner.Login, *repo.Name, "master", protectionRequest)
+	protection, _, err := client.Repositories.UpdateBranchProtection(context.Background(), *repo.Owner.Login, *repo.Name, "master", protectionRequest)
 	if err != nil {
 		t.Fatalf("Repositories.UpdateBranchProtection() returned error: %v", err)
 	}
 
 	want := &github.Protection{
 		RequiredStatusChecks: &github.RequiredStatusChecks{
-			IncludeAdmins: true,
-			Strict:        true,
-			Contexts:      []string{"continuous-integration"},
+			Strict:   true,
+			Contexts: []string{"continuous-integration"},
 		},
-		RequiredPullRequestReviews: &github.RequiredPullRequestReviews{
-			IncludeAdmins: true,
+		RequiredPullRequestReviews: &github.PullRequestReviewsEnforcement{
+			DismissalRestrictions: github.DismissalRestrictions{
+				Users: []*github.User{},
+				Teams: []*github.Team{},
+			},
+			DismissStaleReviews: true,
+		},
+		EnforceAdmins: &github.AdminEnforcement{
+			Enabled: true,
 		},
 		Restrictions: nil,
 	}
@@ -153,7 +156,7 @@ func TestRepositories_EditBranches(t *testing.T) {
 		t.Errorf("Repositories.UpdateBranchProtection() returned %+v, want %+v", protection, want)
 	}
 
-	_, err = client.Repositories.Delete(*repo.Owner.Login, *repo.Name)
+	_, err = client.Repositories.Delete(context.Background(), *repo.Owner.Login, *repo.Name)
 	if err != nil {
 		t.Fatalf("Repositories.Delete() returned error: %v", err)
 	}
@@ -164,18 +167,18 @@ func TestRepositories_List(t *testing.T) {
 		return
 	}
 
-	_, _, err := client.Repositories.List("", nil)
+	_, _, err := client.Repositories.List(context.Background(), "", nil)
 	if err != nil {
 		t.Fatalf("Repositories.List('') returned error: %v", err)
 	}
 
-	_, _, err = client.Repositories.List("google", nil)
+	_, _, err = client.Repositories.List(context.Background(), "google", nil)
 	if err != nil {
 		t.Fatalf("Repositories.List('google') returned error: %v", err)
 	}
 
 	opt := github.RepositoryListOptions{Sort: "created"}
-	repos, _, err := client.Repositories.List("google", &opt)
+	repos, _, err := client.Repositories.List(context.Background(), "google", &opt)
 	if err != nil {
 		t.Fatalf("Repositories.List('google') with Sort opt returned error: %v", err)
 	}
