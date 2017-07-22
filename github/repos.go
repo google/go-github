fb256a0e9b341e277520f4d811b6dc6e5d4aa481
RaviTeja Pothana <ravi-teja@live.com>
2017-07-22 11:26:58 +0530

    Add preview Topics support to List methods. (#676)
    This change adds support for Topics preview API for the following
    methods that were missed in #670:
    
    -	RepositoriesService.List
    -	RepositoriesService.ListByOrg
    -	RepositoriesService.ListForks
    -	OrganizationsService.ListTeamRepos
    
    Rename acceptHeader to acceptHeaders for consistency.
    
    Fixes #675.
    Updates #670.

--- github/repos.go
+++ github/repos.go
@@ -176,8 +176,9 @@ func (s *RepositoriesService) List(ctx context.Context, user string, opt *Reposi
 		return nil, nil, err
 	}
 
-	// TODO: remove custom Accept header when license support fully launches
-	req.Header.Set("Accept", mediaTypeLicensesPreview)
+	// TODO: remove custom Accept headers when APIs fully launch.
+	acceptHeaders := []string{mediaTypeLicensesPreview, mediaTypeTopicsPreview}
+	req.Header.Set("Accept", strings.Join(acceptHeaders, ", "))
 
 	var repos []*Repository
 	resp, err := s.client.Do(ctx, req, &repos)
@@ -213,8 +214,9 @@ func (s *RepositoriesService) ListByOrg(ctx context.Context, org string, opt *Re
 		return nil, nil, err
 	}
 
-	// TODO: remove custom Accept header when license support fully launches
-	req.Header.Set("Accept", mediaTypeLicensesPreview)
+	// TODO: remove custom Accept headers when APIs fully launch.
+	acceptHeaders := []string{mediaTypeLicensesPreview, mediaTypeTopicsPreview}
+	req.Header.Set("Accept", strings.Join(acceptHeaders, ", "))
 
 	var repos []*Repository
 	resp, err := s.client.Do(ctx, req, &repos)
