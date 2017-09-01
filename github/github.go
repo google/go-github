4f85cdc43a859ccad9c5da8bc55bf24205cda9d8
jlaportebot <jlaportebot@users.noreply.github.com>
2017-08-31 21:11:34 -0500

    Rename UpdateAt to UpdatedAt; fix JSON tag in Deployment structs. (#709)
    Authorization struct had a "updated_at" JSON key mapped to UpdateAt,
    which was an oversight/typo. It has been renamed to UpdatedAt, like all
    other similar fields.
    
    Deployments and DeploymentStatus structs had incorrectly mapped their
    UpdatedAt field to a non-existing "pushed_at" JSON key. That means
    their values were never populated. This change fixes that.
    
    Bump library version as this is a breaking change.
    
    Helps #605.

--- github/github.go
+++ github/github.go
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	libraryVersion = "10"
+	libraryVersion = "11"
 	defaultBaseURL = "https://api.github.com/"
 	uploadBaseURL  = "https://uploads.github.com/"
 	userAgent      = "go-github/" + libraryVersion
