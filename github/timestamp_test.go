dbb46f13e518bb1d36724a28ce08cdd898cde44a
Elliott Beach <elliott2.71828@gmail.com>
2017-09-09 17:40:26 -0500

    (#715) Add test for anomalous timestamp format from Projects API.

--- github/timestamp_test.go
+++ github/timestamp_test.go
@@ -15,6 +15,7 @@ import (
 const (
 	emptyTimeStr         = `"0001-01-01T00:00:00Z"`
 	referenceTimeStr     = `"2006-01-02T15:04:05Z"`
+	referenceTimeStrFractional = `"2006-01-02T15:04:05.000Z"` // This format was returned by the Projects API before October 1, 2017.
 	referenceUnixTimeStr = `1136214245`
 )
 
@@ -58,6 +59,7 @@ func TestTimestamp_Unmarshal(t *testing.T) {
 	}{
 		{"Reference", referenceTimeStr, Timestamp{referenceTime}, false, true},
 		{"ReferenceUnix", `1136214245`, Timestamp{referenceTime}, false, true},
+		{"ReferenceFractional", referenceTimeStrFractional, Timestamp{referenceTime}, false, true},
 		{"Empty", emptyTimeStr, Timestamp{}, false, true},
 		{"UnixStart", `0`, Timestamp{unixOrigin}, false, true},
 		{"Mismatch", referenceTimeStr, Timestamp{}, false, false},
