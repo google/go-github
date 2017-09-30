2a5ad00df5775bcd970924003acc5f28615a3227
Elliott Beach <elliott2.71828@gmail.com>
2017-09-30 13:39:09 -0500

    add testHeaders method

--- github/github_test.go
+++ github/github_test.go
@@ -106,6 +106,10 @@ func testHeader(t *testing.T, r *http.Request, header string, want string) {
 	}
 }
 
+func testHeaders(t *testing.T, r *http.Request, header string, want []string) {
+	testHeader(t,r, header, strings.Join(want, ","))
+}
+
 func testURLParseError(t *testing.T, err error) {
 	if err == nil {
 		t.Errorf("Expected error to be returned")
