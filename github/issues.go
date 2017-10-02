9a4ecdb16ad965d696eb09788c86bee7a55b4ced
Dmitri Shuralyov <shurcooL@gmail.com>
2017-10-02 16:59:08 -0400

    Add missing period at the end of a sentence in docs.

--- github/issues.go
+++ github/issues.go
@@ -105,7 +105,7 @@ type IssueListOptions struct {
 }
 
 // PullRequestLinks object is added to the Issue object when it's an issue included
-// in the IssueCommentEvent webhook payload, if the webhook is fired by a comment on a PR
+// in the IssueCommentEvent webhook payload, if the webhook is fired by a comment on a PR.
 type PullRequestLinks struct {
 	URL      *string `json:"url,omitempty"`
 	HTMLURL  *string `json:"html_url,omitempty"`
