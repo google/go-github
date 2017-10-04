b9c5873fbe29fc380e5289a56cd8f03f652a0b65
Jeremy Morris <jeremy.morris@publicisna.com>
2017-10-04 00:30:29 -0400

    Added colons to url path for add and remove repo.

--- github/apps_installation.go
+++ github/apps_installation.go
@@ -55,7 +55,9 @@ func (s *AppsService) ListRepos(ctx context.Context, opt *ListOptions) ([]*Repos
 //
 // GitHub API docs: https://developer.github.com/v3/apps/installations/#add-repository-to-installation
 func (s *AppsService) AddRepo(ctx context.Context, instID int, repoID int) (*Repository, *Response, error) {
-	u := fmt.Sprintf("app/installations/%v/repositories/%v", instID, repoID)
+	// PUT user/installations/:installation_id/repositories/:repository_id
+	u := fmt.Sprintf("app/installations/:%v/repositories/:%v", instID, repoID)
+
 	req, err := s.client.NewRequest("PUT", u, nil)
 	if err != nil {
 		return nil, nil, err
@@ -77,7 +79,8 @@ func (s *AppsService) AddRepo(ctx context.Context, instID int, repoID int) (*Rep
 //
 // GitHub docs: https://developer.github.com/v3/apps/installations/#add-repository-to-installation
 func (s *AppsService) RemoveRepo(ctx context.Context, instID int, repoID int) (*Response, error) {
-	u := fmt.Sprintf("app/installations/%v/repositories/%v", instID, repoID)
+	// DELETE /user/installations/:installation_id/repositories/:repository_id
+	u := fmt.Sprintf("app/installations/:%v/repositories/:%v", instID, repoID)
 	req, err := s.client.NewRequest("DELETE", u, nil)
 	if err != nil {
 		return nil, err
