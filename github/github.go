98b35b2be8ae24fcea8e1f84daebb0f59260d593
Glenn Lewis <gmlewis@google.com>
2017-10-08 21:12:11 -0700

    Bump libraryVersion
    Change-Id: I522f603a39882f7c1219af2571d616127f1b527f

--- github/github.go
+++ github/github.go
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	libraryVersion = "11"
+	libraryVersion = "13"
 	defaultBaseURL = "https://api.github.com/"
 	uploadBaseURL  = "https://uploads.github.com/"
 	userAgent      = "go-github/" + libraryVersion
