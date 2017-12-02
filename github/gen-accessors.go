21d7519172d886133f1bab9148a6b2ce19705676
Sahil Dua <sahildua2305@gmail.com>
2017-12-02 21:04:27 +0100

    Remove generated accessors for Client struct and unexported fields. (#794)
    This change makes gen-accessors.go skip the following structs and fields
    from consideration when generating accessor methods:
    
    1.	Unexported structs and fields are skipped. They're unexported,
    	and shouldn't have accessors.
    2.	Client struct is explicitly blacklistd and skipped. It was not meant
    	to have accessors, and it doesn't need them.
    
    The generated accessors that are removed were added relatively recently,
    and it's not likely they were used since that time.
    
    Resolves #778.

--- github/gen-accessors.go
+++ github/gen-accessors.go
@@ -37,14 +37,18 @@ var (
 
 	sourceTmpl = template.Must(template.New("source").Parse(source))
 
-	// blacklist lists which "struct.method" combos to not generate.
-	blacklist = map[string]bool{
+	// blacklistStructMethod lists "struct.method" combos to skip.
+	blacklistStructMethod = map[string]bool{
 		"RepositoryContent.GetContent":    true,
 		"Client.GetBaseURL":               true,
 		"Client.GetUploadURL":             true,
 		"ErrorResponse.GetResponse":       true,
 		"RateLimitError.GetResponse":      true,
 		"AbuseRateLimitError.GetResponse": true,
+	}
+	// blacklistStruct lists structs to skip.
+	blacklistStruct = map[string]bool{
+		"Client": true,
 	}
 )
 
@@ -95,6 +99,16 @@ func (t *templateData) processAST(f *ast.File) error {
 			if !ok {
 				continue
 			}
+			// Skip unexported identifiers.
+			if !ts.Name.IsExported() {
+				logf("Struct %v is unexported; skipping.", ts.Name)
+				continue
+			}
+			// Check if the struct is blacklisted.
+			if blacklistStruct[ts.Name.Name] {
+				logf("Struct %v is blacklisted; skipping.", ts.Name)
+				continue
+			}
 			st, ok := ts.Type.(*ast.StructType)
 			if !ok {
 				continue
@@ -106,8 +120,14 @@ func (t *templateData) processAST(f *ast.File) error {
 				}
 
 				fieldName := field.Names[0]
-				if key := fmt.Sprintf("%v.Get%v", ts.Name, fieldName); blacklist[key] {
-					logf("Method %v blacklisted; skipping.", key)
+				// Skip unexported identifiers.
+				if !fieldName.IsExported() {
+					logf("Field %v is unexported; skipping.", fieldName)
+					continue
+				}
+				// Check if "struct.method" is blacklisted.
+				if key := fmt.Sprintf("%v.Get%v", ts.Name, fieldName); blacklistStructMethod[key] {
+					logf("Method %v is blacklisted; skipping.", key)
 					continue
 				}
 
@@ -139,7 +159,7 @@ func (t *templateData) dump() error {
 		return nil
 	}
 
-	// Sort getters by ReceiverType.FieldName
+	// Sort getters by ReceiverType.FieldName.
 	sort.Sort(byName(t.Getters))
 
 	var buf bytes.Buffer
@@ -225,7 +245,7 @@ func (t *templateData) addMapType(x *ast.MapType, receiverType, fieldName string
 }
 
 func (t *templateData) addSelectorExpr(x *ast.SelectorExpr, receiverType, fieldName string) {
-	if strings.ToLower(fieldName[:1]) == fieldName[:1] { // non-exported field
+	if strings.ToLower(fieldName[:1]) == fieldName[:1] { // Non-exported field.
 		return
 	}
 
@@ -261,13 +281,13 @@ type templateData struct {
 }
 
 type getter struct {
-	sortVal      string // lower-case version of "ReceiverType.FieldName"
-	ReceiverVar  string // the one-letter variable name to match the ReceiverType
+	sortVal      string // Lower-case version of "ReceiverType.FieldName".
+	ReceiverVar  string // The one-letter variable name to match the ReceiverType.
 	ReceiverType string
 	FieldName    string
 	FieldType    string
 	ZeroValue    string
-	NamedStruct  bool // getter for named struct
+	NamedStruct  bool // Getter for named struct.
 }
 
 type byName []*getter
