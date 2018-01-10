e1d4709b04e7ffa203c478982289285919f56820
Carlos Alexandro Becker <caarlos0@gmail.com>
2018-01-10 08:43:48 -0200

    fix: abuse rate limits handling
    Seems like the URL changed, docs were not updated though:
    
    https://developer.github.com/v3/#abuse-rate-limits
    
    Will also send an email to github support

--- github/github_test.go
+++ github/github_test.go
@@ -657,7 +657,7 @@ func TestDo_rateLimit_abuseRateLimitError(t *testing.T) {
 		// there is no "Retry-After" header.
 		fmt.Fprintln(w, `{
    "message": "You have triggered an abuse detection mechanism and have been temporarily blocked from content creation. Please retry your request again later.",
-   "documentation_url": "https://developer.github.com/v3#abuse-rate-limits"
+   "documentation_url": "https://developer.github.com/v3/#abuse-rate-limits"
 }`)
 	})
 
@@ -687,7 +687,7 @@ func TestDo_rateLimit_abuseRateLimitError_retryAfter(t *testing.T) {
 		w.WriteHeader(http.StatusForbidden)
 		fmt.Fprintln(w, `{
    "message": "You have triggered an abuse detection mechanism ...",
-   "documentation_url": "https://developer.github.com/v3#abuse-rate-limits"
+   "documentation_url": "https://developer.github.com/v3/#abuse-rate-limits"
 }`)
 	})
 
