b6b7371e39e7e9185e76baf597f5f00cd3256996
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2018-02-05 14:03:27 +0530

    Remove preview media type for Team Review Requests
    As the Team Review Requests API is fully supported by GitHub API v3, we
    have removed the preview(custom) media type: thor-preview.
    
    Closes #840

--- github/github.go
+++ github/github.go
@@ -99,9 +99,6 @@ const (
 	// https://developer.github.com/changes/2017-07-17-update-topics-on-repositories/
 	mediaTypeTopicsPreview = "application/vnd.github.mercy-preview+json"
 
-	// https://developer.github.com/changes/2017-07-26-team-review-request-thor-preview/
-	mediaTypeTeamReviewPreview = "application/vnd.github.thor-preview+json"
-
 	// https://developer.github.com/v3/apps/marketplace/
 	mediaTypeMarketplacePreview = "application/vnd.github.valkyrie-preview+json"
 
