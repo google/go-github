7d97b647fc2798d22268428ce068dad5fc2eddc6
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-02-06 17:47:25 -0500

    getting and checking name

--- example/cli/main.go
+++ example/cli/main.go
@@ -9,5 +9,12 @@ func main() {
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
 	if token == "" {
 		fmt.Println("Unauthorized: No token present")
+		return
 	}
+	name := os.Args[1:]
+	if len(name) == 0 {
+		fmt.Println("No name: New repos must be given a name")
+		return
+	}
+	fmt.Println(name)
 }
