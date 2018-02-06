aa84b7d176093a848afeb0f4c8f83d8f54102c1a
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-02-06 18:30:30 -0500

    creating repo

--- example/cli/main.go
+++ example/cli/main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	name := strings.Join(os.Args[1:], "-")
 	if token == "" {
 		fmt.Println("Unauthorized: No token present")
 		return
 	}
-	if len(os.Args[1:]) == 0 {
+	if name == "" {
 		fmt.Println("No name: New repos must be given a name")
 		return
 	}
-	name := os.Args[1:][0]
-	fmt.Println(name)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	repo := &github.Repository{Name: github.String(name)}
+
+	repo, _, err := client.Repositories.Create(ctx, "", repo)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Println("Sucsesfully created new repo: ", *repo.Name)
 }
