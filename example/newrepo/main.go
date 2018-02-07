a1a25739446050f3513139e1a398301409cfa578
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2018-02-06 19:03:50 -0500

    Copyright and introductory comments

--- example/newrepo/main.go
+++ example/newrepo/main.go
@@ -1,3 +1,11 @@
+// Copyright 2018 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// The newrepo command utilizes go-github as a cli tool for
+// creating new repositories.
+
 package main
 
 import (
