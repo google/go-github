b5b3dc43c6af66abec946e852de396c4e8541d1c
Joseph Herlant <herlantj@gmail.com>
2018-04-13 16:09:10 -0700

    Add an example for PullRequestsService.Create

--- github/examples_test.go
+++ github/examples_test.go
@@ -74,3 +74,23 @@ func ExampleUsersService_ListAll() {
 		// Process users...
 	}
 }
+
+func ExamplePullRequestsService_Create() {
+	// In this example we're creating a PR and displaying the HTML url at the end.
+	client := github.NewClient(nil)
+
+	newPR := &github.NewPullRequest{
+		Title:               github.String("My awesome pull request"),
+		Head:                github.String("branch_to_merge"),
+		Base:                github.String("master"),
+		Body:                github.String("This is the description of the PR created with the package `github.com/google/go-github/github`"),
+		MaintainerCanModify: github.Bool(true),
+	}
+
+	pr, _, err := client.PullRequests.Create(context.Background(), "myOrganization", "myRepository", newPR)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("PR created: %s\n", *pr.HTMLURL)
+}
