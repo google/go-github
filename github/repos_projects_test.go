bbfc4f156afbab49d79962491238b728b94aa9ee
jlaportebot <jlaportebot@users.noreply.github.com>
2018-08-13 13:20:17 +0100

    projects: Add missing fields to struct (#969)
    Helps #884.

--- github/repos_projects_test.go
+++ github/repos_projects_test.go
@@ -41,7 +41,7 @@ func TestRepositoriesService_CreateProject(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	input := &ProjectOptions{Name: "Project Name", Body: "Project body."}
+	input := &ProjectOptions{Name: String("Project Name"), Body: String("Project body.")}
 
 	mux.HandleFunc("/repos/o/r/projects", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "POST")
