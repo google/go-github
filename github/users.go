523045e7124c8b7331c5cdbcf2b505cd9e0ed1af
shakeelrao <shakeelrao79@gmail.com>
2018-11-16 09:35:38 -0500

    Add 2FA field to User struct (#1054)
    Fixes #1053.

--- github/users.go
+++ github/users.go
@@ -18,34 +18,35 @@ type UsersService service
 
 // User represents a GitHub user.
 type User struct {
-	Login             *string    `json:"login,omitempty"`
-	ID                *int64     `json:"id,omitempty"`
-	NodeID            *string    `json:"node_id,omitempty"`
-	AvatarURL         *string    `json:"avatar_url,omitempty"`
-	HTMLURL           *string    `json:"html_url,omitempty"`
-	GravatarID        *string    `json:"gravatar_id,omitempty"`
-	Name              *string    `json:"name,omitempty"`
-	Company           *string    `json:"company,omitempty"`
-	Blog              *string    `json:"blog,omitempty"`
-	Location          *string    `json:"location,omitempty"`
-	Email             *string    `json:"email,omitempty"`
-	Hireable          *bool      `json:"hireable,omitempty"`
-	Bio               *string    `json:"bio,omitempty"`
-	PublicRepos       *int       `json:"public_repos,omitempty"`
-	PublicGists       *int       `json:"public_gists,omitempty"`
-	Followers         *int       `json:"followers,omitempty"`
-	Following         *int       `json:"following,omitempty"`
-	CreatedAt         *Timestamp `json:"created_at,omitempty"`
-	UpdatedAt         *Timestamp `json:"updated_at,omitempty"`
-	SuspendedAt       *Timestamp `json:"suspended_at,omitempty"`
-	Type              *string    `json:"type,omitempty"`
-	SiteAdmin         *bool      `json:"site_admin,omitempty"`
-	TotalPrivateRepos *int       `json:"total_private_repos,omitempty"`
-	OwnedPrivateRepos *int       `json:"owned_private_repos,omitempty"`
-	PrivateGists      *int       `json:"private_gists,omitempty"`
-	DiskUsage         *int       `json:"disk_usage,omitempty"`
-	Collaborators     *int       `json:"collaborators,omitempty"`
-	Plan              *Plan      `json:"plan,omitempty"`
+	Login                   *string    `json:"login,omitempty"`
+	ID                      *int64     `json:"id,omitempty"`
+	NodeID                  *string    `json:"node_id,omitempty"`
+	AvatarURL               *string    `json:"avatar_url,omitempty"`
+	HTMLURL                 *string    `json:"html_url,omitempty"`
+	GravatarID              *string    `json:"gravatar_id,omitempty"`
+	Name                    *string    `json:"name,omitempty"`
+	Company                 *string    `json:"company,omitempty"`
+	Blog                    *string    `json:"blog,omitempty"`
+	Location                *string    `json:"location,omitempty"`
+	Email                   *string    `json:"email,omitempty"`
+	Hireable                *bool      `json:"hireable,omitempty"`
+	Bio                     *string    `json:"bio,omitempty"`
+	PublicRepos             *int       `json:"public_repos,omitempty"`
+	PublicGists             *int       `json:"public_gists,omitempty"`
+	Followers               *int       `json:"followers,omitempty"`
+	Following               *int       `json:"following,omitempty"`
+	CreatedAt               *Timestamp `json:"created_at,omitempty"`
+	UpdatedAt               *Timestamp `json:"updated_at,omitempty"`
+	SuspendedAt             *Timestamp `json:"suspended_at,omitempty"`
+	Type                    *string    `json:"type,omitempty"`
+	SiteAdmin               *bool      `json:"site_admin,omitempty"`
+	TotalPrivateRepos       *int       `json:"total_private_repos,omitempty"`
+	OwnedPrivateRepos       *int       `json:"owned_private_repos,omitempty"`
+	PrivateGists            *int       `json:"private_gists,omitempty"`
+	DiskUsage               *int       `json:"disk_usage,omitempty"`
+	Collaborators           *int       `json:"collaborators,omitempty"`
+	TwoFactorAuthentication *bool      `json:"two_factor_authentication,omitempty"`
+	Plan                    *Plan      `json:"plan,omitempty"`
 
 	// API URLs
 	URL               *string `json:"url,omitempty"`
