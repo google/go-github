0cceb33c49eec4234ddf130d9783d9a26850b674
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2018-11-21 21:27:56 -0500

    Update version to v19

--- example/commitpr/main.go
+++ example/commitpr/main.go
@@ -31,7 +31,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v18/github"
+	"github.com/google/go-github/v19/github"
 	"golang.org/x/oauth2"
 )
 
--- example/newrepo/main.go
+++ example/newrepo/main.go
@@ -16,7 +16,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/google/go-github/v18/github"
+	"github.com/google/go-github/v19/github"
 	"golang.org/x/oauth2"
 )
 
