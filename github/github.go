4076fd7418e5e1b04d32ffba6adf5084891952d5
Palash Nigam <npalash25@gmail.com>
2018-12-08 22:52:49 +0530

    IssueEvents: Support Project Card fields (#1066)
    Fixes #1000.

--- github/github.go
+++ github/github.go
@@ -128,6 +128,9 @@ const (
 
 	// https://developer.github.com/changes/2018-02-22-protected-branches-required-signatures/
 	mediaTypeSignaturePreview = "application/vnd.github.zzzax-preview+json"
+
+	// https://developer.github.com/changes/2018-09-05-project-card-events/
+	mediaTypeProjectCardDetailsPreview = "application/vnd.github.starfox-preview+json"
 )
 
 // A Client manages communication with the GitHub API.
