3d39c90aacec6e5c83c28b6f55551ec405f175cf
Wasim Thabraze <wasim@thabraze.me>
2018-12-22 15:08:17 +0530

    Added API to add or update the interactions for a repository

--- github/interactions_repos.go
+++ github/interactions_repos.go
@@ -32,3 +32,26 @@ func (s *InteractionsService) GetInteractions(ctx context.Context, owner string,
 
 	return repositoryInteractions, resp, nil
 }
+
+// UpdateInteractions adds or updates the interaction restrictions for a repository.
+//
+// GitHub API docs: https://developer.github.com/v3/interactions/repos/#add-or-update-interaction-restrictions-for-a-repository
+func (s *InteractionsService) UpdateInteractions(ctx context.Context, owner string, repo string, interaction *Interaction) (*Interaction, *Response, error) {
+	u := fmt.Sprintf("repos/%v/%v/interaction-limits", owner, repo)
+	req, err := s.client.NewRequest("PUT", u, interaction)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// TODO: remove custom Accept header when this API fully launches.
+	req.Header.Set("Accept", mediaTypeRepositoryInteractionsPreview)
+
+	repositoryInteractions := new(Interaction)
+
+	resp, err := s.client.Do(ctx, req, repositoryInteractions)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return repositoryInteractions, resp, nil
+}
--- github/interactions_repos_test.go
+++ github/interactions_repos_test.go
@@ -7,6 +7,7 @@ package github
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -24,6 +25,35 @@ func TestInteractionsService_GetInteractions(t *testing.T) {
 	})
 
 	repoInteractions, _, err := client.Interactions.GetInteractions(context.Background(), "o", "r")
+	if err != nil {
+		t.Errorf("Interactions.GetInteractions returned error: %v", err)
+	}
+
+	want := &Interaction{Origin: String("repository")}
+	if !reflect.DeepEqual(repoInteractions, want) {
+		t.Errorf("Interactions.GetInteractions returned %+v, want %+v", repoInteractions, want)
+	}
+}
+
+func TestInteractionsService_UpdateInteractions(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	input := &Interaction{Limit: String("existing_users")}
+
+	mux.HandleFunc("/repos/o/r/interaction-limits", func(w http.ResponseWriter, r *http.Request) {
+		v := new(Interaction)
+		json.NewDecoder(r.Body).Decode(v)
+
+		testMethod(t, r, "PUT")
+		testHeader(t, r, "Accept", mediaTypeRepositoryInteractionsPreview)
+		if !reflect.DeepEqual(v, input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+		fmt.Fprint(w, `{"origin":"repository"}`)
+	})
+
+	repoInteractions, _, err := client.Interactions.UpdateInteractions(context.Background(), "o", "r", input)
 	if err != nil {
 		t.Errorf("Interactions.GetInteractions returned error: %v", err)
 	}
