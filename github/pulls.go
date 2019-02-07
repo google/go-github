fbfacb4e2ba6cc2c9ffb89ef30af7e9cc62c9bff
Felix Geisendörfer <felix@debuggable.com>
2019-02-07 03:19:41 +0100

    Add missing PullRequest.ReviewComments field (#1113)

--- github/pulls.go
+++ github/pulls.go
@@ -52,6 +52,7 @@ type PullRequest struct {
 	CommentsURL         *string    `json:"comments_url,omitempty"`
 	ReviewCommentsURL   *string    `json:"review_comments_url,omitempty"`
 	ReviewCommentURL    *string    `json:"review_comment_url,omitempty"`
+	ReviewComments      *int       `json:"review_comments,omitempty"`
 	Assignee            *User      `json:"assignee,omitempty"`
 	Assignees           []*User    `json:"assignees,omitempty"`
 	Milestone           *Milestone `json:"milestone,omitempty"`
