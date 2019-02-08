f7ab81d2087466c630355b2c1ee20f79c9a68d2a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-02-07 22:52:01 -0500

    Bump v22 to v23

--- example/newrepo/main.go
+++ example/newrepo/main.go
@@ -16,7 +16,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/google/go-github/v22/github"
+	"github.com/google/go-github/v23/github"
 	"golang.org/x/oauth2"
 )
 
--- example/simple/main.go
+++ example/simple/main.go
@@ -12,7 +12,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/google/go-github/v22/github"
+	"github.com/google/go-github/v23/github"
 )
 
 // Fetch all the public organizations' membership of a user.
