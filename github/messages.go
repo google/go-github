8d1febdbbf1d1a088cb7f181ff45e552791f1557
Jordan Brockopp <Jordan.Brockopp@target.com>
2019-02-25 08:11:06 -0600

    only validate signature if secret key is not nil

--- github/messages.go
+++ github/messages.go
@@ -130,6 +130,8 @@ func messageMAC(signature string) ([]byte, func() hash.Hash, error) {
 // The Content-Type header of the payload can be "application/json" or "application/x-www-form-urlencoded".
 // If the Content-Type is neither then an error is returned.
 // secretKey is the GitHub Webhook secret message.
+// If your webhook does not contain a secret message, you can pass nil. This
+// is intended for local development purposes only and all webhooks should contain a secret message.
 //
 // Example usage:
 //
@@ -175,10 +177,15 @@ func ValidatePayload(r *http.Request, secretKey []byte) (payload []byte, err err
 		return nil, fmt.Errorf("Webhook request has unsupported Content-Type %q", ct)
 	}
 
-	sig := r.Header.Get(signatureHeader)
-	if err := ValidateSignature(sig, body, secretKey); err != nil {
-		return nil, err
+	// Only validate the signature if a secret message exists. This is intended for
+	// local development only and all webhooks should contain a secret message.
+	if secretKey != nil {
+		sig := r.Header.Get(signatureHeader)
+		if err := ValidateSignature(sig, body, secretKey); err != nil {
+			return nil, err
+		}
 	}
+
 	return payload, nil
 }
 
