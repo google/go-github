83cad2fc0eae18b43f067911d30339f44aa970c4
jlaportebot <jlaportebot@users.noreply.github.com>
2019-02-27 23:31:12 +0000

    Fix Webhook creation. (#1117)

--- github/repos_hooks_test.go
+++ github/repos_hooks_test.go
@@ -25,7 +25,7 @@ func TestRepositoriesService_CreateHook(t *testing.T) {
 		json.NewDecoder(r.Body).Decode(v)
 
 		testMethod(t, r, "POST")
-		want := &createHookRequest{}
+		want := &createHookRequest{Name: "web"}
 		if !reflect.DeepEqual(v, want) {
 			t.Errorf("Request body = %+v, want %+v", v, want)
 		}
