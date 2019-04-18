e1318e4db0f4197ca632c158d23dccc252aa62fb
jlaportebot <jlaportebot@users.noreply.github.com>
2019-04-18 06:45:47 -0400

    Bump version

--- example/basicauth/main.go
+++ example/basicauth/main.go
@@ -16,7 +16,7 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/google/go-github/v24/github"
+	"github.com/google/go-github/v25/github"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
--- example/migrations/main.go
+++ example/migrations/main.go
@@ -12,7 +12,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/google/go-github/v24/github"
+	"github.com/google/go-github/v25/github"
 	"golang.org/x/oauth2"
 )
 
