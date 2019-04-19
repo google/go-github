6ac0e7d7ccd2cd36a834738ac9862570138ff3f4
haya14busa <haya14busa@gmail.com>
2019-04-19 13:33:11 +0000

    Use go module in test directory
    It removed github.com/google/go-github v17.0.0+incompatible dependencies
    in go.mod.

--- test/integration/activity_test.go
+++ test/integration/activity_test.go
@@ -11,7 +11,7 @@ import (
 	"context"
 	"testing"
 
-	"github.com/google/go-github/github"
+	"github.com/google/go-github/v25/github"
 )
 
 const (
--- test/integration/repos_test.go
+++ test/integration/repos_test.go
@@ -13,7 +13,7 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/google/go-github/github"
+	"github.com/google/go-github/v25/github"
 )
 
 func TestRepositories_CRUD(t *testing.T) {
