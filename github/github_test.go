5182c7258e950cc464719fc3d5835efa82a1a3af
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2019-05-26 17:21:04 -0400

    Add NewClient test

--- github/github_test.go
+++ github/github_test.go
@@ -172,6 +172,11 @@ func TestNewClient(t *testing.T) {
 	if got, want := c.UserAgent, userAgent; got != want {
 		t.Errorf("NewClient UserAgent is %v, want %v", got, want)
 	}
+
+	c2 := NewClient(nil)
+	if c.client == c2.client {
+		t.Error("NewClient returned same http.Clients, but they should differ")
+	}
 }
 
 func TestNewEnterpriseClient(t *testing.T) {
