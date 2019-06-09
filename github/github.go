b5b0b2977ea1c1959b79c72b8ea5f0daf1dbaa91
Carlos Panato <ctadeu@gmail.com>
2019-06-09 14:37:16 +0200

    add automated-security-fixes endpoint
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- github/github.go
+++ github/github.go
@@ -137,6 +137,9 @@ const (
 
 	// https://developer.github.com/changes/2019-04-24-vulnerability-alerts/
 	mediaTypeRequiredVulnerabilityAlertsPreview = "application/vnd.github.dorian-preview+json"
+
+	// https://developer.github.com/changes/2019-06-04-automated-security-fixes/
+	mediaTypeRequiredAutomatedSecurityFixesPreview = "application/vnd.github.london-preview+json"
 )
 
 // A Client manages communication with the GitHub API.
