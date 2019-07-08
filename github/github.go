40b4c6296f87ce9a98ead85cfa8dcb588c70bb0c
jlaportebot <jlaportebot@users.noreply.github.com>
2019-07-09 07:27:19 +0900

    Add repository creation permissions field (#1213)

--- github/github.go
+++ github/github.go
@@ -150,6 +150,9 @@ const (
 
 	// https://developer.github.com/changes/2019-06-12-team-sync/
 	mediaTypeTeamSyncPreview = "application/vnd.github.team-sync-preview+json"
+
+	// https://developer.github.com/v3/previews/#repository-creation-permissions
+	mediaTypeMemberAllowedRepoCreationTypePreview = "application/vnd.github.surtur-preview+json"
 )
 
 // A Client manages communication with the GitHub API.
