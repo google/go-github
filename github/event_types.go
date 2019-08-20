98800865a167958581e85c675a552fdbd779aa0c
jlaportebot <jlaportebot@users.noreply.github.com>
2019-08-20 12:14:35 +0900

    Add PullsURL to PushEventRepository (#1253)

--- github/event_types.go
+++ github/event_types.go
@@ -707,6 +707,7 @@ type PushEventRepository struct {
 	PushedAt        *Timestamp `json:"pushed_at,omitempty"`
 	UpdatedAt       *Timestamp `json:"updated_at,omitempty"`
 	Homepage        *string    `json:"homepage,omitempty"`
+	PullsURL        *string    `json:"pulls_url,omitempty"`
 	Size            *int       `json:"size,omitempty"`
 	StargazersCount *int       `json:"stargazers_count,omitempty"`
 	WatchersCount   *int       `json:"watchers_count,omitempty"`
