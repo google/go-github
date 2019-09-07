2df39e5c765a2e9eaaa46a429acc484e688a1df6
Michael Cristina <mcristina422@users.noreply.github.com>
2019-09-07 18:55:57 -0500

    Remove blob_href from check annotations (#1242)

--- github/checks.go
+++ github/checks.go
@@ -51,7 +51,6 @@ type CheckRunOutput struct {
 // CheckRunAnnotation represents an annotation object for a CheckRun output.
 type CheckRunAnnotation struct {
 	Path            *string `json:"path,omitempty"`
-	BlobHRef        *string `json:"blob_href,omitempty"`
 	StartLine       *int    `json:"start_line,omitempty"`
 	EndLine         *int    `json:"end_line,omitempty"`
 	StartColumn     *int    `json:"start_column,omitempty"`
