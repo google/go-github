a01ea50f2fc0ce93dad509f31ed1b279cc07b424
Daoq <masseto2002@gmail.com>
2019-09-18 08:18:10 +0800

    Add default enterprise suffix when creating an enterprise method (#958) (#1165)
    Fixes #958.

--- github/github.go
+++ github/github.go
@@ -300,13 +300,16 @@ func NewClient(httpClient *http.Client) *Client {
 }
 
 // NewEnterpriseClient returns a new GitHub API client with provided
-// base URL and upload URL (often the same URL).
-// If either URL does not have a trailing slash, one is added automatically.
-// If a nil httpClient is provided, a new http.Client will be used.
+// base URL and upload URL (often the same URL and is your GitHub Enterprise hostname).
+// If either URL does not have the suffix "/api/v3/", it will be added automatically.
+// If a nil httpClient is provided, http.DefaultClient will be used.
 //
 // Note that NewEnterpriseClient is a convenience helper only;
 // its behavior is equivalent to using NewClient, followed by setting
 // the BaseURL and UploadURL fields.
+//
+// Another important thing is that by default, the GitHub Enterprise URL format
+// should be http(s)://[hostname]/api/v3 or you will always receive the 406 status code.
 func NewEnterpriseClient(baseURL, uploadURL string, httpClient *http.Client) (*Client, error) {
 	baseEndpoint, err := url.Parse(baseURL)
 	if err != nil {
@@ -315,6 +318,9 @@ func NewEnterpriseClient(baseURL, uploadURL string, httpClient *http.Client) (*C
 	if !strings.HasSuffix(baseEndpoint.Path, "/") {
 		baseEndpoint.Path += "/"
 	}
+	if !strings.HasSuffix(baseEndpoint.Path, "/api/v3/") {
+		baseEndpoint.Path += "api/v3/"
+	}
 
 	uploadEndpoint, err := url.Parse(uploadURL)
 	if err != nil {
@@ -322,6 +328,9 @@ func NewEnterpriseClient(baseURL, uploadURL string, httpClient *http.Client) (*C
 	}
 	if !strings.HasSuffix(uploadEndpoint.Path, "/") {
 		uploadEndpoint.Path += "/"
+	}
+	if !strings.HasSuffix(uploadEndpoint.Path, "/api/v3/") {
+		uploadEndpoint.Path += "api/v3/"
 	}
 
 	c := NewClient(httpClient)
