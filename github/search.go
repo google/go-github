740806ae9dd43b6f68f872c299eedee17bcfa717
Quang Le Hong <iamquang95@gmail.com>
2019-10-06 20:59:32 +0700

    Add document for query of search functions (#1300)
    * Add document for search query
    
    * Add more detail of using search query

--- github/search.go
+++ github/search.go
@@ -24,6 +24,11 @@ import (
 // will search for such issues, sorting by creation date in ascending order
 // (i.e., oldest first).
 //
+// If query includes multiple conditions, it MUST NOT include "+" as the condition separator.
+// You have to use " " as the separator instead.
+// For example, querying with "language:c++" and "leveldb", then query should be
+// "language:c++ leveldb" but not "language:c+++leveldb".
+//
 // GitHub API docs: https://developer.github.com/v3/search/
 type SearchService service
 
