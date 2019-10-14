e11e1607b4edbe638f4ee2b7290b116e973aa641
Will Norris <will@willnorris.com>
2019-10-14 16:01:01 -0700

    add experimental scrape package
    This package adds a very experimental client for accessing GitHub data
    via screen scraping.  It is exempt from any stability promise that would
    otherwise be implied by the go-github version number.

---
+++ scrape/apps.go
@@ -0,0 +1,125 @@
+// Copyright 2013 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// apps.go contains functions for accessing data about applications installed
+// on a GitHub organization.
+
+package scrape
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+// AppRestrictionsEnabled returns whether the specified organization has
+// restricted third-party application access.
+func (c *Client) AppRestrictionsEnabled(org string) (bool, error) {
+	u := fmt.Sprintf("https://github.com/organizations/%s/settings/oauth_application_policy", org)
+	resp, err := c.Client.Get(u)
+	if err != nil {
+		return false, fmt.Errorf("error fetching url %q: %v", u, err)
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		return false, fmt.Errorf("unable to fetch URL %q", u)
+	}
+
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return false, fmt.Errorf("error parsing response: %v", err)
+	}
+
+	s := doc.Find(".oauth-application-whitelist svg").First()
+	if s.Length() == 0 {
+		return false, errors.New("unable to find expected markup")
+	}
+
+	if s.HasClass("octicon-check") {
+		return true, nil
+	}
+	if s.HasClass("octicon-alert") {
+		return false, nil
+	}
+
+	return false, errors.New("unable to find expected markup")
+}
+
+// ListOAuthApps lists the reviewed OAuth Applications for the
+// specified organization (whether approved or denied).
+func (c *Client) ListOAuthApps(org string) ([]OAuthApp, error) {
+	u := fmt.Sprintf("https://github.com/organizations/%s/settings/oauth_application_policy", org)
+	resp, err := c.Client.Get(u)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching url %q: %v", u, err)
+	}
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, fmt.Errorf("unable to fetch URL %q", u)
+	}
+
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing response: %v", err)
+	}
+
+	var apps []OAuthApp
+	doc.Find(".oauth-application-whitelist ul > li").Each(func(i int, s *goquery.Selection) {
+		var app OAuthApp
+		app.Name = s.Find(".request-info strong").First().Text()
+		app.Description = s.Find(".request-info .application-description").Text()
+
+		if editURL, ok := s.Find(".request-indicator a.edit-link").Attr("href"); ok {
+			app.ID = intFromLastPathSegment(editURL)
+		}
+
+		if r := s.Find(".request-indicator .requestor"); r.Length() > 0 {
+			app.State = OAuthAppRequested
+			app.RequestedBy = r.Text()
+			if editURL, ok := s.Find(".request-indicator a").Last().Attr("href"); ok {
+				app.ID = intFromLastPathSegment(editURL)
+			}
+		} else if r := s.Find(".request-indicator .approved-request"); r.Length() > 0 {
+			app.State = OAuthAppApproved
+		} else if r := s.Find(".request-indicator .denied-request"); r.Length() > 0 {
+			app.State = OAuthAppDenied
+		}
+		apps = append(apps, app)
+	})
+
+	return apps, nil
+}
+
+func intFromLastPathSegment(s string) int {
+	seg := strings.Split(s, "/")
+	if len(seg) > 0 {
+		i, _ := strconv.Atoi(seg[len(seg)-1])
+		return i
+	}
+	return 0
+}
+
+// OAuthAppReviewState indicates the current state of a requested OAuth Application.
+type OAuthAppReviewState int
+
+const (
+	// OAuthAppRequested indicates access has been requested, but not reviewed
+	OAuthAppRequested OAuthAppReviewState = iota + 1
+	// OAuthAppApproved indicates access has been approved
+	OAuthAppApproved
+	// OAuthAppDenied indicates access has been denied
+	OAuthAppDenied
+)
+
+// OAuthApp represents an OAuth application that has been reviewed for access to organization data.
+type OAuthApp struct {
+	ID          int
+	Name        string
+	Description string
+	State       OAuthAppReviewState
+	RequestedBy string
+}
