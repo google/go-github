29528230daec3d6965fc3b2fb81c0df90b80a153
jlaportebot <jlaportebot@users.noreply.github.com>
2019-12-05 07:47:23 -0500

    Add create and delete endpoints (#1315)
    Fixes #1314.

--- github/admin_users.go
+++ github/admin_users.go
@@ -7,6 +7,7 @@ package github
 
 import (
 	"context"
+	"fmt"
 )
 
 // createUserRequest is a subset of User and is used internally
@@ -46,6 +47,77 @@ func (s *AdminService) CreateUser(ctx context.Context, login, email string) (*Us
 // GitHub Enterprise API docs: https://developer.github.com/enterprise/v3/enterprise-admin/users/#delete-a-user
 func (s *AdminService) DeleteUser(ctx context.Context, username string) (*Response, error) {
 	u := "admin/users/" + username
+
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.client.Do(ctx, req, nil)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// ImpersonateUserOptions represents the scoping for the OAuth token.
+type ImpersonateUserOptions struct {
+	Scopes []string `json:"scopes,omitempty"`
+}
+
+// OAuthAPP represents the GitHub Site Administrator OAuth app.
+type OAuthAPP struct {
+	URL      *string `json:"url,omitempty"`
+	Name     *string `json:"name,omitempty"`
+	ClientID *string `json:"client_id,omitempty"`
+}
+
+func (s OAuthAPP) String() string {
+	return Stringify(s)
+}
+
+// UserAuthorization represents the impersonation response.
+type UserAuthorization struct {
+	ID             *int64     `json:"id,omitempty"`
+	URL            *string    `json:"url,omitempty"`
+	Scopes         []string   `json:"scopes,omitempty"`
+	Token          *string    `json:"token,omitempty"`
+	TokenLastEight *string    `json:"token_last_eight,omitempty"`
+	HashedToken    *string    `json:"hashed_token,omitempty"`
+	App            *OAuthAPP  `json:"app,omitempty"`
+	Note           *string    `json:"note,omitempty"`
+	NoteURL        *string    `json:"note_url,omitempty"`
+	UpdatedAt      *Timestamp `json:"updated_at,omitempty"`
+	CreatedAt      *Timestamp `json:"created_at,omitempty"`
+	Fingerprint    *string    `json:"fingerprint,omitempty"`
+}
+
+// CreateUserImpersonation creates an impersonation OAuth token.
+//
+// GitHub Enterprise API docs: https://developer.github.com/enterprise/v3/enterprise-admin/users/#create-an-impersonation-oauth-token
+func (s *AdminService) CreateUserImpersonation(ctx context.Context, username string, opt *ImpersonateUserOptions) (*UserAuthorization, *Response, error) {
+	u := fmt.Sprintf("admin/users/%s/authorizations", username)
+
+	req, err := s.client.NewRequest("POST", u, opt)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	a := new(UserAuthorization)
+	resp, err := s.client.Do(ctx, req, a)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return a, resp, nil
+}
+
+// DeleteUserImpersonation deletes an impersonation OAuth token.
+//
+// GitHub Enterprise API docs: https://developer.github.com/enterprise/v3/enterprise-admin/users/#delete-an-impersonation-oauth-token
+func (s *AdminService) DeleteUserImpersonation(ctx context.Context, username string) (*Response, error) {
+	u := fmt.Sprintf("admin/users/%s/authorizations", username)
 
 	req, err := s.client.NewRequest("DELETE", u, nil)
 	if err != nil {
--- github/admin_users_test.go
+++ github/admin_users_test.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestAdminUsers_Create(t *testing.T) {
@@ -53,5 +54,76 @@ func TestAdminUsers_Delete(t *testing.T) {
 	_, err := client.Admin.DeleteUser(context.Background(), "github")
 	if err != nil {
 		t.Errorf("Admin.DeleteUser returned error: %v", err)
+	}
+}
+
+func TestUserImpersonation_Create(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/admin/users/github/authorizations", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "POST")
+		testBody(t, r, `{"scopes":["repo"]}`+"\n")
+		fmt.Fprint(w, `{"id": 1234,
+		"url": "https://git.company.com/api/v3/authorizations/1234",
+		"app": {
+		  "name": "GitHub Site Administrator",
+		  "url": "https://developer.github.com/v3/enterprise/users/",
+		  "client_id": "1234"
+		},
+		"token": "1234",
+		"hashed_token": "1234",
+		"token_last_eight": "1234",
+		"note": null,
+		"note_url": null,
+		"created_at": "2018-01-01T00:00:00Z",
+		"updated_at": "2018-01-01T00:00:00Z",
+		"scopes": [
+		  "repo"
+		],
+		"fingerprint": null}`)
+	})
+
+	opt := &ImpersonateUserOptions{Scopes: []string{"repo"}}
+	auth, _, err := client.Admin.CreateUserImpersonation(context.Background(), "github", opt)
+	if err != nil {
+		t.Errorf("Admin.CreateUserImpersonation returned error: %v", err)
+	}
+
+	date := Timestamp{Time: time.Date(2018, time.January, 1, 0, 0, 0, 0, time.UTC)}
+	want := &UserAuthorization{
+		ID:  Int64(1234),
+		URL: String("https://git.company.com/api/v3/authorizations/1234"),
+		App: &OAuthAPP{
+			Name:     String("GitHub Site Administrator"),
+			URL:      String("https://developer.github.com/v3/enterprise/users/"),
+			ClientID: String("1234"),
+		},
+		Token:          String("1234"),
+		HashedToken:    String("1234"),
+		TokenLastEight: String("1234"),
+		Note:           nil,
+		NoteURL:        nil,
+		CreatedAt:      &date,
+		UpdatedAt:      &date,
+		Scopes:         []string{"repo"},
+		Fingerprint:    nil,
+	}
+	if !reflect.DeepEqual(auth, want) {
+		t.Errorf("Admin.CreateUserImpersonation returned %+v, want %+v", auth, want)
+	}
+}
+
+func TestUserImpersonation_Delete(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/admin/users/github/authorizations", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	_, err := client.Admin.DeleteUserImpersonation(context.Background(), "github")
+	if err != nil {
+		t.Errorf("Admin.DeleteUserImpersonation returned error: %v", err)
 	}
 }
