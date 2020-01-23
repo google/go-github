eddabd208981d0e872104369ab633555308c29a0
Alex Orr <alexorr.cse@gmail.com>
2020-01-22 23:19:23 -0800

    fix: add functions for team discussion comments endpoints

--- github/teams_discussion_comments.go
+++ github/teams_discussion_comments.go
@@ -39,12 +39,13 @@ type DiscussionCommentListOptions struct {
 	Direction string `url:"direction,omitempty"`
 }
 
-// ListComments lists all comments on a team discussion.
+// ListCommentsByID lists all comments on a team discussion
+// given a team ID and organization ID.
 // Authenticated user must grant read:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#list-comments
-func (s *TeamsService) ListComments(ctx context.Context, teamID int64, discussionNumber int, options *DiscussionCommentListOptions) ([]*DiscussionComment, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v/comments", teamID, discussionNumber)
+func (s *TeamsService) ListCommentsByID(ctx context.Context, orgID, teamID int64, discussionNumber int, options *DiscussionCommentListOptions) ([]*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v/comments", orgID, teamID, discussionNumber)
 	u, err := addOptions(u, options)
 	if err != nil {
 		return nil, nil, err
@@ -64,12 +65,39 @@ func (s *TeamsService) ListComments(ctx context.Context, teamID int64, discussio
 	return comments, resp, nil
 }
 
-// GetComment gets a specific comment on a team discussion.
+// ListCommentsByName lists all comments on a team discussion
+// given a team slug and organization name.
+// Authenticated user must grant read:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#list-comments
+func (s *TeamsService) ListCommentsByName(ctx context.Context, org, slug string, discussionNumber int, options *DiscussionCommentListOptions) ([]*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v/comments", org, slug, discussionNumber)
+	u, err := addOptions(u, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var comments []*DiscussionComment
+	resp, err := s.client.Do(ctx, req, &comments)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return comments, resp, nil
+}
+
+// GetComment gets a specific comment on a team discussion
+// given a team ID and organization ID.
 // Authenticated user must grant read:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#get-a-single-comment
-func (s *TeamsService) GetComment(ctx context.Context, teamID int64, discussionNumber, commentNumber int) (*DiscussionComment, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v/comments/%v", teamID, discussionNumber, commentNumber)
+func (s *TeamsService) GetCommentByID(ctx context.Context, orgID, teamID int64, discussionNumber, commentNumber int) (*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v/comments/%v", orgID, teamID, discussionNumber, commentNumber)
 	req, err := s.client.NewRequest("GET", u, nil)
 	if err != nil {
 		return nil, nil, err
@@ -84,12 +112,34 @@ func (s *TeamsService) GetComment(ctx context.Context, teamID int64, discussionN
 	return discussionComment, resp, nil
 }
 
-// CreateComment creates a new discussion post on a team discussion.
+// GetComment gets a specific comment on a team discussion 
+// given a team slug and organization name.
+// Authenticated user must grant read:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#get-a-single-comment
+func (s *TeamsService) GetCommentByName(ctx context.Context, org, slug string, discussionNumber, commentNumber int) (*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v/comments/%v", org, slug, discussionNumber, commentNumber)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	discussionComment := &DiscussionComment{}
+	resp, err := s.client.Do(ctx, req, discussionComment)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return discussionComment, resp, nil
+}
+
+// CreateCommentByID creates a new discussion post on a team discussion
+// given a team ID and organization ID.
 // Authenticated user must grant write:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#create-a-comment
-func (s *TeamsService) CreateComment(ctx context.Context, teamID int64, discsusionNumber int, comment DiscussionComment) (*DiscussionComment, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v/comments", teamID, discsusionNumber)
+func (s *TeamsService) CreateCommentByID(ctx context.Context, orgID, teamID int64, discsusionNumber int, comment DiscussionComment) (*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v/comments", orgID, teamID, discsusionNumber)
 	req, err := s.client.NewRequest("POST", u, comment)
 	if err != nil {
 		return nil, nil, err
@@ -104,13 +154,35 @@ func (s *TeamsService) CreateComment(ctx context.Context, teamID int64, discsusi
 	return discussionComment, resp, nil
 }
 
-// EditComment edits the body text of a discussion comment.
+// CreateCommentByName creates a new discussion post on a team discussion
+// given a team slug and organization name.
+// Authenticated user must grant write:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#create-a-comment
+func (s *TeamsService) CreateCommentByName(ctx context.Context, org, slug string, discsusionNumber int, comment DiscussionComment) (*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v/comments", org, slug, discsusionNumber)
+	req, err := s.client.NewRequest("POST", u, comment)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	discussionComment := &DiscussionComment{}
+	resp, err := s.client.Do(ctx, req, discussionComment)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return discussionComment, resp, nil
+}
+
+// EditCommentByID edits the body text of a discussion comment
+// given a team ID and organization ID.
 // Authenticated user must grant write:discussion scope.
 // User is allowed to edit body of a comment only.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#edit-a-comment
-func (s *TeamsService) EditComment(ctx context.Context, teamID int64, discussionNumber, commentNumber int, comment DiscussionComment) (*DiscussionComment, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v/comments/%v", teamID, discussionNumber, commentNumber)
+func (s *TeamsService) EditCommentByID(ctx context.Context, orgID, teamID int64, discussionNumber, commentNumber int, comment DiscussionComment) (*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v/comments/%v", orgID, teamID, discussionNumber, commentNumber)
 	req, err := s.client.NewRequest("PATCH", u, comment)
 	if err != nil {
 		return nil, nil, err
@@ -125,12 +197,50 @@ func (s *TeamsService) EditComment(ctx context.Context, teamID int64, discussion
 	return discussionComment, resp, nil
 }
 
-// DeleteComment deletes a comment on a team discussion.
+// EditCommentByName edits the body text of a discussion comment
+// given a team slug and organization name.
+// Authenticated user must grant write:discussion scope.
+// User is allowed to edit body of a comment only.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#edit-a-comment
+func (s *TeamsService) EditCommentByName(ctx context.Context, org, slug string, discussionNumber, commentNumber int, comment DiscussionComment) (*DiscussionComment, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v/comments/%v", org, slug, discussionNumber, commentNumber)
+	req, err := s.client.NewRequest("PATCH", u, comment)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	discussionComment := &DiscussionComment{}
+	resp, err := s.client.Do(ctx, req, discussionComment)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return discussionComment, resp, nil
+}
+
+// DeleteCommentByID deletes a comment on a team discussion
+// given a team ID and organiation ID.
 // Authenticated user must grant write:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#delete-a-comment
-func (s *TeamsService) DeleteComment(ctx context.Context, teamID int64, discussionNumber, commentNumber int) (*Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v/comments/%v", teamID, discussionNumber, commentNumber)
+func (s *TeamsService) DeleteCommentByID(ctx context.Context, orgID, teamID int64, discussionNumber, commentNumber int) (*Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v/comments/%v", orgID, teamID, discussionNumber, commentNumber)
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// DeleteCommentByName deletes a comment on a team discussion
+// given a team slug and organization name.
+// Authenticated user must grant write:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussion_comments/#delete-a-comment
+func (s *TeamsService) DeleteCommentByName(ctx context.Context, org, slug string, discussionNumber, commentNumber int) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v/comments/%v", org, slug, discussionNumber, commentNumber)
 	req, err := s.client.NewRequest("DELETE", u, nil)
 	if err != nil {
 		return nil, err
--- github/teams_discussion_comments_test.go
+++ github/teams_discussion_comments_test.go
@@ -15,11 +15,11 @@ import (
 	"time"
 )
 
-func TestTeamsService_ListComments(t *testing.T) {
+func TestTeamsService_ListCommentsByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/teams/2/discussions/3/comments", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/organizations/1/team/2/discussions/3/comments", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
 		testFormValues(t, r, values{
 			"direction": "desc",
@@ -61,9 +61,9 @@ func TestTeamsService_ListComments(t *testing.T) {
 			]`)
 	})
 
-	comments, _, err := client.Teams.ListComments(context.Background(), 2, 3, &DiscussionCommentListOptions{"desc"})
+	comments, _, err := client.Teams.ListCommentsByID(context.Background(), 1, 2, 3, &DiscussionCommentListOptions{"desc"})
 	if err != nil {
-		t.Errorf("Teams.ListComments returned error: %v", err)
+		t.Errorf("Teams.ListCommentsByID returned error: %v", err)
 	}
 
 	want := []*DiscussionComment{
@@ -101,37 +101,147 @@ func TestTeamsService_ListComments(t *testing.T) {
 		},
 	}
 	if !reflect.DeepEqual(comments, want) {
-		t.Errorf("Teams.ListComments returned %+v, want %+v", comments, want)
+		t.Errorf("Teams.ListCommentsByID returned %+v, want %+v", comments, want)
 	}
 }
 
-func TestTeamsService_GetComment(t *testing.T) {
+func TestTeamsService_ListCommentsByName(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/teams/2/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3/comments", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"direction": "desc",
+		})
+		fmt.Fprintf(w,
+			`[
+				{
+					"author": {
+						"login": "author",
+						"id": 0,
+						"avatar_url": "https://avatars1.githubusercontent.com/u/0?v=4",
+						"gravatar_id": "",
+						"url": "https://api.github.com/users/author",
+						"html_url": "https://github.com/author",
+						"followers_url": "https://api.github.com/users/author/followers",
+						"following_url": "https://api.github.com/users/author/following{/other_user}",
+						"gists_url": "https://api.github.com/users/author/gists{/gist_id}",
+						"starred_url": "https://api.github.com/users/author/starred{/owner}{/repo}",
+						"subscriptions_url": "https://api.github.com/users/author/subscriptions",
+						"organizations_url": "https://api.github.com/users/author/orgs",
+						"repos_url": "https://api.github.com/users/author/repos",
+						"events_url": "https://api.github.com/users/author/events{/privacy}",
+						"received_events_url": "https://api.github.com/users/author/received_events",
+						"type": "User",
+						"site_admin": false
+					},
+					"body": "comment",
+					"body_html": "<p>comment</p>",
+					"body_version": "version",
+					"created_at": "2018-01-01T00:00:00Z",
+					"last_edited_at": null,
+					"discussion_url": "https://api.github.com/teams/2/discussions/3",
+					"html_url": "https://github.com/orgs/1/teams/2/discussions/3/comments/4",
+					"node_id": "node",
+					"number": 4,
+					"updated_at": "2018-01-01T00:00:00Z",
+					"url": "https://api.github.com/teams/2/discussions/3/comments/4"
+				}
+			]`)
+	})
+
+	comments, _, err := client.Teams.ListCommentsByName(context.Background(), "o", "s", 3, &DiscussionCommentListOptions{"desc"})
+	if err != nil {
+		t.Errorf("Teams.ListCommentsByName returned error: %v", err)
+	}
+
+	want := []*DiscussionComment{
+		{
+			Author: &User{
+				Login:             String("author"),
+				ID:                Int64(0),
+				AvatarURL:         String("https://avatars1.githubusercontent.com/u/0?v=4"),
+				GravatarID:        String(""),
+				URL:               String("https://api.github.com/users/author"),
+				HTMLURL:           String("https://github.com/author"),
+				FollowersURL:      String("https://api.github.com/users/author/followers"),
+				FollowingURL:      String("https://api.github.com/users/author/following{/other_user}"),
+				GistsURL:          String("https://api.github.com/users/author/gists{/gist_id}"),
+				StarredURL:        String("https://api.github.com/users/author/starred{/owner}{/repo}"),
+				SubscriptionsURL:  String("https://api.github.com/users/author/subscriptions"),
+				OrganizationsURL:  String("https://api.github.com/users/author/orgs"),
+				ReposURL:          String("https://api.github.com/users/author/repos"),
+				EventsURL:         String("https://api.github.com/users/author/events{/privacy}"),
+				ReceivedEventsURL: String("https://api.github.com/users/author/received_events"),
+				Type:              String("User"),
+				SiteAdmin:         Bool(false),
+			},
+			Body:          String("comment"),
+			BodyHTML:      String("<p>comment</p>"),
+			BodyVersion:   String("version"),
+			CreatedAt:     &Timestamp{time.Date(2018, time.January, 1, 0, 0, 0, 0, time.UTC)},
+			LastEditedAt:  nil,
+			DiscussionURL: String("https://api.github.com/teams/2/discussions/3"),
+			HTMLURL:       String("https://github.com/orgs/1/teams/2/discussions/3/comments/4"),
+			NodeID:        String("node"),
+			Number:        Int(4),
+			UpdatedAt:     &Timestamp{time.Date(2018, time.January, 1, 0, 0, 0, 0, time.UTC)},
+			URL:           String("https://api.github.com/teams/2/discussions/3/comments/4"),
+		},
+	}
+	if !reflect.DeepEqual(comments, want) {
+		t.Errorf("Teams.ListCommentsByName returned %+v, want %+v", comments, want)
+	}
+}
+
+func TestTeamsService_GetCommentByID(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/organizations/1/team/2/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
 		fmt.Fprint(w, `{"number":4}`)
 	})
 
-	comment, _, err := client.Teams.GetComment(context.Background(), 2, 3, 4)
+	comment, _, err := client.Teams.GetCommentByID(context.Background(), 1, 2, 3, 4)
 	if err != nil {
-		t.Errorf("Teams.GetComment returned error: %v", err)
+		t.Errorf("Teams.GetCommentByID returned error: %v", err)
 	}
 
 	want := &DiscussionComment{Number: Int(4)}
 	if !reflect.DeepEqual(comment, want) {
-		t.Errorf("Teams.GetComment returned %+v, want %+v", comment, want)
+		t.Errorf("Teams.GetCommentByID returned %+v, want %+v", comment, want)
 	}
 }
 
-func TestTeamsService_CreateComment(t *testing.T) {
+func TestTeamsService_GetCommentByName(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"number":4}`)
+	})
+
+	comment, _, err := client.Teams.GetCommentByName(context.Background(), "o", "s", 3, 4)
+	if err != nil {
+		t.Errorf("Teams.GetCommentByName returned error: %v", err)
+	}
+
+	want := &DiscussionComment{Number: Int(4)}
+	if !reflect.DeepEqual(comment, want) {
+		t.Errorf("Teams.GetCommentByName returned %+v, want %+v", comment, want)
+	}
+}
+
+func TestTeamsService_CreateCommentByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
 	input := DiscussionComment{Body: String("c")}
 
-	mux.HandleFunc("/teams/2/discussions/3/comments", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/organizations/1/team/2/discussions/3/comments", func(w http.ResponseWriter, r *http.Request) {
 		v := new(DiscussionComment)
 		json.NewDecoder(r.Body).Decode(v)
 
@@ -143,24 +253,53 @@ func TestTeamsService_CreateComment(t *testing.T) {
 		fmt.Fprint(w, `{"number":4}`)
 	})
 
-	comment, _, err := client.Teams.CreateComment(context.Background(), 2, 3, input)
+	comment, _, err := client.Teams.CreateCommentByID(context.Background(), 1, 2, 3, input)
 	if err != nil {
-		t.Errorf("Teams.CreateComment returned error: %v", err)
+		t.Errorf("Teams.CreateCommentByID returned error: %v", err)
 	}
 
 	want := &DiscussionComment{Number: Int(4)}
 	if !reflect.DeepEqual(comment, want) {
-		t.Errorf("Teams.CreateComment returned %+v, want %+v", comment, want)
+		t.Errorf("Teams.CreateCommentByID returned %+v, want %+v", comment, want)
 	}
 }
 
-func TestTeamsService_EditComment(t *testing.T) {
+func TestTeamsService_CreateCommentByName(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	input := DiscussionComment{Body: String("c")}
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3/comments", func(w http.ResponseWriter, r *http.Request) {
+		v := new(DiscussionComment)
+		json.NewDecoder(r.Body).Decode(v)
+
+		testMethod(t, r, "POST")
+		if !reflect.DeepEqual(v, &input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+
+		fmt.Fprint(w, `{"number":4}`)
+	})
+
+	comment, _, err := client.Teams.CreateCommentByName(context.Background(), "o", "s", 3, input)
+	if err != nil {
+		t.Errorf("Teams.CreateCommentByName returned error: %v", err)
+	}
+
+	want := &DiscussionComment{Number: Int(4)}
+	if !reflect.DeepEqual(comment, want) {
+		t.Errorf("Teams.CreateCommentByName returned %+v, want %+v", comment, want)
+	}
+}
+
+func TestTeamsService_EditCommentByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
 	input := DiscussionComment{Body: String("e")}
 
-	mux.HandleFunc("/teams/2/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/organizations/1/team/2/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
 		v := new(DiscussionComment)
 		json.NewDecoder(r.Body).Decode(v)
 
@@ -172,27 +311,70 @@ func TestTeamsService_EditComment(t *testing.T) {
 		fmt.Fprint(w, `{"number":4}`)
 	})
 
-	comment, _, err := client.Teams.EditComment(context.Background(), 2, 3, 4, input)
+	comment, _, err := client.Teams.EditCommentByID(context.Background(), 1, 2, 3, 4, input)
 	if err != nil {
-		t.Errorf("Teams.EditComment returned error: %v", err)
+		t.Errorf("Teams.EditCommentByID returned error: %v", err)
 	}
 
 	want := &DiscussionComment{Number: Int(4)}
 	if !reflect.DeepEqual(comment, want) {
-		t.Errorf("Teams.EditComment returned %+v, want %+v", comment, want)
+		t.Errorf("Teams.EditCommentByID returned %+v, want %+v", comment, want)
 	}
 }
 
-func TestTeamsService_DeleteComment(t *testing.T) {
+func TestTeamsService_EditCommentByName(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/teams/2/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
+	input := DiscussionComment{Body: String("e")}
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
+		v := new(DiscussionComment)
+		json.NewDecoder(r.Body).Decode(v)
+
+		testMethod(t, r, "PATCH")
+		if !reflect.DeepEqual(v, &input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+
+		fmt.Fprint(w, `{"number":4}`)
+	})
+
+	comment, _, err := client.Teams.EditCommentByName(context.Background(), "o", "s", 3, 4, input)
+	if err != nil {
+		t.Errorf("Teams.EditCommentByName returned error: %v", err)
+	}
+
+	want := &DiscussionComment{Number: Int(4)}
+	if !reflect.DeepEqual(comment, want) {
+		t.Errorf("Teams.EditCommentByName returned %+v, want %+v", comment, want)
+	}
+}
+
+func TestTeamsService_DeleteCommentByID(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/organizations/1/team/2/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "DELETE")
 	})
 
-	_, err := client.Teams.DeleteComment(context.Background(), 2, 3, 4)
+	_, err := client.Teams.DeleteCommentByID(context.Background(), 1, 2, 3, 4)
 	if err != nil {
-		t.Errorf("Teams.DeleteComment returned error: %v", err)
+		t.Errorf("Teams.DeleteCommentByID returned error: %v", err)
+	}
+}
+
+func TestTeamsService_DeleteCommentByName(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3/comments/4", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	_, err := client.Teams.DeleteCommentByName(context.Background(), "o", "s", 3, 4)
+	if err != nil {
+		t.Errorf("Teams.DeleteCommentByName returned error: %v", err)
 	}
 }
