c6643e9bd05932eb62a1fe53f97da161dbb0d189
Martins Sipenko <martins.sipenko@gmail.com>
2020-01-28 16:14:42 +0200

    Update copyright year to 2020

--- github/actions_secrets.go
+++ github/actions_secrets.go
@@ -1,4 +1,4 @@
-// Copyright 2013 The go-github AUTHORS. All rights reserved.
+// Copyright 2020 The go-github AUTHORS. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
