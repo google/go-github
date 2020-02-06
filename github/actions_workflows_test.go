6e9e15c7d9b367773ff09242343935e3cb2a656b
joshuabezaleel <joshua.bezaleel@gmail.com>
2020-02-06 14:56:40 +0700

    Update test file according to GetWorkflow refactor to 2 methods

--- github/actions_workflows_test.go
+++ github/actions_workflows_test.go
@@ -42,7 +42,7 @@ func TestActionsService_ListWorkflows(t *testing.T) {
 	}
 }
 
-func TestActionsService_GetWorkflow(t *testing.T) {
+func TestActionsService_GetWorkflowByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
@@ -51,9 +51,9 @@ func TestActionsService_GetWorkflow(t *testing.T) {
 		fmt.Fprint(w, `{"id":72844,"created_at":"2019-01-02T15:04:05Z","updated_at":"2020-01-02T15:04:05Z"}`)
 	})
 
-	workflow, _, err := client.Actions.GetWorkflow(context.Background(), "o", "r", 72844)
+	workflow, _, err := client.Actions.GetWorkflowByID(context.Background(), "o", "r", 72844)
 	if err != nil {
-		t.Errorf("Actions.GetWorkflow returned error: %v", err)
+		t.Errorf("Actions.GetWorkflowByID returned error: %v", err)
 	}
 
 	want := &Workflow{
@@ -62,6 +62,30 @@ func TestActionsService_GetWorkflow(t *testing.T) {
 		UpdatedAt: Timestamp{time.Date(2020, time.January, 02, 15, 04, 05, 0, time.UTC)},
 	}
 	if !reflect.DeepEqual(workflow, want) {
-		t.Errorf("Actions.GetWorkflow returned %+v, want %+v", workflow, want)
+		t.Errorf("Actions.GetWorkflowByID returned %+v, want %+v", workflow, want)
+	}
+}
+
+func TestActionsService_GetWorkflowByFileName(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/repos/o/r/actions/workflows/main.yml", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"id":72844,"created_at":"2019-01-02T15:04:05Z","updated_at":"2020-01-02T15:04:05Z"}`)
+	})
+
+	workflow, _, err := client.Actions.GetWorkflowByFileName(context.Background(), "o", "r", "main.yml")
+	if err != nil {
+		t.Errorf("Actions.GetWorkflowByFileName returned error: %v", err)
+	}
+
+	want := &Workflow{
+		ID:        72844,
+		CreatedAt: Timestamp{time.Date(2019, time.January, 02, 15, 04, 05, 0, time.UTC)},
+		UpdatedAt: Timestamp{time.Date(2020, time.January, 02, 15, 04, 05, 0, time.UTC)},
+	}
+	if !reflect.DeepEqual(workflow, want) {
+		t.Errorf("Actions.GetWorkflowByFileName returned %+v, want %+v", workflow, want)
 	}
 }
