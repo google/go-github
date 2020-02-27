f9f5fb84f0f8dd97758e00d84cce7b18bcabefb6
jlaportebot <jlaportebot@users.noreply.github.com>
2020-02-28 07:53:31 +1100

    Update teams_discussions with ByID and BySlug endpoints (#1426)
    Fixes: #1424.

--- github/teams_discussions.go
+++ github/teams_discussions.go
@@ -42,15 +42,17 @@ type DiscussionListOptions struct {
 	// Sorts the discussion by the date they were created.
 	// Accepted values are asc and desc. Default is desc.
 	Direction string `url:"direction,omitempty"`
+
+	ListOptions
 }
 
-// ListDiscussions lists all discussions on team's page.
+// ListDiscussionsByID lists all discussions on team's page given Organization and Team ID.
 // Authenticated user must grant read:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussions/#list-discussions
-func (s *TeamsService) ListDiscussions(ctx context.Context, teamID int64, options *DiscussionListOptions) ([]*TeamDiscussion, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions", teamID)
-	u, err := addOptions(u, options)
+func (s *TeamsService) ListDiscussionsByID(ctx context.Context, orgID, teamID int64, opts *DiscussionListOptions) ([]*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions", orgID, teamID)
+	u, err := addOptions(u, opts)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -69,12 +71,37 @@ func (s *TeamsService) ListDiscussions(ctx context.Context, teamID int64, option
 	return teamDiscussions, resp, nil
 }
 
-// GetDiscussion gets a specific discussion on a team's page.
+// ListDiscussionsBySlug lists all discussions on team's page given Organization name and Team's slug.
+// Authenticated user must grant read:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussions/#list-discussions
+func (s *TeamsService) ListDiscussionsBySlug(ctx context.Context, org, slug string, opts *DiscussionListOptions) ([]*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions", org, slug)
+	u, err := addOptions(u, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var teamDiscussions []*TeamDiscussion
+	resp, err := s.client.Do(ctx, req, &teamDiscussions)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return teamDiscussions, resp, nil
+}
+
+// GetDiscussionByID gets a specific discussion on a team's page given Organization and Team ID.
 // Authenticated user must grant read:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussions/#get-a-single-discussion
-func (s *TeamsService) GetDiscussion(ctx context.Context, teamID int64, discussionNumber int) (*TeamDiscussion, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v", teamID, discussionNumber)
+func (s *TeamsService) GetDiscussionByID(ctx context.Context, orgID, teamID int64, discussionNumber int) (*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v", orgID, teamID, discussionNumber)
 	req, err := s.client.NewRequest("GET", u, nil)
 	if err != nil {
 		return nil, nil, err
@@ -89,12 +116,32 @@ func (s *TeamsService) GetDiscussion(ctx context.Context, teamID int64, discussi
 	return teamDiscussion, resp, nil
 }
 
-// CreateDiscussion creates a new discussion post on a team's page.
+// GetDiscussionBySlug gets a specific discussion on a team's page given Organization name and Team's slug.
+// Authenticated user must grant read:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussions/#get-a-single-discussion
+func (s *TeamsService) GetDiscussionBySlug(ctx context.Context, org, slug string, discussionNumber int) (*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v", org, slug, discussionNumber)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	teamDiscussion := &TeamDiscussion{}
+	resp, err := s.client.Do(ctx, req, teamDiscussion)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return teamDiscussion, resp, nil
+}
+
+// CreateDiscussionByID creates a new discussion post on a team's page given Organization and Team ID.
 // Authenticated user must grant write:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussions/#create-a-discussion
-func (s *TeamsService) CreateDiscussion(ctx context.Context, teamID int64, discussion TeamDiscussion) (*TeamDiscussion, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions", teamID)
+func (s *TeamsService) CreateDiscussionByID(ctx context.Context, orgID, teamID int64, discussion TeamDiscussion) (*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions", orgID, teamID)
 	req, err := s.client.NewRequest("POST", u, discussion)
 	if err != nil {
 		return nil, nil, err
@@ -109,13 +156,33 @@ func (s *TeamsService) CreateDiscussion(ctx context.Context, teamID int64, discu
 	return teamDiscussion, resp, nil
 }
 
-// EditDiscussion edits the title and body text of a discussion post.
+// CreateDiscussionBySlug creates a new discussion post on a team's page given Organization name and Team's slug.
+// Authenticated user must grant write:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussions/#create-a-discussion
+func (s *TeamsService) CreateDiscussionBySlug(ctx context.Context, org, slug string, discussion TeamDiscussion) (*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions", org, slug)
+	req, err := s.client.NewRequest("POST", u, discussion)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	teamDiscussion := &TeamDiscussion{}
+	resp, err := s.client.Do(ctx, req, teamDiscussion)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return teamDiscussion, resp, nil
+}
+
+// EditDiscussionByID edits the title and body text of a discussion post given Organization and Team ID.
 // Authenticated user must grant write:discussion scope.
 // User is allowed to change Title and Body of a discussion only.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussions/#edit-a-discussion
-func (s *TeamsService) EditDiscussion(ctx context.Context, teamID int64, discussionNumber int, discussion TeamDiscussion) (*TeamDiscussion, *Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v", teamID, discussionNumber)
+func (s *TeamsService) EditDiscussionByID(ctx context.Context, orgID, teamID int64, discussionNumber int, discussion TeamDiscussion) (*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v", orgID, teamID, discussionNumber)
 	req, err := s.client.NewRequest("PATCH", u, discussion)
 	if err != nil {
 		return nil, nil, err
@@ -130,12 +197,47 @@ func (s *TeamsService) EditDiscussion(ctx context.Context, teamID int64, discuss
 	return teamDiscussion, resp, nil
 }
 
-// DeleteDiscussion deletes a discussion from team's page.
+// EditDiscussionBySlug edits the title and body text of a discussion post given Organization name and Team's slug.
+// Authenticated user must grant write:discussion scope.
+// User is allowed to change Title and Body of a discussion only.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussions/#edit-a-discussion
+func (s *TeamsService) EditDiscussionBySlug(ctx context.Context, org, slug string, discussionNumber int, discussion TeamDiscussion) (*TeamDiscussion, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v", org, slug, discussionNumber)
+	req, err := s.client.NewRequest("PATCH", u, discussion)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	teamDiscussion := &TeamDiscussion{}
+	resp, err := s.client.Do(ctx, req, teamDiscussion)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return teamDiscussion, resp, nil
+}
+
+// DeleteDiscussionByID deletes a discussion from team's page given Organization and Team ID.
 // Authenticated user must grant write:discussion scope.
 //
 // GitHub API docs: https://developer.github.com/v3/teams/discussions/#delete-a-discussion
-func (s *TeamsService) DeleteDiscussion(ctx context.Context, teamID int64, discussionNumber int) (*Response, error) {
-	u := fmt.Sprintf("teams/%v/discussions/%v", teamID, discussionNumber)
+func (s *TeamsService) DeleteDiscussionByID(ctx context.Context, orgID, teamID int64, discussionNumber int) (*Response, error) {
+	u := fmt.Sprintf("organizations/%v/team/%v/discussions/%v", orgID, teamID, discussionNumber)
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// DeleteDiscussionBySlug deletes a discussion from team's page given Organization name and Team's slug.
+// Authenticated user must grant write:discussion scope.
+//
+// GitHub API docs: https://developer.github.com/v3/teams/discussions/#delete-a-discussion
+func (s *TeamsService) DeleteDiscussionBySlug(ctx context.Context, org, slug string, discussionNumber int) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/teams/%v/discussions/%v", org, slug, discussionNumber)
 	req, err := s.client.NewRequest("DELETE", u, nil)
 	if err != nil {
 		return nil, err
--- github/teams_discussions_test.go
+++ github/teams_discussions_test.go
@@ -15,14 +15,15 @@ import (
 	"time"
 )
 
-func TestTeamsService_ListDiscussions(t *testing.T) {
+func TestTeamsService_ListDiscussionsByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/teams/2/discussions", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/organizations/1/team/2/discussions", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
 		testFormValues(t, r, values{
 			"direction": "desc",
+			"page":      "2",
 		})
 		fmt.Fprintf(w,
 			`[
@@ -65,9 +66,9 @@ func TestTeamsService_ListDiscussions(t *testing.T) {
 				}
 			]`)
 	})
-	discussions, _, err := client.Teams.ListDiscussions(context.Background(), 2, &DiscussionListOptions{"desc"})
+	discussions, _, err := client.Teams.ListDiscussionsByID(context.Background(), 1, 2, &DiscussionListOptions{"desc", ListOptions{Page: 2}})
 	if err != nil {
-		t.Errorf("Teams.ListDiscussions returned error: %v", err)
+		t.Errorf("Teams.ListDiscussionsByID returned error: %v", err)
 	}
 
 	want := []*TeamDiscussion{
@@ -110,37 +111,157 @@ func TestTeamsService_ListDiscussions(t *testing.T) {
 		},
 	}
 	if !reflect.DeepEqual(discussions, want) {
-		t.Errorf("Teams.ListDiscussions returned %+v, want %+v", discussions, want)
+		t.Errorf("Teams.ListDiscussionsByID returned %+v, want %+v", discussions, want)
 	}
 }
 
-func TestTeamsService_GetDiscussion(t *testing.T) {
+func TestTeamsService_ListDiscussionsBySlug(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/teams/2/discussions/3", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/orgs/o/teams/s/discussions", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{
+			"direction": "desc",
+			"page":      "2",
+		})
+		fmt.Fprintf(w,
+			`[
+				{
+					"author": {
+						"login": "author",
+						"id": 0,
+						"avatar_url": "https://avatars1.githubusercontent.com/u/0?v=4",
+						"gravatar_id": "",
+						"url": "https://api.github.com/users/author",
+						"html_url": "https://github.com/author",
+						"followers_url": "https://api.github.com/users/author/followers",
+						"following_url": "https://api.github.com/users/author/following{/other_user}",
+						"gists_url": "https://api.github.com/users/author/gists{/gist_id}",
+						"starred_url": "https://api.github.com/users/author/starred{/owner}{/repo}",
+						"subscriptions_url": "https://api.github.com/users/author/subscriptions",
+						"organizations_url": "https://api.github.com/users/author/orgs",
+						"repos_url": "https://api.github.com/users/author/repos",
+						"events_url": "https://api.github.com/users/author/events{/privacy}",
+						"received_events_url": "https://api.github.com/users/author/received_events",
+						"type": "User",
+						"site_admin": false
+					},
+					"body": "test",
+					"body_html": "<p>test</p>",
+					"body_version": "version",
+					"comments_count": 1,
+					"comments_url": "https://api.github.com/teams/2/discussions/3/comments",
+					"created_at": "2018-01-01T00:00:00Z",
+					"last_edited_at": null,
+					"html_url": "https://github.com/orgs/1/teams/2/discussions/3",
+					"node_id": "node",
+					"number": 3,
+					"pinned": false,
+					"private": false,
+					"team_url": "https://api.github.com/teams/2",
+					"title": "test",
+					"updated_at": "2018-01-01T00:00:00Z",
+					"url": "https://api.github.com/teams/2/discussions/3"
+				}
+			]`)
+	})
+	discussions, _, err := client.Teams.ListDiscussionsBySlug(context.Background(), "o", "s", &DiscussionListOptions{"desc", ListOptions{Page: 2}})
+	if err != nil {
+		t.Errorf("Teams.ListDiscussionsBySlug returned error: %v", err)
+	}
+
+	want := []*TeamDiscussion{
+		{
+			Author: &User{
+				Login:             String("author"),
+				ID:                Int64(0),
+				AvatarURL:         String("https://avatars1.githubusercontent.com/u/0?v=4"),
+				GravatarID:        String(""),
+				URL:               String("https://api.github.com/users/author"),
+				HTMLURL:           String("https://github.com/author"),
+				FollowersURL:      String("https://api.github.com/users/author/followers"),
+				FollowingURL:      String("https://api.github.com/users/author/following{/other_user}"),
+				GistsURL:          String("https://api.github.com/users/author/gists{/gist_id}"),
+				StarredURL:        String("https://api.github.com/users/author/starred{/owner}{/repo}"),
+				SubscriptionsURL:  String("https://api.github.com/users/author/subscriptions"),
+				OrganizationsURL:  String("https://api.github.com/users/author/orgs"),
+				ReposURL:          String("https://api.github.com/users/author/repos"),
+				EventsURL:         String("https://api.github.com/users/author/events{/privacy}"),
+				ReceivedEventsURL: String("https://api.github.com/users/author/received_events"),
+				Type:              String("User"),
+				SiteAdmin:         Bool(false),
+			},
+			Body:          String("test"),
+			BodyHTML:      String("<p>test</p>"),
+			BodyVersion:   String("version"),
+			CommentsCount: Int(1),
+			CommentsURL:   String("https://api.github.com/teams/2/discussions/3/comments"),
+			CreatedAt:     &Timestamp{time.Date(2018, time.January, 1, 0, 0, 0, 0, time.UTC)},
+			LastEditedAt:  nil,
+			HTMLURL:       String("https://github.com/orgs/1/teams/2/discussions/3"),
+			NodeID:        String("node"),
+			Number:        Int(3),
+			Pinned:        Bool(false),
+			Private:       Bool(false),
+			TeamURL:       String("https://api.github.com/teams/2"),
+			Title:         String("test"),
+			UpdatedAt:     &Timestamp{time.Date(2018, time.January, 1, 0, 0, 0, 0, time.UTC)},
+			URL:           String("https://api.github.com/teams/2/discussions/3"),
+		},
+	}
+	if !reflect.DeepEqual(discussions, want) {
+		t.Errorf("Teams.ListDiscussionsBySlug returned %+v, want %+v", discussions, want)
+	}
+}
+
+func TestTeamsService_GetDiscussionByID(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/organizations/1/team/2/discussions/3", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
 		fmt.Fprint(w, `{"number":3}`)
 	})
 
-	discussion, _, err := client.Teams.GetDiscussion(context.Background(), 2, 3)
+	discussion, _, err := client.Teams.GetDiscussionByID(context.Background(), 1, 2, 3)
 	if err != nil {
-		t.Errorf("Teams.GetDiscussion returned error: %v", err)
+		t.Errorf("Teams.GetDiscussionByID returned error: %v", err)
 	}
 
 	want := &TeamDiscussion{Number: Int(3)}
 	if !reflect.DeepEqual(discussion, want) {
-		t.Errorf("Teams.GetDiscussion returned %+v, want %+v", discussion, want)
+		t.Errorf("Teams.GetDiscussionByID returned %+v, want %+v", discussion, want)
 	}
 }
 
-func TestTeamsService_CreateDiscussion(t *testing.T) {
+func TestTeamsService_GetDiscussionBySlug(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"number":3}`)
+	})
+
+	discussion, _, err := client.Teams.GetDiscussionBySlug(context.Background(), "o", "s", 3)
+	if err != nil {
+		t.Errorf("Teams.GetDiscussionBySlug returned error: %v", err)
+	}
+
+	want := &TeamDiscussion{Number: Int(3)}
+	if !reflect.DeepEqual(discussion, want) {
+		t.Errorf("Teams.GetDiscussionBySlug returned %+v, want %+v", discussion, want)
+	}
+}
+
+func TestTeamsService_CreateDiscussionByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
 	input := TeamDiscussion{Title: String("c_t"), Body: String("c_b")}
 
-	mux.HandleFunc("/teams/2/discussions", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/organizations/1/team/2/discussions", func(w http.ResponseWriter, r *http.Request) {
 		v := new(TeamDiscussion)
 		json.NewDecoder(r.Body).Decode(v)
 
@@ -152,24 +273,53 @@ func TestTeamsService_CreateDiscussion(t *testing.T) {
 		fmt.Fprint(w, `{"number":3}`)
 	})
 
-	comment, _, err := client.Teams.CreateDiscussion(context.Background(), 2, input)
+	comment, _, err := client.Teams.CreateDiscussionByID(context.Background(), 1, 2, input)
 	if err != nil {
-		t.Errorf("Teams.CreateDiscussion returned error: %v", err)
+		t.Errorf("Teams.CreateDiscussionByID returned error: %v", err)
 	}
 
 	want := &TeamDiscussion{Number: Int(3)}
 	if !reflect.DeepEqual(comment, want) {
-		t.Errorf("Teams.CreateDiscussion returned %+v, want %+v", comment, want)
+		t.Errorf("Teams.CreateDiscussionByID returned %+v, want %+v", comment, want)
 	}
 }
 
-func TestTeamsService_EditDiscussion(t *testing.T) {
+func TestTeamsService_CreateDiscussionBySlug(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	input := TeamDiscussion{Title: String("c_t"), Body: String("c_b")}
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions", func(w http.ResponseWriter, r *http.Request) {
+		v := new(TeamDiscussion)
+		json.NewDecoder(r.Body).Decode(v)
+
+		testMethod(t, r, "POST")
+		if !reflect.DeepEqual(v, &input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+
+		fmt.Fprint(w, `{"number":3}`)
+	})
+
+	comment, _, err := client.Teams.CreateDiscussionBySlug(context.Background(), "o", "s", input)
+	if err != nil {
+		t.Errorf("Teams.CreateDiscussionBySlug returned error: %v", err)
+	}
+
+	want := &TeamDiscussion{Number: Int(3)}
+	if !reflect.DeepEqual(comment, want) {
+		t.Errorf("Teams.CreateDiscussionBySlug returned %+v, want %+v", comment, want)
+	}
+}
+
+func TestTeamsService_EditDiscussionByID(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
 	input := TeamDiscussion{Title: String("e_t"), Body: String("e_b")}
 
-	mux.HandleFunc("/teams/2/discussions/3", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/organizations/1/team/2/discussions/3", func(w http.ResponseWriter, r *http.Request) {
 		v := new(TeamDiscussion)
 		json.NewDecoder(r.Body).Decode(v)
 
@@ -181,27 +331,70 @@ func TestTeamsService_EditDiscussion(t *testing.T) {
 		fmt.Fprint(w, `{"number":3}`)
 	})
 
-	comment, _, err := client.Teams.EditDiscussion(context.Background(), 2, 3, input)
+	comment, _, err := client.Teams.EditDiscussionByID(context.Background(), 1, 2, 3, input)
 	if err != nil {
-		t.Errorf("Teams.EditDiscussion returned error: %v", err)
+		t.Errorf("Teams.EditDiscussionByID returned error: %v", err)
 	}
 
 	want := &TeamDiscussion{Number: Int(3)}
 	if !reflect.DeepEqual(comment, want) {
-		t.Errorf("Teams.EditDiscussion returned %+v, want %+v", comment, want)
+		t.Errorf("Teams.EditDiscussionByID returned %+v, want %+v", comment, want)
 	}
 }
 
-func TestTeamsService_DeleteDiscussion(t *testing.T) {
+func TestTeamsService_EditDiscussionBySlug(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/teams/2/discussions/3", func(w http.ResponseWriter, r *http.Request) {
+	input := TeamDiscussion{Title: String("e_t"), Body: String("e_b")}
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3", func(w http.ResponseWriter, r *http.Request) {
+		v := new(TeamDiscussion)
+		json.NewDecoder(r.Body).Decode(v)
+
+		testMethod(t, r, "PATCH")
+		if !reflect.DeepEqual(v, &input) {
+			t.Errorf("Request body = %+v, want %+v", v, input)
+		}
+
+		fmt.Fprint(w, `{"number":3}`)
+	})
+
+	comment, _, err := client.Teams.EditDiscussionBySlug(context.Background(), "o", "s", 3, input)
+	if err != nil {
+		t.Errorf("Teams.EditDiscussionBySlug returned error: %v", err)
+	}
+
+	want := &TeamDiscussion{Number: Int(3)}
+	if !reflect.DeepEqual(comment, want) {
+		t.Errorf("Teams.EditDiscussionBySlug returned %+v, want %+v", comment, want)
+	}
+}
+
+func TestTeamsService_DeleteDiscussionByID(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/organizations/1/team/2/discussions/3", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "DELETE")
 	})
 
-	_, err := client.Teams.DeleteDiscussion(context.Background(), 2, 3)
+	_, err := client.Teams.DeleteDiscussionByID(context.Background(), 1, 2, 3)
 	if err != nil {
-		t.Errorf("Teams.DeleteDiscussion returned error: %v", err)
+		t.Errorf("Teams.DeleteDiscussionByID returned error: %v", err)
+	}
+}
+
+func TestTeamsService_DeleteDiscussionBySlug(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/teams/s/discussions/3", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	_, err := client.Teams.DeleteDiscussionBySlug(context.Background(), "o", "s", 3)
+	if err != nil {
+		t.Errorf("Teams.DeleteDiscussionBySlug returned error: %v", err)
 	}
 }
