5599c9b422bc69c034fc4bb6b0c8ae492ae890ee
Ryan Mast <mast.ryan@gmail.com>
2020-05-15 18:55:18 -0700

    Add organization secret API endpoints

--- github/actions_secrets.go
+++ github/actions_secrets.go
@@ -35,11 +35,32 @@ func (s *ActionsService) GetRepositoryPublicKey(ctx context.Context, owner, repo
 	return pubKey, resp, nil
 }
 
+// GetOrganizationPublicKey gets a public key that should be used for secret encryption.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#get-an-organization-public-key
+func (s *ActionsService) GetOrganizationPublicKey(ctx context.Context, org string) (*PublicKey, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/public-key", org)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pubKey := new(PublicKey)
+	resp, err := s.client.Do(ctx, req, pubKey)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return pubKey, resp, nil
+}
+
 // Secret represents a repository action secret.
 type Secret struct {
-	Name      string    `json:"name"`
-	CreatedAt Timestamp `json:"created_at"`
-	UpdatedAt Timestamp `json:"updated_at"`
+	Name                    string    `json:"name"`
+	CreatedAt               Timestamp `json:"created_at"`
+	UpdatedAt               Timestamp `json:"updated_at"`
+	Visibility              string    `json:"visibility,omitempty"`
+	SelectedRepositoriesURL string    `json:"selected_repositories_url,omitempty"`
 }
 
 // Secrets represents one item from the ListSecrets response.
@@ -53,7 +74,7 @@ type Secrets struct {
 //
 // GitHub API docs: https://developer.github.com/v3/actions/secrets/#list-repository-secrets
 func (s *ActionsService) ListRepositorySecrets(ctx context.Context, owner, repo string, opts *ListOptions) (*Secrets, *Response, error) {
-	u := fmt.Sprintf("repos/%s/%s/actions/secrets", owner, repo)
+	u := fmt.Sprintf("repos/%v/%v/actions/secrets", owner, repo)
 	u, err := addOptions(u, opts)
 	if err != nil {
 		return nil, nil, err
@@ -92,6 +113,11 @@ func (s *ActionsService) GetRepositorySecret(ctx context.Context, owner, repo, n
 	return secret, resp, nil
 }
 
+// SelectedRepositoryIDs are the repository IDs that have access to the secret.
+type SelectedRepositoryIDs struct {
+	SelectedRepositoryIDs []int64 `json:"selected_repository_ids,omitempty"`
+}
+
 // EncryptedSecret represents a secret that is encrypted using a public key.
 //
 // The value of EncryptedValue must be your secret, encrypted with
@@ -101,6 +127,8 @@ type EncryptedSecret struct {
 	Name           string `json:"-"`
 	KeyID          string `json:"key_id"`
 	EncryptedValue string `json:"encrypted_value"`
+	Visibility     string `json:"visibility,omitempty"`
+	SelectedRepositoryIDs
 }
 
 // CreateOrUpdateRepositorySecret creates or updates a repository secret with an encrypted value.
@@ -122,6 +150,142 @@ func (s *ActionsService) CreateOrUpdateRepositorySecret(ctx context.Context, own
 // GitHub API docs: https://developer.github.com/v3/actions/secrets/#delete-a-repository-secret
 func (s *ActionsService) DeleteRepositorySecret(ctx context.Context, owner, repo, name string) (*Response, error) {
 	u := fmt.Sprintf("repos/%v/%v/actions/secrets/%v", owner, repo, name)
+
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// ListOrganizationSecrets lists all secrets available in an organization
+// without revealing their encrypted values.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#list-organization-secrets
+func (s *ActionsService) ListOrganizationSecrets(ctx context.Context, org string, opts *ListOptions) (*Secrets, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets", org)
+	u, err := addOptions(u, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	secrets := new(Secrets)
+	resp, err := s.client.Do(ctx, req, &secrets)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return secrets, resp, nil
+}
+
+// GetOrganizationSecret gets a single organization secret without revealing its encrypted value.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#get-an-organization-secret
+func (s *ActionsService) GetOrganizationSecret(ctx context.Context, org, name string) (*Secret, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v", org, name)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	secret := new(Secret)
+	resp, err := s.client.Do(ctx, req, secret)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return secret, resp, nil
+}
+
+// CreateOrUpdateOrganizationSecret creates or updates an organization secret with an encrypted value.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#create-or-update-an-organization-secret
+func (s *ActionsService) CreateOrUpdateOrganizationSecret(ctx context.Context, org string, eSecret *EncryptedSecret) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v", org, eSecret.Name)
+
+	req, err := s.client.NewRequest("PUT", u, eSecret)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// SelectedRepositoriesList represents the list of repositories selected for an organization secret.
+type SelectedRepositoriesList struct {
+	TotalCount   *int          `json:"total_count,omitempty"`
+	Repositories []*Repository `json:"repositories,omitempty"`
+}
+
+// ListSelectedRepositoriesForOrganizationSecret lists all repositories that have access to a secret.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#list-selected-repositories-for-an-organization-secret
+func (s *ActionsService) ListSelectedRepositoriesForOrganizationSecret(ctx context.Context, org, name string) (*SelectedRepositoriesList, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v/repositories", org, name)
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	result := new(SelectedRepositoriesList)
+	resp, err := s.client.Do(ctx, req, result)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return result, resp, nil
+}
+
+// SetSelectedRepositoriesForOrganizationSecret sets the repositories that have access to a secret.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#set-selected-repositories-for-an-organization-secret
+func (s *ActionsService) SetSelectedRepositoriesForOrganizationSecret(ctx context.Context, org, name string, ids SelectedRepositoryIDs) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v/repositories", org, name)
+	req, err := s.client.NewRequest("PUT", u, ids)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// AddSelectedRepositoryToOrganizationSecret adds a repository to an organization secret.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#add-selected-repository-to-an-organization-secret
+func (s *ActionsService) AddSelectedRepositoryToOrganizationSecret(ctx context.Context, org, name string, repo *Repository) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v/repositories/%v", org, name, *repo.ID)
+	req, err := s.client.NewRequest("PUT", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// RemoveSelectedRepositoryFromOrganizationSecret removes a repository from an organization secret.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#remove-selected-repository-from-an-organization-secret
+func (s *ActionsService) RemoveSelectedRepositoryFromOrganizationSecret(ctx context.Context, org, name string, repo *Repository) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v/repositories/%v", org, name, *repo.ID)
+	req, err := s.client.NewRequest("DELETE", u, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return s.client.Do(ctx, req, nil)
+}
+
+// DeleteOrganizationSecret deletes a secret in an organization using the secret name.
+//
+// GitHub API docs: https://developer.github.com/v3/actions/secrets/#delete-an-organization-secret
+func (s *ActionsService) DeleteOrganizationSecret(ctx context.Context, org, name string) (*Response, error) {
+	u := fmt.Sprintf("orgs/%v/actions/secrets/%v", org, name)
 
 	req, err := s.client.NewRequest("DELETE", u, nil)
 	if err != nil {
--- github/actions_secrets_test.go
+++ github/actions_secrets_test.go
@@ -121,3 +121,187 @@ func TestActionsService_DeleteRepositorySecret(t *testing.T) {
 		t.Errorf("Actions.DeleteRepositorySecret returned error: %v", err)
 	}
 }
+
+func TestActionsService_GetOrganizationPublicKey(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/public-key", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"key_id":"012345678","key":"2Sg8iYjAxxmI2LvUXpJjkYrMxURPc8r+dB7TJyvv1234"}`)
+	})
+
+	key, _, err := client.Actions.GetOrganizationPublicKey(context.Background(), "o")
+	if err != nil {
+		t.Errorf("Actions.GetOrganizationPublicKey returned error: %v", err)
+	}
+
+	want := &PublicKey{KeyID: String("012345678"), Key: String("2Sg8iYjAxxmI2LvUXpJjkYrMxURPc8r+dB7TJyvv1234")}
+	if !reflect.DeepEqual(key, want) {
+		t.Errorf("Actions.GetOrganizationPublicKey returned %+v, want %+v", key, want)
+	}
+}
+
+func TestActionsService_ListOrganizationSecrets(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testFormValues(t, r, values{"per_page": "2", "page": "2"})
+		fmt.Fprint(w, `{"total_count":3,"secrets":[{"name":"GIST_ID","created_at":"2019-08-10T14:59:22Z","updated_at":"2020-01-10T14:59:22Z","visibility":"private"},{"name":"DEPLOY_TOKEN","created_at":"2019-08-10T14:59:22Z","updated_at":"2020-01-10T14:59:22Z","visibility":"all"},{"name":"GH_TOKEN","created_at":"2019-08-10T14:59:22Z","updated_at":"2020-01-10T14:59:22Z","visibility":"selected","selected_repositories_url":"https://api.github.com/orgs/octo-org/actions/secrets/SUPER_SECRET/repositories"}]}`)
+	})
+
+	opts := &ListOptions{Page: 2, PerPage: 2}
+	secrets, _, err := client.Actions.ListOrganizationSecrets(context.Background(), "o", opts)
+	if err != nil {
+		t.Errorf("Actions.ListOrganizationSecrets returned error: %v", err)
+	}
+
+	want := &Secrets{
+		TotalCount: 3,
+		Secrets: []*Secret{
+			{Name: "GIST_ID", CreatedAt: Timestamp{time.Date(2019, time.August, 10, 14, 59, 22, 0, time.UTC)}, UpdatedAt: Timestamp{time.Date(2020, time.January, 10, 14, 59, 22, 0, time.UTC)}, Visibility: "private"},
+			{Name: "DEPLOY_TOKEN", CreatedAt: Timestamp{time.Date(2019, time.August, 10, 14, 59, 22, 0, time.UTC)}, UpdatedAt: Timestamp{time.Date(2020, time.January, 10, 14, 59, 22, 0, time.UTC)}, Visibility: "all"},
+			{Name: "GH_TOKEN", CreatedAt: Timestamp{time.Date(2019, time.August, 10, 14, 59, 22, 0, time.UTC)}, UpdatedAt: Timestamp{time.Date(2020, time.January, 10, 14, 59, 22, 0, time.UTC)}, Visibility: "selected", SelectedRepositoriesURL: "https://api.github.com/orgs/octo-org/actions/secrets/SUPER_SECRET/repositories"},
+		},
+	}
+	if !reflect.DeepEqual(secrets, want) {
+		t.Errorf("Actions.ListOrganizationSecrets returned %+v, want %+v", secrets, want)
+	}
+}
+
+func TestActionsService_GetOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, `{"name":"NAME","created_at":"2019-01-02T15:04:05Z","updated_at":"2020-01-02T15:04:05Z","visibility":"selected","selected_repositories_url":"https://api.github.com/orgs/octo-org/actions/secrets/SUPER_SECRET/repositories"}`)
+	})
+
+	secret, _, err := client.Actions.GetOrganizationSecret(context.Background(), "o", "NAME")
+	if err != nil {
+		t.Errorf("Actions.GetRepositorySecret returned error: %v", err)
+	}
+
+	want := &Secret{
+		Name:                    "NAME",
+		CreatedAt:               Timestamp{time.Date(2019, time.January, 02, 15, 04, 05, 0, time.UTC)},
+		UpdatedAt:               Timestamp{time.Date(2020, time.January, 02, 15, 04, 05, 0, time.UTC)},
+		Visibility:              "selected",
+		SelectedRepositoriesURL: "https://api.github.com/orgs/octo-org/actions/secrets/SUPER_SECRET/repositories",
+	}
+	if !reflect.DeepEqual(secret, want) {
+		t.Errorf("Actions.GetOrganizationSecret returned %+v, want %+v", secret, want)
+	}
+}
+
+func TestActionsService_CreateOrUpdateOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		testHeader(t, r, "Content-Type", "application/json")
+		testBody(t, r, `{"key_id":"1234","encrypted_value":"QIv=","visibility":"selected","selected_repository_ids":[1296269,1269280]}`+"\n")
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	input := &EncryptedSecret{
+		Name:                  "NAME",
+		EncryptedValue:        "QIv=",
+		KeyID:                 "1234",
+		Visibility:            "selected",
+		SelectedRepositoryIDs: SelectedRepositoryIDs{[]int64{1296269, 1269280}},
+	}
+	_, err := client.Actions.CreateOrUpdateOrganizationSecret(context.Background(), "o", input)
+	if err != nil {
+		t.Errorf("Actions.CreateOrUpdateOrganizationSecret returned error: %v", err)
+	}
+}
+
+func TestActionsService_ListSelectedRepositoriesForOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME/repositories", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprintf(w, `{"total_count":1,"repositories":[{"id":1}]}`)
+	})
+
+	repos, _, err := client.Actions.ListSelectedRepositoriesForOrganizationSecret(context.Background(), "o", "NAME")
+	if err != nil {
+		t.Errorf("Actions.ListSelectedRepositoriesForOrganizationSecret returned error: %v", err)
+	}
+
+	want := &SelectedRepositoriesList{
+		TotalCount: Int(1),
+		Repositories: []*Repository{
+			{ID: Int64(1)},
+		},
+	}
+	if !reflect.DeepEqual(repos, want) {
+		t.Errorf("Actions.ListSelectedRepositoriesForOrganizationSecret returned %+v, want %+v", repos, want)
+	}
+}
+
+func TestActionsService_SetSelectedRepositoriesForOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME/repositories", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		testHeader(t, r, "Content-Type", "application/json")
+		testBody(t, r, `{"selected_repository_ids":[64780797]}`+"\n")
+	})
+
+	_, err := client.Actions.SetSelectedRepositoriesForOrganizationSecret(context.Background(), "o", "NAME", SelectedRepositoryIDs{[]int64{64780797}})
+	if err != nil {
+		t.Errorf("Actions.SetSelectedRepositoriesForOrganizationSecret returned error: %v", err)
+	}
+}
+
+func TestActionsService_AddSelectedRepositoryToOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME/repositories/1234", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+	})
+
+	repo := &Repository{ID: Int64(1234)}
+	_, err := client.Actions.AddSelectedRepositoryToOrganizationSecret(context.Background(), "o", "NAME", repo)
+	if err != nil {
+		t.Errorf("Actions.AddSelectedRepositoryToOrganizationSecret returned error: %v", err)
+	}
+}
+
+func TestActionsService_RemoveSelectedRepositoryFromOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME/repositories/1234", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	repo := &Repository{ID: Int64(1234)}
+	_, err := client.Actions.RemoveSelectedRepositoryFromOrganizationSecret(context.Background(), "o", "NAME", repo)
+	if err != nil {
+		t.Errorf("Actions.RemoveSelectedRepositoryFromOrganizationSecret returned error: %v", err)
+	}
+}
+
+func TestActionsService_DeleteOrganizationSecret(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/actions/secrets/NAME", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "DELETE")
+	})
+
+	_, err := client.Actions.DeleteOrganizationSecret(context.Background(), "o", "NAME")
+	if err != nil {
+		t.Errorf("Actions.DeleteOrganizationSecret returned error: %v", err)
+	}
+}
