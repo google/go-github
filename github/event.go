da1cc97d9c5707109bbe0ca1e33e61f94c83c71b
Nikolaos Kanakis <4174162+nicolasmanic@users.noreply.github.com>
2020-08-15 23:15:21 +0200

    Add Workflow run and dispatch events (#1600)

--- github/event.go
+++ github/event.go
@@ -118,6 +118,10 @@ func (e *Event) ParsePayload() (payload interface{}, err error) {
 		payload = &UserEvent{}
 	case "WatchEvent":
 		payload = &WatchEvent{}
+	case "WorkflowDispatchEvent":
+		payload = &WorkflowDispatchEvent{}
+	case "WorkflowRunEvent":
+		payload = &WorkflowRunEvent{}
 	}
 	err = json.Unmarshal(*e.RawPayload, &payload)
 	return payload, err
