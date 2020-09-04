64eb9568bebe61e283f515be9d04f7bafed1ed00
Russell Boley <raboley@gmail.com>
2020-09-04 07:33:31 -0700

    Revisions made to clean up godoc comments and other suggestions from @gmlewis

--- example/newreposecret/main.go
+++ example/newreposecret/main.go
@@ -1,3 +1,24 @@
+// Copyright 2018 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// newreposecret creates a new secret in GitHub for a given owner/repo
+// It has two required flags for owner and repo, and takes in one argument for the name of the secret to add.
+// Provide the value of the secret you want to add with an environment variable of the same name.
+// To authenticate with GitHub provide it via an environment variable GITHUB_AUTH_TOKEN
+//
+// To verify the new secret navigate to GitHub Repository > Settings > left side options bar > Secrets
+//
+// Usage:
+//	export GITHUB_AUTH_TOKEN=<auth token from github that has secret create rights>
+//	export SECRET_VARIABLE=<secret value of the secret variable>
+//	go run main.go -owner <owner name> -repo <repository name> SECRET_VARIABLE
+//
+// Example:
+//	export GITHUB_AUTH_TOKEN=0000000000000000
+//	export SECRET_VARIABLE="my-secret"
+//	go run main.go -owner google -repo go-github SECRET_VARIABLE
 package main
 
 import (
@@ -18,113 +39,68 @@ var (
 	owner = flag.String("owner", "", "The owner of there repo this should be added to, ex. google")
 )
 
-/*
-main creates a new secret in github for a given owner/repo based on a secretName passed in as an arg
-and a github token, and secret value provided via the environment
-
-usage:
-	export GITHUB_AUTH_TOKEN=<auth token from github that has secret create rights>
-	export SECRET_VARIABLE=<secret value of the secret variable>
-	go run main.go -owner <owner name> -repo <repository name> SECRET_VARIABLE
-
-ex:
-	export GITHUB_AUTH_TOKEN=0000000000000000
-	export SECRET_VARIABLE="my-secret"
-	go run main.go -owner google -repo go-github SECRET_VARIABLE
-
-Once it runs go to the github repository > settings > left side options bar > Secrets
-And you should see the new secret appear there.
-*/
 func main() {
 	flag.Parse()
 
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
 	if token == "" {
-		log.Fatal(`Unauthorized: No token present in environment variables. 
-Add your github api token as an environment variable named GITHUB_AUTH_TOKEN
-
-ex:
-	export GITHUB_AUTH_TOKEN=<github auth token>`)
+		log.Fatal("please provide a GitHub API token via env variable GITHUB_AUTH_TOKEN")
 	}
 
 	if *repo == "" {
-		log.Fatal(`required flag repo was not passed. 
-please pass the repo you want to add this flag to as part of the command
-
-ex:
-	go run main.go --repo <repo name> --owner <repo owner name> 
-`)
+		log.Fatal("please provide required flag --repo to specify GitHub repository ")
 	}
 
 	if *owner == "" {
-		log.Fatal(`required flag owner was not passed. 
-please pass the owner you want to add this flag to as part of the command
-
-ex:
-	go run main.go --repo <repo name> --owner <repo owner name> 
-`)
+		log.Fatal("please provide required flag --owner to speify GitHub user/org owner")
 	}
 
-	ctx, client, err := GithubAuth()
-	if err != nil {
-		log.Fatal("unable to authorize client with github using token found in env var GITHUB_AUTH_TOKEN, error was passed: ", err.Error())
-	}
 	secretName, err := getSecretName()
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 
 	secretValue, err := getSecretValue(secretName)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
 
-	addedSecretName, err := AddRepoSecret(ctx, client, *owner, *repo, secretName, secretValue)
+	ctx, client, err := githubAuth(token)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalf("unable to authorize using env GITHUB_AUTH_TOKEN: %v", err)
 	}
 
-	fmt.Println(fmt.Sprintf("Added Secret %s to the repo %s/%s", addedSecretName, *owner, *repo))
+	err = addRepoSecret(ctx, client, *owner, *repo, secretName, secretValue)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	fmt.Printf("Added secret %q to the repo %v/%v\n", secretName, *owner, *repo)
 }
 
 func getSecretName() (string, error) {
-	if len(os.Args) < 2 {
-		err := errors.New("no secrets passed in the go run command. pass in a secret name and then ensure to have the secret variable as an environment variable")
+	secretName := flag.Arg(0)
+	if secretName == "" {
+		err := fmt.Errorf("missing argument secret name")
 		return "", err
 	}
-	args := os.Args
-	secret := args[3:]
-	return secret[0], nil
+	return secretName, nil
 }
 
 func getSecretValue(secretName string) (string, error) {
 	secretValue := os.Getenv(secretName)
 	if secretValue == "" {
-		return "", errors.New(fmt.Sprintf(`secret with name: %s not defined as an environment variable, 
-please export the variable as an enviornment variable so it can be read in
-
-ex:
-	export %s="secret value"
-`, secretName, secretName))
+		err := fmt.Errorf("secret value not found under env variable %q", secretName)
+		return "", err
 	}
 	return secretValue, nil
 }
 
-// GithubAuth reads an api token from the environment
-// expecting API_GITHUB_TOKEN and authenticates with github api
-// and returns an authenticated client.
-func GithubAuth() (context.Context, *github.Client, error) {
-	token := os.Getenv("API_GITHUB_TOKEN")
-	if token == "" {
-		return nil, nil, errors.New(`no API_GITHUB_TOKEN was found in the environment variables.
-This is needed to authenticate with github. Please generate a github token that has access to do what you are trying to do
-and export it as an environment variable.
-
-ex:
-	export API_GITHUB_TOKEN="<token contents string>"
-`)
-	}
+// githubAuth returns a GitHub client and context
+// It reads an environment variable GITHUB_AUTH_TOKEN
+// for a GitHub API token with secret read/write permissions
+// cannot be the default token from GitHub actions
+func githubAuth(token string) (context.Context, *github.Client, error) {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: token},
@@ -135,43 +111,49 @@ ex:
 	return ctx, client, nil
 }
 
-// AddRepoSecret will add a secret value to a given github repo for a given owner
-// It encrypts the secret using sodium before sending the secret to github api
-// therefore requires libsodium to be installed on the machine running this code
+// addRepoSecret will add a secret to a GitHub repo for use in GitHub Actions
+//
+// To communicate with GitHub addRepoSecret requires a client and context.
+// To determine what repository to add this secret to it will use the owner and repo combination
+// Finally the secretName and secretValue will determine the name of the secret added and it's corresponding value
+//
+// The actual transmission of the secret value to GitHub using the api requires that the secret value is encrypted
+// using the public key of the target repo. This encryption must be done using sodium. This function has a hard
+// dependency on sodium being installed where this is run. Sodium can be installed from this url:
 // https://formulae.brew.sh/formula/libsodium
-// Github is very picky over formats things need to be to be sent, and not very descriptive in how
-// they will be sent to the user
-// To get a secret uploaded you need to get the public key of the repo that will be receiving the
-// secret. This key is used to encrypt the secret before transport on the sending side, and then decrypted by github.
-// Once you have the public key you need to encrypt the secret with sodiumlib before sending it.
-// The public key comes base64 encoded, and sodiumlib expects it to not be base64 encoded so you need
-// to decode the public key before using it.
-// Once the public key is decoded you need to convert the string secret into bytes.
-// once you have the public key decoded, and the secret string in bytes you can encrypt it using
-// sodium.CryptoBoxSeal
-// That will produce the correctly encrypted secret as bytes, but you need to then convert it to
-// a base64 encoded string. After doing that you can use that base64 encoded string as the encrypted value
-// to be part of the github.EncodedSecret type.
-// The name is the string (no encoding, or base64 needed) of the secret name that will appear in github secrets
-// then the KeyID will be the public key of the repo's ID, which is gettable from the public key's GetKeyID method.
-// Finally you can pass that object in and have it be created or updated in github.
-func AddRepoSecret(ctx context.Context, client *github.Client, owner string, repo string, secretName string, secretValue string) (string, error) {
+//
+// First step of the upload process by this function is to get the public key of the repo. The public key comes base64
+// encoded, so it must be decoded prior to use in sodiumlib.
+//
+// Second the secret value starts as a string type, but must be converted into a slice of bytes.
+// Third the decoded public key of the repo is used to encrypt the secret with sodium.CryptoBoxSeal resulting
+// in a secret encrypted as bytes
+//
+// Finally the secret bytes need to be encoded as a base64 string and used in a github.EncodedSecret type
+// that is passed into the GitHub client.Actions.CreateOrUpdateRepoSecret method to populate the secret in GitHub.
+//
+// The other two properties of the github.EncodedSecret type are the name of the secret to be added (string not base64)
+// and the KeyID of the public key used to encrypt the secret, which is gettable from the public key's GetKeyID method.
+//
+// Finally it passes in the github.EncodedSecret object to CreateOrUpdateRepoSecret which creates or updates the secret
+// in GitHub
+func addRepoSecret(ctx context.Context, client *github.Client, owner string, repo string, secretName string, secretValue string) error {
 	publicKey, _, err := client.Actions.GetRepoPublicKey(ctx, owner, repo)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	encryptedSecret, err := encryptSecretWithPublicKey(publicKey, secretName, secretValue)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	_, err = client.Actions.CreateOrUpdateRepoSecret(ctx, owner, repo, encryptedSecret)
 	if err != nil {
-		return "", errors.New(fmt.Sprintf("Actions.CreateOrUpdateRepoSecret returned error: %v", err))
+		return fmt.Errorf("Actions.CreateOrUpdateRepoSecret returned error: %v", err)
 	}
 
-	return secretName, nil
+	return nil
 }
 
 func encryptSecretWithPublicKey(publicKey *github.PublicKey, secretName string, secretValue string) (*github.EncryptedSecret, error) {
