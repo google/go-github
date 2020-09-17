8da2410a408643f0a1781c0f748b9c3b7039402b
Jacob Valdemar <jan@lunar.app>
2020-09-17 17:20:12 +0200

    Add Base, Ref, and SHA to EditChange struct (#1619)
    Fixes: #1617.

---
+++ github/event_types_test.go
@@ -0,0 +1,105 @@
+// Copyright 2020 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"testing"
+)
+
+func TestEditChange_Marshal_TitleChange(t *testing.T) {
+	testJSONMarshal(t, &EditChange{}, "{}")
+
+	TitleFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("TitleFrom"),
+	}
+
+	u := &EditChange{
+		Title: &TitleFrom,
+		Body:  nil,
+		Base:  nil,
+	}
+
+	want := `{
+		"title": {
+			"from": "TitleFrom"
+		  }
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestEditChange_Marshal_BodyChange(t *testing.T) {
+	testJSONMarshal(t, &EditChange{}, "{}")
+
+	BodyFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("BodyFrom"),
+	}
+
+	u := &EditChange{
+		Title: nil,
+		Body:  &BodyFrom,
+		Base:  nil,
+	}
+
+	want := `{
+		"body": {
+			"from": "BodyFrom"
+		  }
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestEditChange_Marshal_BaseChange(t *testing.T) {
+	testJSONMarshal(t, &EditChange{}, "{}")
+
+	RefFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("BaseRefFrom"),
+	}
+
+	SHAFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("BaseSHAFrom"),
+	}
+
+	Base := struct {
+		Ref *struct {
+			From *string `json:"from,omitempty"`
+		} `json:"ref,omitempty"`
+		SHA *struct {
+			From *string `json:"from,omitempty"`
+		} `json:"sha,omitempty"`
+	}{
+		Ref: &RefFrom,
+		SHA: &SHAFrom,
+	}
+
+	u := &EditChange{
+		Title: nil,
+		Body:  nil,
+		Base:  &Base,
+	}
+
+	want := `{
+		"base": {
+			"ref": {
+				"from": "BaseRefFrom"
+			},
+			"sha": {
+				"from": "BaseSHAFrom"
+			}
+		}
+	}`
+
+	testJSONMarshal(t, u, want)
+}
