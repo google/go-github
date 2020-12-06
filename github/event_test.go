a5a4c848cac60bb0467d2c373230df39d48d01cb
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-06 17:56:19 -0500

    Add event_test.go to increase code coverage

---
+++ github/event_test.go
@@ -0,0 +1,29 @@
+// Copyright 2020 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestPayload_Panic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Payload did not panic but should have")
+		}
+	}()
+
+	e := &Event{}
+	e.Payload()
+}
+
+func TestPayload_NoPanic(t *testing.T) {
+	name := "UserEvent"
+	body := json.RawMessage("{}")
+	e := &Event{Type: &name, RawPayload: &body}
+	e.Payload()
+}
