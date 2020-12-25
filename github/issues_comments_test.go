49f80c7d8af430f89fc5717b82678ffe0fc1276c
jlaportebot <jlaportebot@users.noreply.github.com>
2020-12-25 18:49:04 -0500

    Improve issues_comments.go coverage (#1755)

--- github/issues_comments_test.go
+++ github/issues_comments_test.go
@@ -44,6 +44,20 @@ func TestIssuesService_ListComments_allIssues(t *testing.T) {
 	if !reflect.DeepEqual(comments, want) {
 		t.Errorf("Issues.ListComments returned %+v, want %+v", comments, want)
 	}
+
+	const methodName = "ListComments"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Issues.ListComments(ctx, "\n", "\n", -1, opt)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Issues.ListComments(ctx, "o", "r", 0, opt)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestIssuesService_ListComments_specificIssue(t *testing.T) {
@@ -66,6 +80,20 @@ func TestIssuesService_ListComments_specificIssue(t *testing.T) {
 	if !reflect.DeepEqual(comments, want) {
 		t.Errorf("Issues.ListComments returned %+v, want %+v", comments, want)
 	}
+
+	const methodName = "ListComments"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Issues.ListComments(ctx, "\n", "\n", -1, nil)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Issues.ListComments(ctx, "o", "r", 1, nil)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestIssuesService_ListComments_invalidOwner(t *testing.T) {
@@ -97,6 +125,20 @@ func TestIssuesService_GetComment(t *testing.T) {
 	if !reflect.DeepEqual(comment, want) {
 		t.Errorf("Issues.GetComment returned %+v, want %+v", comment, want)
 	}
+
+	const methodName = "GetComment"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Issues.GetComment(ctx, "\n", "\n", -1)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Issues.GetComment(ctx, "o", "r", 1)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestIssuesService_GetComment_invalidOrg(t *testing.T) {
@@ -136,6 +178,20 @@ func TestIssuesService_CreateComment(t *testing.T) {
 	if !reflect.DeepEqual(comment, want) {
 		t.Errorf("Issues.CreateComment returned %+v, want %+v", comment, want)
 	}
+
+	const methodName = "CreateComment"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Issues.CreateComment(ctx, "\n", "\n", -1, input)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Issues.CreateComment(ctx, "o", "r", 1, input)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestIssuesService_CreateComment_invalidOrg(t *testing.T) {
@@ -175,6 +231,20 @@ func TestIssuesService_EditComment(t *testing.T) {
 	if !reflect.DeepEqual(comment, want) {
 		t.Errorf("Issues.EditComment returned %+v, want %+v", comment, want)
 	}
+
+	const methodName = "EditComment"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Issues.EditComment(ctx, "\n", "\n", -1, input)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Issues.EditComment(ctx, "o", "r", 1, input)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestIssuesService_EditComment_invalidOwner(t *testing.T) {
@@ -199,6 +269,16 @@ func TestIssuesService_DeleteComment(t *testing.T) {
 	if err != nil {
 		t.Errorf("Issues.DeleteComments returned error: %v", err)
 	}
+
+	const methodName = "DeleteComment"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Issues.DeleteComment(ctx, "\n", "\n", -1)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Issues.DeleteComment(ctx, "o", "r", 1)
+	})
 }
 
 func TestIssuesService_DeleteComment_invalidOwner(t *testing.T) {
