75e15e5743feb86dad0d03bbe6ea0da2b71dd499
jlaportebot <jlaportebot@users.noreply.github.com>
2020-12-25 18:50:57 -0500

    Improve repos_hooks.go coverage (#1750)

--- github/repos_hooks_test.go
+++ github/repos_hooks_test.go
@@ -43,6 +43,20 @@ func TestRepositoriesService_CreateHook(t *testing.T) {
 	if !reflect.DeepEqual(hook, want) {
 		t.Errorf("Repositories.CreateHook returned %+v, want %+v", hook, want)
 	}
+
+	const methodName = "CreateHook"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Repositories.CreateHook(ctx, "\n", "\n", input)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Repositories.CreateHook(ctx, "o", "r", input)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestRepositoriesService_ListHooks(t *testing.T) {
@@ -67,6 +81,20 @@ func TestRepositoriesService_ListHooks(t *testing.T) {
 	if !reflect.DeepEqual(hooks, want) {
 		t.Errorf("Repositories.ListHooks returned %+v, want %+v", hooks, want)
 	}
+
+	const methodName = "ListHooks"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Repositories.ListHooks(ctx, "\n", "\n", opt)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Repositories.ListHooks(ctx, "o", "r", opt)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestRepositoriesService_ListHooks_invalidOwner(t *testing.T) {
@@ -97,6 +125,20 @@ func TestRepositoriesService_GetHook(t *testing.T) {
 	if !reflect.DeepEqual(hook, want) {
 		t.Errorf("Repositories.GetHook returned %+v, want %+v", hook, want)
 	}
+
+	const methodName = "GetHook"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Repositories.GetHook(ctx, "\n", "\n", -1)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Repositories.GetHook(ctx, "o", "r", 1)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestRepositoriesService_GetHook_invalidOwner(t *testing.T) {
@@ -136,6 +178,20 @@ func TestRepositoriesService_EditHook(t *testing.T) {
 	if !reflect.DeepEqual(hook, want) {
 		t.Errorf("Repositories.EditHook returned %+v, want %+v", hook, want)
 	}
+
+	const methodName = "EditHook"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Repositories.EditHook(ctx, "\n", "\n", -1, input)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Repositories.EditHook(ctx, "o", "r", 1, input)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestRepositoriesService_EditHook_invalidOwner(t *testing.T) {
@@ -160,6 +216,16 @@ func TestRepositoriesService_DeleteHook(t *testing.T) {
 	if err != nil {
 		t.Errorf("Repositories.DeleteHook returned error: %v", err)
 	}
+
+	const methodName = "DeleteHook"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Repositories.DeleteHook(ctx, "\n", "\n", -1)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Repositories.DeleteHook(ctx, "o", "r", 1)
+	})
 }
 
 func TestRepositoriesService_DeleteHook_invalidOwner(t *testing.T) {
@@ -184,6 +250,16 @@ func TestRepositoriesService_PingHook(t *testing.T) {
 	if err != nil {
 		t.Errorf("Repositories.PingHook returned error: %v", err)
 	}
+
+	const methodName = "PingHook"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Repositories.PingHook(ctx, "\n", "\n", -1)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Repositories.PingHook(ctx, "o", "r", 1)
+	})
 }
 
 func TestRepositoriesService_TestHook(t *testing.T) {
@@ -199,6 +275,16 @@ func TestRepositoriesService_TestHook(t *testing.T) {
 	if err != nil {
 		t.Errorf("Repositories.TestHook returned error: %v", err)
 	}
+
+	const methodName = "TestHook"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Repositories.TestHook(ctx, "\n", "\n", -1)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Repositories.TestHook(ctx, "o", "r", 1)
+	})
 }
 
 func TestRepositoriesService_TestHook_invalidOwner(t *testing.T) {
