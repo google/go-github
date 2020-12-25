b66aa77cda0b7f01eeb9fb3d4511f4959a2cab9a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-25 16:49:26 -0500

    Improve actions_workflows.go coverage

--- github/actions_workflows_test.go
+++ github/actions_workflows_test.go
@@ -42,6 +42,20 @@ func TestActionsService_ListWorkflows(t *testing.T) {
 	if !reflect.DeepEqual(workflows, want) {
 		t.Errorf("Actions.ListWorkflows returned %+v, want %+v", workflows, want)
 	}
+
+	const methodName = "ListWorkflows"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Actions.ListWorkflows(ctx, "\n", "\n", opts)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Actions.ListWorkflows(ctx, "o", "r", opts)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActionsService_GetWorkflowByID(t *testing.T) {
@@ -67,6 +81,20 @@ func TestActionsService_GetWorkflowByID(t *testing.T) {
 	if !reflect.DeepEqual(workflow, want) {
 		t.Errorf("Actions.GetWorkflowByID returned %+v, want %+v", workflow, want)
 	}
+
+	const methodName = "GetWorkflowByID"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Actions.GetWorkflowByID(ctx, "\n", "\n", -72844)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Actions.GetWorkflowByID(ctx, "o", "r", 72844)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActionsService_GetWorkflowByFileName(t *testing.T) {
@@ -92,6 +120,20 @@ func TestActionsService_GetWorkflowByFileName(t *testing.T) {
 	if !reflect.DeepEqual(workflow, want) {
 		t.Errorf("Actions.GetWorkflowByFileName returned %+v, want %+v", workflow, want)
 	}
+
+	const methodName = "GetWorkflowByFileName"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Actions.GetWorkflowByFileName(ctx, "\n", "\n", "\n")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Actions.GetWorkflowByFileName(ctx, "o", "r", "main.yml")
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActionsService_GetWorkflowUsageByID(t *testing.T) {
@@ -125,6 +167,20 @@ func TestActionsService_GetWorkflowUsageByID(t *testing.T) {
 	if !reflect.DeepEqual(workflowUsage, want) {
 		t.Errorf("Actions.GetWorkflowUsageByID returned %+v, want %+v", workflowUsage, want)
 	}
+
+	const methodName = "GetWorkflowUsageByID"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Actions.GetWorkflowUsageByID(ctx, "\n", "\n", -72844)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Actions.GetWorkflowUsageByID(ctx, "o", "r", 72844)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActionsService_GetWorkflowUsageByFileName(t *testing.T) {
@@ -158,6 +214,20 @@ func TestActionsService_GetWorkflowUsageByFileName(t *testing.T) {
 	if !reflect.DeepEqual(workflowUsage, want) {
 		t.Errorf("Actions.GetWorkflowUsageByFileName returned %+v, want %+v", workflowUsage, want)
 	}
+
+	const methodName = "GetWorkflowUsageByFileName"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Actions.GetWorkflowUsageByFileName(ctx, "\n", "\n", "\n")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Actions.GetWorkflowUsageByFileName(ctx, "o", "r", "main.yml")
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActionsService_CreateWorkflowDispatchEventByID(t *testing.T) {
@@ -192,6 +262,16 @@ func TestActionsService_CreateWorkflowDispatchEventByID(t *testing.T) {
 	if err == nil {
 		t.Error("client.BaseURL.Path='' CreateWorkflowDispatchEventByID err = nil, want error")
 	}
+
+	const methodName = "CreateWorkflowDispatchEventByID"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Actions.CreateWorkflowDispatchEventByID(ctx, "o", "r", 72844, event)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Actions.CreateWorkflowDispatchEventByID(ctx, "o", "r", 72844, event)
+	})
 }
 
 func TestActionsService_CreateWorkflowDispatchEventByFileName(t *testing.T) {
@@ -226,6 +306,16 @@ func TestActionsService_CreateWorkflowDispatchEventByFileName(t *testing.T) {
 	if err == nil {
 		t.Error("client.BaseURL.Path='' CreateWorkflowDispatchEventByFileName err = nil, want error")
 	}
+
+	const methodName = "CreateWorkflowDispatchEventByFileName"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Actions.CreateWorkflowDispatchEventByFileName(ctx, "o", "r", "main.yml", event)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Actions.CreateWorkflowDispatchEventByFileName(ctx, "o", "r", "main.yml", event)
+	})
 }
 
 func TestActionsService_EnableWorkflowByID(t *testing.T) {
@@ -251,6 +341,16 @@ func TestActionsService_EnableWorkflowByID(t *testing.T) {
 	if err == nil {
 		t.Error("client.BaseURL.Path='' EnableWorkflowByID err = nil, want error")
 	}
+
+	const methodName = "EnableWorkflowByID"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Actions.EnableWorkflowByID(ctx, "o", "r", 72844)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Actions.EnableWorkflowByID(ctx, "o", "r", 72844)
+	})
 }
 
 func TestActionsService_EnableWorkflowByFilename(t *testing.T) {
@@ -276,6 +376,16 @@ func TestActionsService_EnableWorkflowByFilename(t *testing.T) {
 	if err == nil {
 		t.Error("client.BaseURL.Path='' EnableWorkflowByFilename err = nil, want error")
 	}
+
+	const methodName = "EnableWorkflowByFileName"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Actions.EnableWorkflowByFileName(ctx, "o", "r", "main.yml")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Actions.EnableWorkflowByFileName(ctx, "o", "r", "main.yml")
+	})
 }
 
 func TestActionsService_DisableWorkflowByID(t *testing.T) {
@@ -301,6 +411,16 @@ func TestActionsService_DisableWorkflowByID(t *testing.T) {
 	if err == nil {
 		t.Error("client.BaseURL.Path='' DisableWorkflowByID err = nil, want error")
 	}
+
+	const methodName = "DisableWorkflowByID"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Actions.DisableWorkflowByID(ctx, "o", "r", 72844)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Actions.DisableWorkflowByID(ctx, "o", "r", 72844)
+	})
 }
 
 func TestActionsService_DisableWorkflowByFileName(t *testing.T) {
@@ -326,4 +446,14 @@ func TestActionsService_DisableWorkflowByFileName(t *testing.T) {
 	if err == nil {
 		t.Error("client.BaseURL.Path='' DisableWorkflowByFileName err = nil, want error")
 	}
+
+	const methodName = "DisableWorkflowByFileName"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Actions.DisableWorkflowByFileName(ctx, "o", "r", "main.yml")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Actions.DisableWorkflowByFileName(ctx, "o", "r", "main.yml")
+	})
 }
