d9418a9f5d07fb73352e4921b9fcbdb6a66759e7
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2020-12-25 12:41:05 -0500

    Improve activity_watching.go coverage

--- github/activity_watching_test.go
+++ github/activity_watching_test.go
@@ -37,6 +37,20 @@ func TestActivityService_ListWatchers(t *testing.T) {
 	if !reflect.DeepEqual(watchers, want) {
 		t.Errorf("Activity.ListWatchers returned %+v, want %+v", watchers, want)
 	}
+
+	const methodName = "ListWatchers"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.ListWatchers(ctx, "\n", "\n", &ListOptions{Page: 2})
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.ListWatchers(ctx, "o", "r", &ListOptions{Page: 2})
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_ListWatched_authenticatedUser(t *testing.T) {
@@ -61,6 +75,20 @@ func TestActivityService_ListWatched_authenticatedUser(t *testing.T) {
 	if !reflect.DeepEqual(watched, want) {
 		t.Errorf("Activity.ListWatched returned %+v, want %+v", watched, want)
 	}
+
+	const methodName = "ListWatched"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.ListWatched(ctx, "\n", &ListOptions{Page: 2})
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.ListWatched(ctx, "", &ListOptions{Page: 2})
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_ListWatched_specifiedUser(t *testing.T) {
@@ -85,6 +113,20 @@ func TestActivityService_ListWatched_specifiedUser(t *testing.T) {
 	if !reflect.DeepEqual(watched, want) {
 		t.Errorf("Activity.ListWatched returned %+v, want %+v", watched, want)
 	}
+
+	const methodName = "ListWatched"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.ListWatched(ctx, "\n", &ListOptions{Page: 2})
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.ListWatched(ctx, "u", &ListOptions{Page: 2})
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_GetRepositorySubscription_true(t *testing.T) {
@@ -106,6 +148,20 @@ func TestActivityService_GetRepositorySubscription_true(t *testing.T) {
 	if !reflect.DeepEqual(sub, want) {
 		t.Errorf("Activity.GetRepositorySubscription returned %+v, want %+v", sub, want)
 	}
+
+	const methodName = "GetRepositorySubscription"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.GetRepositorySubscription(ctx, "\n", "\n")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.GetRepositorySubscription(ctx, "o", "r")
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_GetRepositorySubscription_false(t *testing.T) {
@@ -127,6 +183,20 @@ func TestActivityService_GetRepositorySubscription_false(t *testing.T) {
 	if !reflect.DeepEqual(sub, want) {
 		t.Errorf("Activity.GetRepositorySubscription returned %+v, want %+v", sub, want)
 	}
+
+	const methodName = "GetRepositorySubscription"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.GetRepositorySubscription(ctx, "\n", "\n")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.GetRepositorySubscription(ctx, "o", "r")
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_GetRepositorySubscription_error(t *testing.T) {
@@ -143,6 +213,20 @@ func TestActivityService_GetRepositorySubscription_error(t *testing.T) {
 	if err == nil {
 		t.Errorf("Expected HTTP 400 response")
 	}
+
+	const methodName = "GetRepositorySubscription"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.GetRepositorySubscription(ctx, "\n", "\n")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.GetRepositorySubscription(ctx, "o", "r")
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_SetRepositorySubscription(t *testing.T) {
@@ -173,6 +257,20 @@ func TestActivityService_SetRepositorySubscription(t *testing.T) {
 	if !reflect.DeepEqual(sub, want) {
 		t.Errorf("Activity.SetRepositorySubscription returned %+v, want %+v", sub, want)
 	}
+
+	const methodName = "SetRepositorySubscription"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.Activity.SetRepositorySubscription(ctx, "\n", "\n", input)
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Activity.SetRepositorySubscription(ctx, "o", "r", input)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
 }
 
 func TestActivityService_DeleteRepositorySubscription(t *testing.T) {
@@ -189,4 +287,14 @@ func TestActivityService_DeleteRepositorySubscription(t *testing.T) {
 	if err != nil {
 		t.Errorf("Activity.DeleteRepositorySubscription returned error: %v", err)
 	}
+
+	const methodName = "DeleteRepositorySubscription"
+	testBadOptions(t, methodName, func() (err error) {
+		_, err = client.Activity.DeleteRepositorySubscription(ctx, "\n", "\n")
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		return client.Activity.DeleteRepositorySubscription(ctx, "o", "r")
+	})
 }
