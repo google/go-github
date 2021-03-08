8cf1049c9cae80f1f382bc41e1a51113634b4ae7
Avinash Sridhar <sridharavinash@github.com>
2021-03-08 18:10:21 -0500

    use enterprise as var name instead of org

--- github/enterprise_audit_log.go
+++ github/enterprise_audit_log.go
@@ -13,8 +13,8 @@ import (
 // GetAuditLog gets the audit-log entries for an organization
 //
 // GitHub API docs: https://docs.github.com/en/rest/reference/orgs#get-the-audit-log-for-an-organization
-func (s *EnterpriseService) GetAuditLog(ctx context.Context, org string, opts *GetAuditLogOptions) ([]*AuditEntry, *Response, error) {
-	u := fmt.Sprintf("enterprises/%v/audit-log", org)
+func (s *EnterpriseService) GetAuditLog(ctx context.Context, enterprise string, opts *GetAuditLogOptions) ([]*AuditEntry, *Response, error) {
+	u := fmt.Sprintf("enterprises/%v/audit-log", enterprise)
 	u, err := addOptions(u, opts)
 	if err != nil {
 		return nil, nil, err
