805702649fb8c0cbff127762fccacc8f46b40080
jlaportebot <jlaportebot@users.noreply.github.com>
2021-04-08 08:46:53 -0400

    Add Support For Millisecond Timestamps (#1850)
    Fixes: #1849.

--- github/orgs_audit_log_test.go
+++ github/orgs_audit_log_test.go
@@ -62,7 +62,8 @@ func TestOrganizationService_GetAuditLog(t *testing.T) {
 	}
 	startedAt, _ := time.Parse(time.RFC3339, "2021-03-07T00:33:04.000Z")
 	completedAt, _ := time.Parse(time.RFC3339, "2021-03-07T00:35:08.000Z")
-	timestamp := time.Unix(1615077308538, 0)
+	timestamp := time.Unix(0, 1615077308538*1e6)
+
 	want := []*AuditEntry{
 		{
 			Timestamp:     &Timestamp{timestamp},
