77a5f59b56b7812f1c792972731439626893567a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2021-05-12 09:29:46 -0400

    Change all *map to map in JSON structs (#1870)
    Fixes: #1865.

--- github/github-accessors_test.go
+++ github/github-accessors_test.go
@@ -14554,8 +14554,8 @@ func TestRepository_GetParent(tt *testing.T) {
 }
 
 func TestRepository_GetPermissions(tt *testing.T) {
-	var zeroValue map[string]bool
-	r := &Repository{Permissions: &zeroValue}
+	zeroValue := map[string]bool{}
+	r := &Repository{Permissions: zeroValue}
 	r.GetPermissions()
 	r = &Repository{}
 	r.GetPermissions()
@@ -16782,8 +16782,8 @@ func TestTeam_GetPermission(tt *testing.T) {
 }
 
 func TestTeam_GetPermissions(tt *testing.T) {
-	var zeroValue map[string]bool
-	t := &Team{Permissions: &zeroValue}
+	zeroValue := map[string]bool{}
+	t := &Team{Permissions: zeroValue}
 	t.GetPermissions()
 	t = &Team{}
 	t.GetPermissions()
@@ -18076,8 +18076,8 @@ func TestUser_GetOwnedPrivateRepos(tt *testing.T) {
 }
 
 func TestUser_GetPermissions(tt *testing.T) {
-	var zeroValue map[string]bool
-	u := &User{Permissions: &zeroValue}
+	zeroValue := map[string]bool{}
+	u := &User{Permissions: zeroValue}
 	u.GetPermissions()
 	u = &User{}
 	u.GetPermissions()
