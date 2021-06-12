4ae50c20f426b047877341b37f70aac4067a6538
Will Norris <will@willnorris.com>
2021-06-12 16:42:33 -0700

    support go1.17 build constraint syntax
    https://tip.golang.org/doc/go1.17
    
    Fixes #1890

--- github/gen-stringify-test.go
+++ github/gen-stringify-test.go
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build ignore
 // +build ignore
 
 // gen-stringify-test generates test methods to test the String methods.
