d00af2e90307b1709079b860acf6ba37969d72d2
sagar23sj <sagarsonwane23@gmail.com>
2021-06-22 01:14:10 +0530

    test-cases: Added test-cases for resource JSON Marshalling
    Resources Covered :
    - Page
    - TeamChange
    
    Adds fix to : #55

--- github/event_types_test.go
+++ github/event_types_test.go
@@ -103,3 +103,159 @@ func TestEditChange_Marshal_BaseChange(t *testing.T) {
 
 	testJSONMarshal(t, u, want)
 }
+
+func TestPage_Marshal(t *testing.T) {
+	testJSONMarshal(t, &Page{}, "{}")
+
+	u := &Page{
+		PageName: String("p"),
+		Title:    String("t"),
+		Summary:  String("s"),
+		Action:   String("a"),
+		SHA:      String("s"),
+		HTMLURL:  String("h"),
+	}
+
+	want := `{
+		"page_name": "p",
+		"title": "t",
+		"summary": "s",
+		"action": "a",
+		"sha": "s",
+		"html_url": "h"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestTeamChange_Marshal_DescriptionChange(t *testing.T) {
+	testJSONMarshal(t, &TeamChange{}, "{}")
+
+	DescriptionFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("DescriptionFrom"),
+	}
+
+	u := &TeamChange{
+		Description: &DescriptionFrom,
+		Name:        nil,
+		Privacy:     nil,
+		Repository:  nil,
+	}
+
+	want := `{
+		"description": {
+			"from": "DescriptionFrom"
+		  }
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestTeamChange_Marshal_NameChange(t *testing.T) {
+	testJSONMarshal(t, &TeamChange{}, "{}")
+
+	NameFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("NameFrom"),
+	}
+
+	u := &TeamChange{
+		Description: nil,
+		Name:        &NameFrom,
+		Privacy:     nil,
+		Repository:  nil,
+	}
+
+	want := `{
+		"name": {
+			"from": "NameFrom"
+		  }
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestTeamChange_Marshal_PrivacyChange(t *testing.T) {
+	testJSONMarshal(t, &TeamChange{}, "{}")
+
+	PrivacyFrom := struct {
+		From *string `json:"from,omitempty"`
+	}{
+		From: String("PrivacyFrom"),
+	}
+
+	u := &TeamChange{
+		Description: nil,
+		Name:        nil,
+		Privacy:     &PrivacyFrom,
+		Repository:  nil,
+	}
+
+	want := `{
+		"privacy": {
+			"from": "PrivacyFrom"
+		  }
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestTeamChange_Marshal_RepositoryChange(t *testing.T) {
+	testJSONMarshal(t, &TeamChange{}, "{}")
+
+	From := struct {
+		Admin *bool `json:"admin,omitempty"`
+		Pull  *bool `json:"pull,omitempty"`
+		Push  *bool `json:"push,omitempty"`
+	}{
+		Admin: Bool(true),
+		Pull:  Bool(true),
+		Push:  Bool(false),
+	}
+
+	PermissionsFrom := struct {
+		From *struct {
+			Admin *bool `json:"admin,omitempty"`
+			Pull  *bool `json:"pull,omitempty"`
+			Push  *bool `json:"push,omitempty"`
+		} `json:"from,omitempty"`
+	}{
+		From: &From,
+	}
+
+	Repository := struct {
+		Permissions *struct {
+			From *struct {
+				Admin *bool `json:"admin,omitempty"`
+				Pull  *bool `json:"pull,omitempty"`
+				Push  *bool `json:"push,omitempty"`
+			} `json:"from,omitempty"`
+		} `json:"permissions,omitempty"`
+	}{
+		Permissions: &PermissionsFrom,
+	}
+
+	u := &TeamChange{
+		Description: nil,
+		Name:        nil,
+		Privacy:     nil,
+		Repository:  &Repository,
+	}
+
+	want := `{
+		"repository": {
+			"permissions": {
+				"from": {
+					"admin": true,
+					"pull": true,
+					"push": false
+				}
+			}
+		  }
+	}`
+
+	testJSONMarshal(t, u, want)
+}
