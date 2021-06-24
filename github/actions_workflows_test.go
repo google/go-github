7b534cc16d7427e989307ddfb7c23a6f64259dd8
sagar23sj <sagarsonwane23@gmail.com>
2021-06-24 16:49:22 +0530

    Resource Tested for JSON marshalling : WorkflowUsage

--- github/actions_workflows_test.go
+++ github/actions_workflows_test.go
@@ -574,3 +574,37 @@ func TestWorkflowEnvironment_Marshal(t *testing.T) {
 
 	testJSONMarshal(t, u, want)
 }
+
+func TestWorkflowUsage_Marshal(t *testing.T) {
+	testJSONMarshal(t, &WorkflowUsage{}, "{}")
+
+	u := &WorkflowUsage{
+		Billable: &WorkflowEnvironment{
+			Ubuntu: &WorkflowBill{
+				TotalMS: Int64(1),
+			},
+			MacOS: &WorkflowBill{
+				TotalMS: Int64(1),
+			},
+			Windows: &WorkflowBill{
+				TotalMS: Int64(1),
+			},
+		},
+	}
+
+	want := `{
+		"billable": {
+			"UBUNTU": {
+				"total_ms": 1
+			},
+			"MACOS": {
+				"total_ms": 1
+			},
+			"WINDOWS": {
+				"total_ms": 1
+			}
+		}
+	}`
+
+	testJSONMarshal(t, u, want)
+}
