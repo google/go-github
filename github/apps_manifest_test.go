066918d3cbd260d0aacfcce8b619f7b657d999e5
jlaportebot <jlaportebot@users.noreply.github.com>
2021-07-09 02:01:21 +0530

    Add test cases for JSON resource marshaling (#1926)

--- github/apps_manifest_test.go
+++ github/apps_manifest_test.go
@@ -66,3 +66,79 @@ func TestGetConfig(t *testing.T) {
 		return resp, err
 	})
 }
+
+func TestAppConfig_Marshal(t *testing.T) {
+	testJSONMarshal(t, &AppConfig{}, "{}")
+
+	u := &AppConfig{
+		ID:     Int64(1),
+		NodeID: String("nid"),
+		Owner: &User{
+			Login:           String("l"),
+			ID:              Int64(1),
+			URL:             String("u"),
+			AvatarURL:       String("a"),
+			GravatarID:      String("g"),
+			Name:            String("n"),
+			Company:         String("c"),
+			Blog:            String("b"),
+			Location:        String("l"),
+			Email:           String("e"),
+			Hireable:        Bool(true),
+			Bio:             String("b"),
+			TwitterUsername: String("t"),
+			PublicRepos:     Int(1),
+			Followers:       Int(1),
+			Following:       Int(1),
+			CreatedAt:       &Timestamp{referenceTime},
+			SuspendedAt:     &Timestamp{referenceTime},
+		},
+		Name:          String("n"),
+		Description:   String("d"),
+		ExternalURL:   String("eu"),
+		HTMLURL:       String("hu"),
+		CreatedAt:     &Timestamp{referenceTime},
+		UpdatedAt:     &Timestamp{referenceTime},
+		ClientID:      String("ci"),
+		ClientSecret:  String("cs"),
+		WebhookSecret: String("ws"),
+		PEM:           String("pem"),
+	}
+
+	want := `{
+		"id": 1,
+		"node_id": "nid",
+		"owner": {
+			"login": "l",
+			"id": 1,
+			"avatar_url": "a",
+			"gravatar_id": "g",
+			"name": "n",
+			"company": "c",
+			"blog": "b",
+			"location": "l",
+			"email": "e",
+			"hireable": true,
+			"bio": "b",
+			"twitter_username": "t",
+			"public_repos": 1,
+			"followers": 1,
+			"following": 1,
+			"created_at": ` + referenceTimeStr + `,
+			"suspended_at": ` + referenceTimeStr + `,
+			"url": "u"
+		},
+		"name": "n",
+		"description": "d",
+		"external_url": "eu",
+		"html_url": "hu",
+		"created_at": ` + referenceTimeStr + `,
+		"updated_at": ` + referenceTimeStr + `,
+		"client_id": "ci",
+		"client_secret": "cs",
+		"webhook_secret": "ws",
+		"pem": "pem"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
