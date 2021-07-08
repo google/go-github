8ec2d9e8957d32db628484d9540539107e398399
jlaportebot <jlaportebot@users.noreply.github.com>
2021-07-09 02:02:37 +0530

    Add test cases for JSON resource marshaling (#1928)

--- github/apps_test.go
+++ github/apps_test.go
@@ -594,3 +594,633 @@ func TestAppsService_FindUserInstallation(t *testing.T) {
 		return resp, err
 	})
 }
+
+func TestContentReference_Marshal(t *testing.T) {
+	testJSONMarshal(t, &ContentReference{}, "{}")
+
+	u := &ContentReference{
+		ID:        Int64(1),
+		NodeID:    String("nid"),
+		Reference: String("r"),
+	}
+
+	want := `{
+		"id": 1,
+		"node_id": "nid",
+		"reference": "r"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestAttachment_Marshal(t *testing.T) {
+	testJSONMarshal(t, &Attachment{}, "{}")
+
+	u := &Attachment{
+		ID:    Int64(1),
+		Title: String("t"),
+		Body:  String("b"),
+	}
+
+	want := `{
+		"id": 1,
+		"title": "t",
+		"body": "b"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestInstallationPermissions_Marshal(t *testing.T) {
+	testJSONMarshal(t, &InstallationPermissions{}, "{}")
+
+	u := &InstallationPermissions{
+		Actions:                       String("a"),
+		Administration:                String("ad"),
+		Checks:                        String("c"),
+		Contents:                      String("co"),
+		ContentReferences:             String("cr"),
+		Deployments:                   String("d"),
+		Environments:                  String("e"),
+		Issues:                        String("i"),
+		Metadata:                      String("md"),
+		Members:                       String("m"),
+		OrganizationAdministration:    String("oa"),
+		OrganizationHooks:             String("oh"),
+		OrganizationPlan:              String("op"),
+		OrganizationPreReceiveHooks:   String("opr"),
+		OrganizationProjects:          String("op"),
+		OrganizationSecrets:           String("os"),
+		OrganizationSelfHostedRunners: String("osh"),
+		OrganizationUserBlocking:      String("oub"),
+		Packages:                      String("pkg"),
+		Pages:                         String("pg"),
+		PullRequests:                  String("pr"),
+		RepositoryHooks:               String("rh"),
+		RepositoryProjects:            String("rp"),
+		RepositoryPreReceiveHooks:     String("rprh"),
+		Secrets:                       String("s"),
+		SecretScanningAlerts:          String("ssa"),
+		SecurityEvents:                String("se"),
+		SingleFile:                    String("sf"),
+		Statuses:                      String("s"),
+		TeamDiscussions:               String("td"),
+		VulnerabilityAlerts:           String("va"),
+		Workflows:                     String("w"),
+	}
+
+	want := `{
+		"actions": "a",
+		"administration": "ad",
+		"checks": "c",
+		"contents": "co",
+		"content_references": "cr",
+		"deployments": "d",
+		"environments": "e",
+		"issues": "i",
+		"metadata": "md",
+		"members": "m",
+		"organization_administration": "oa",
+		"organization_hooks": "oh",
+		"organization_plan": "op",
+		"organization_pre_receive_hooks": "opr",
+		"organization_projects": "op",
+		"organization_secrets": "os",
+		"organization_self_hosted_runners": "osh",
+		"organization_user_blocking": "oub",
+		"packages": "pkg",
+		"pages": "pg",
+		"pull_requests": "pr",
+		"repository_hooks": "rh",
+		"repository_projects": "rp",
+		"repository_pre_receive_hooks": "rprh",
+		"secrets": "s",
+		"secret_scanning_alerts": "ssa",
+		"security_events": "se",
+		"single_file": "sf",
+		"statuses": "s",
+		"team_discussions": "td",
+		"vulnerability_alerts":"va",
+		"workflows": "w"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestInstallation_Marshal(t *testing.T) {
+	testJSONMarshal(t, &Installation{}, "{}")
+
+	u := &Installation{
+		ID:       Int64(1),
+		NodeID:   String("nid"),
+		AppID:    Int64(1),
+		AppSlug:  String("as"),
+		TargetID: Int64(1),
+		Account: &User{
+			Login:           String("l"),
+			ID:              Int64(1),
+			URL:             String("u"),
+			AvatarURL:       String("a"),
+			GravatarID:      String("g"),
+			Name:            String("n"),
+			Company:         String("c"),
+			Blog:            String("b"),
+			Location:        String("l"),
+			Email:           String("e"),
+			Hireable:        Bool(true),
+			Bio:             String("b"),
+			TwitterUsername: String("t"),
+			PublicRepos:     Int(1),
+			Followers:       Int(1),
+			Following:       Int(1),
+			CreatedAt:       &Timestamp{referenceTime},
+			SuspendedAt:     &Timestamp{referenceTime},
+		},
+		AccessTokensURL:     String("atu"),
+		RepositoriesURL:     String("ru"),
+		HTMLURL:             String("hu"),
+		TargetType:          String("tt"),
+		SingleFileName:      String("sfn"),
+		RepositorySelection: String("rs"),
+		Events:              []string{"e"},
+		SingleFilePaths:     []string{"s"},
+		Permissions: &InstallationPermissions{
+			Actions:                       String("a"),
+			Administration:                String("ad"),
+			Checks:                        String("c"),
+			Contents:                      String("co"),
+			ContentReferences:             String("cr"),
+			Deployments:                   String("d"),
+			Environments:                  String("e"),
+			Issues:                        String("i"),
+			Metadata:                      String("md"),
+			Members:                       String("m"),
+			OrganizationAdministration:    String("oa"),
+			OrganizationHooks:             String("oh"),
+			OrganizationPlan:              String("op"),
+			OrganizationPreReceiveHooks:   String("opr"),
+			OrganizationProjects:          String("op"),
+			OrganizationSecrets:           String("os"),
+			OrganizationSelfHostedRunners: String("osh"),
+			OrganizationUserBlocking:      String("oub"),
+			Packages:                      String("pkg"),
+			Pages:                         String("pg"),
+			PullRequests:                  String("pr"),
+			RepositoryHooks:               String("rh"),
+			RepositoryProjects:            String("rp"),
+			RepositoryPreReceiveHooks:     String("rprh"),
+			Secrets:                       String("s"),
+			SecretScanningAlerts:          String("ssa"),
+			SecurityEvents:                String("se"),
+			SingleFile:                    String("sf"),
+			Statuses:                      String("s"),
+			TeamDiscussions:               String("td"),
+			VulnerabilityAlerts:           String("va"),
+			Workflows:                     String("w"),
+		},
+		CreatedAt:              &Timestamp{referenceTime},
+		UpdatedAt:              &Timestamp{referenceTime},
+		HasMultipleSingleFiles: Bool(false),
+		SuspendedBy: &User{
+			Login:           String("l"),
+			ID:              Int64(1),
+			URL:             String("u"),
+			AvatarURL:       String("a"),
+			GravatarID:      String("g"),
+			Name:            String("n"),
+			Company:         String("c"),
+			Blog:            String("b"),
+			Location:        String("l"),
+			Email:           String("e"),
+			Hireable:        Bool(true),
+			Bio:             String("b"),
+			TwitterUsername: String("t"),
+			PublicRepos:     Int(1),
+			Followers:       Int(1),
+			Following:       Int(1),
+			CreatedAt:       &Timestamp{referenceTime},
+			SuspendedAt:     &Timestamp{referenceTime},
+		},
+		SuspendedAt: &Timestamp{referenceTime},
+	}
+
+	want := `{
+		"id": 1,
+		"node_id": "nid",
+		"app_id": 1,
+		"app_slug": "as",
+		"target_id": 1,
+		"account": {
+			"login": "l",
+			"id": 1,
+			"avatar_url": "a",
+			"gravatar_id": "g",
+			"name": "n",
+			"company": "c",
+			"blog": "b",
+			"location": "l",
+			"email": "e",
+			"hireable": true,
+			"bio": "b",
+			"twitter_username": "t",
+			"public_repos": 1,
+			"followers": 1,
+			"following": 1,
+			"created_at": ` + referenceTimeStr + `,
+			"suspended_at": ` + referenceTimeStr + `,
+			"url": "u"
+		},
+		"access_tokens_url": "atu",
+		"repositories_url": "ru",
+		"html_url": "hu",
+		"target_type": "tt",
+		"single_file_name": "sfn",
+		"repository_selection": "rs",
+		"events": [
+			"e"
+		],
+		"single_file_paths": [
+			"s"
+		],
+		"permissions": {
+			"actions": "a",
+			"administration": "ad",
+			"checks": "c",
+			"contents": "co",
+			"content_references": "cr",
+			"deployments": "d",
+			"environments": "e",
+			"issues": "i",
+			"metadata": "md",
+			"members": "m",
+			"organization_administration": "oa",
+			"organization_hooks": "oh",
+			"organization_plan": "op",
+			"organization_pre_receive_hooks": "opr",
+			"organization_projects": "op",
+			"organization_secrets": "os",
+			"organization_self_hosted_runners": "osh",
+			"organization_user_blocking": "oub",
+			"packages": "pkg",
+			"pages": "pg",
+			"pull_requests": "pr",
+			"repository_hooks": "rh",
+			"repository_projects": "rp",
+			"repository_pre_receive_hooks": "rprh",
+			"secrets": "s",
+			"secret_scanning_alerts": "ssa",
+			"security_events": "se",
+			"single_file": "sf",
+			"statuses": "s",
+			"team_discussions": "td",
+			"vulnerability_alerts": "va",
+			"workflows": "w"
+		},
+		"created_at": ` + referenceTimeStr + `,
+		"updated_at": ` + referenceTimeStr + `,
+		"has_multiple_single_files": false,
+		"suspended_by": {
+			"login": "l",
+			"id": 1,
+			"avatar_url": "a",
+			"gravatar_id": "g",
+			"name": "n",
+			"company": "c",
+			"blog": "b",
+			"location": "l",
+			"email": "e",
+			"hireable": true,
+			"bio": "b",
+			"twitter_username": "t",
+			"public_repos": 1,
+			"followers": 1,
+			"following": 1,
+			"created_at": ` + referenceTimeStr + `,
+			"suspended_at": ` + referenceTimeStr + `,
+			"url": "u"
+		},
+		"suspended_at": ` + referenceTimeStr + `
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestInstallationTokenOptions_Marshal(t *testing.T) {
+	testJSONMarshal(t, &InstallationTokenOptions{}, "{}")
+
+	u := &InstallationTokenOptions{
+		RepositoryIDs: []int64{1},
+		Permissions: &InstallationPermissions{
+			Actions:                       String("a"),
+			Administration:                String("ad"),
+			Checks:                        String("c"),
+			Contents:                      String("co"),
+			ContentReferences:             String("cr"),
+			Deployments:                   String("d"),
+			Environments:                  String("e"),
+			Issues:                        String("i"),
+			Metadata:                      String("md"),
+			Members:                       String("m"),
+			OrganizationAdministration:    String("oa"),
+			OrganizationHooks:             String("oh"),
+			OrganizationPlan:              String("op"),
+			OrganizationPreReceiveHooks:   String("opr"),
+			OrganizationProjects:          String("op"),
+			OrganizationSecrets:           String("os"),
+			OrganizationSelfHostedRunners: String("osh"),
+			OrganizationUserBlocking:      String("oub"),
+			Packages:                      String("pkg"),
+			Pages:                         String("pg"),
+			PullRequests:                  String("pr"),
+			RepositoryHooks:               String("rh"),
+			RepositoryProjects:            String("rp"),
+			RepositoryPreReceiveHooks:     String("rprh"),
+			Secrets:                       String("s"),
+			SecretScanningAlerts:          String("ssa"),
+			SecurityEvents:                String("se"),
+			SingleFile:                    String("sf"),
+			Statuses:                      String("s"),
+			TeamDiscussions:               String("td"),
+			VulnerabilityAlerts:           String("va"),
+			Workflows:                     String("w"),
+		},
+	}
+
+	want := `{
+		"repository_ids": [1],
+		"permissions": {
+			"actions": "a",
+			"administration": "ad",
+			"checks": "c",
+			"contents": "co",
+			"content_references": "cr",
+			"deployments": "d",
+			"environments": "e",
+			"issues": "i",
+			"metadata": "md",
+			"members": "m",
+			"organization_administration": "oa",
+			"organization_hooks": "oh",
+			"organization_plan": "op",
+			"organization_pre_receive_hooks": "opr",
+			"organization_projects": "op",
+			"organization_secrets": "os",
+			"organization_self_hosted_runners": "osh",
+			"organization_user_blocking": "oub",
+			"packages": "pkg",
+			"pages": "pg",
+			"pull_requests": "pr",
+			"repository_hooks": "rh",
+			"repository_projects": "rp",
+			"repository_pre_receive_hooks": "rprh",
+			"secrets": "s",
+			"secret_scanning_alerts": "ssa",
+			"security_events": "se",
+			"single_file": "sf",
+			"statuses": "s",
+			"team_discussions": "td",
+			"vulnerability_alerts": "va",
+			"workflows": "w"
+		}
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestInstallationToken_Marshal(t *testing.T) {
+	testJSONMarshal(t, &InstallationToken{}, "{}")
+
+	u := &InstallationToken{
+		Token:     String("t"),
+		ExpiresAt: &referenceTime,
+		Permissions: &InstallationPermissions{
+			Actions:                       String("a"),
+			Administration:                String("ad"),
+			Checks:                        String("c"),
+			Contents:                      String("co"),
+			ContentReferences:             String("cr"),
+			Deployments:                   String("d"),
+			Environments:                  String("e"),
+			Issues:                        String("i"),
+			Metadata:                      String("md"),
+			Members:                       String("m"),
+			OrganizationAdministration:    String("oa"),
+			OrganizationHooks:             String("oh"),
+			OrganizationPlan:              String("op"),
+			OrganizationPreReceiveHooks:   String("opr"),
+			OrganizationProjects:          String("op"),
+			OrganizationSecrets:           String("os"),
+			OrganizationSelfHostedRunners: String("osh"),
+			OrganizationUserBlocking:      String("oub"),
+			Packages:                      String("pkg"),
+			Pages:                         String("pg"),
+			PullRequests:                  String("pr"),
+			RepositoryHooks:               String("rh"),
+			RepositoryProjects:            String("rp"),
+			RepositoryPreReceiveHooks:     String("rprh"),
+			Secrets:                       String("s"),
+			SecretScanningAlerts:          String("ssa"),
+			SecurityEvents:                String("se"),
+			SingleFile:                    String("sf"),
+			Statuses:                      String("s"),
+			TeamDiscussions:               String("td"),
+			VulnerabilityAlerts:           String("va"),
+			Workflows:                     String("w"),
+		},
+		Repositories: []*Repository{
+			{
+				ID:   Int64(1),
+				URL:  String("u"),
+				Name: String("n"),
+			},
+		},
+	}
+
+	want := `{
+		"token": "t",
+		"expires_at": ` + referenceTimeStr + `,
+		"permissions": {
+			"actions": "a",
+			"administration": "ad",
+			"checks": "c",
+			"contents": "co",
+			"content_references": "cr",
+			"deployments": "d",
+			"environments": "e",
+			"issues": "i",
+			"metadata": "md",
+			"members": "m",
+			"organization_administration": "oa",
+			"organization_hooks": "oh",
+			"organization_plan": "op",
+			"organization_pre_receive_hooks": "opr",
+			"organization_projects": "op",
+			"organization_secrets": "os",
+			"organization_self_hosted_runners": "osh",
+			"organization_user_blocking": "oub",
+			"packages": "pkg",
+			"pages": "pg",
+			"pull_requests": "pr",
+			"repository_hooks": "rh",
+			"repository_projects": "rp",
+			"repository_pre_receive_hooks": "rprh",
+			"secrets": "s",
+			"secret_scanning_alerts": "ssa",
+			"security_events": "se",
+			"single_file": "sf",
+			"statuses": "s",
+			"team_discussions": "td",
+			"vulnerability_alerts": "va",
+			"workflows": "w"
+		},
+		"repositories": [
+			{
+				"id": 1,
+				"url": "u",
+				"name": "n"
+			}
+		]
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestApp_Marshal(t *testing.T) {
+	testJSONMarshal(t, &App{}, "{}")
+
+	u := &App{
+		ID:     Int64(1),
+		Slug:   String("s"),
+		NodeID: String("nid"),
+		Owner: &User{
+			Login:           String("l"),
+			ID:              Int64(1),
+			URL:             String("u"),
+			AvatarURL:       String("a"),
+			GravatarID:      String("g"),
+			Name:            String("n"),
+			Company:         String("c"),
+			Blog:            String("b"),
+			Location:        String("l"),
+			Email:           String("e"),
+			Hireable:        Bool(true),
+			Bio:             String("b"),
+			TwitterUsername: String("t"),
+			PublicRepos:     Int(1),
+			Followers:       Int(1),
+			Following:       Int(1),
+			CreatedAt:       &Timestamp{referenceTime},
+			SuspendedAt:     &Timestamp{referenceTime},
+		},
+		Name:        String("n"),
+		Description: String("d"),
+		ExternalURL: String("eu"),
+		HTMLURL:     String("hu"),
+		CreatedAt:   &Timestamp{referenceTime},
+		UpdatedAt:   &Timestamp{referenceTime},
+		Permissions: &InstallationPermissions{
+			Actions:                       String("a"),
+			Administration:                String("ad"),
+			Checks:                        String("c"),
+			Contents:                      String("co"),
+			ContentReferences:             String("cr"),
+			Deployments:                   String("d"),
+			Environments:                  String("e"),
+			Issues:                        String("i"),
+			Metadata:                      String("md"),
+			Members:                       String("m"),
+			OrganizationAdministration:    String("oa"),
+			OrganizationHooks:             String("oh"),
+			OrganizationPlan:              String("op"),
+			OrganizationPreReceiveHooks:   String("opr"),
+			OrganizationProjects:          String("op"),
+			OrganizationSecrets:           String("os"),
+			OrganizationSelfHostedRunners: String("osh"),
+			OrganizationUserBlocking:      String("oub"),
+			Packages:                      String("pkg"),
+			Pages:                         String("pg"),
+			PullRequests:                  String("pr"),
+			RepositoryHooks:               String("rh"),
+			RepositoryProjects:            String("rp"),
+			RepositoryPreReceiveHooks:     String("rprh"),
+			Secrets:                       String("s"),
+			SecretScanningAlerts:          String("ssa"),
+			SecurityEvents:                String("se"),
+			SingleFile:                    String("sf"),
+			Statuses:                      String("s"),
+			TeamDiscussions:               String("td"),
+			VulnerabilityAlerts:           String("va"),
+			Workflows:                     String("w"),
+		},
+		Events: []string{"s"},
+	}
+
+	want := `{
+		"id": 1,
+		"slug": "s",
+		"node_id": "nid",
+		"owner": {
+			"login": "l",
+			"id": 1,
+			"avatar_url": "a",
+			"gravatar_id": "g",
+			"name": "n",
+			"company": "c",
+			"blog": "b",
+			"location": "l",
+			"email": "e",
+			"hireable": true,
+			"bio": "b",
+			"twitter_username": "t",
+			"public_repos": 1,
+			"followers": 1,
+			"following": 1,
+			"created_at": ` + referenceTimeStr + `,
+			"suspended_at": ` + referenceTimeStr + `,
+			"url": "u"
+		},
+		"name": "n",
+		"description": "d",
+		"external_url": "eu",
+		"html_url": "hu",
+		"created_at": ` + referenceTimeStr + `,
+		"updated_at": ` + referenceTimeStr + `,
+		"permissions": {
+			"actions": "a",
+			"administration": "ad",
+			"checks": "c",
+			"contents": "co",
+			"content_references": "cr",
+			"deployments": "d",
+			"environments": "e",
+			"issues": "i",
+			"metadata": "md",
+			"members": "m",
+			"organization_administration": "oa",
+			"organization_hooks": "oh",
+			"organization_plan": "op",
+			"organization_pre_receive_hooks": "opr",
+			"organization_projects": "op",
+			"organization_secrets": "os",
+			"organization_self_hosted_runners": "osh",
+			"organization_user_blocking": "oub",
+			"packages": "pkg",
+			"pages": "pg",
+			"pull_requests": "pr",
+			"repository_hooks": "rh",
+			"repository_projects": "rp",
+			"repository_pre_receive_hooks": "rprh",
+			"secrets": "s",
+			"secret_scanning_alerts": "ssa",
+			"security_events": "se",
+			"single_file": "sf",
+			"statuses": "s",
+			"team_discussions": "td",
+			"vulnerability_alerts": "va",
+			"workflows": "w"
+		},
+		"events": ["s"]
+	}`
+
+	testJSONMarshal(t, u, want)
+}
