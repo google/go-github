e87b9aa0c3e40c8bb64cf215558ff9903f90d408
Eric Norris <1906605+ericnorris@users.noreply.github.com>
2021-10-12 13:00:04 -0400

    Support specifying repository names (#2123)

--- github/apps.go
+++ github/apps.go
@@ -47,6 +47,10 @@ type InstallationTokenOptions struct {
 	// Providing repository IDs restricts the access of an installation token to specific repositories.
 	RepositoryIDs []int64 `json:"repository_ids,omitempty"`
 
+	// The names of the repositories that the installation token can access.
+	// Providing repository names restricts the access of an installation token to specific repositories.
+	Repositories []string `json:"repositories,omitempty"`
+
 	// The permissions granted to the access token.
 	// The permissions object includes the permission names and their access type.
 	Permissions *InstallationPermissions `json:"permissions,omitempty"`
