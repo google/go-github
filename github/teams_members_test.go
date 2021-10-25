70ea478a2369d19d11836b1ef96876486aad3117
jlaportebot <jlaportebot@users.noreply.github.com>
2021-10-25 08:32:44 +0530

    Add test cases for JSON resource marshaling (#2139)

--- github/teams_members_test.go
+++ github/teams_members_test.go
@@ -789,3 +789,17 @@ func TestTeamsService__ListPendingTeamInvitationsBySlug_invalidOrg(t *testing.T)
 	_, _, err := client.Teams.ListPendingTeamInvitationsBySlug(ctx, "%", "s", nil)
 	testURLParseError(t, err)
 }
+
+func TestTeamAddTeamMembershipOptions_Marshal(t *testing.T) {
+	testJSONMarshal(t, &TeamAddTeamMembershipOptions{}, "{}")
+
+	u := &TeamAddTeamMembershipOptions{
+		Role: "role",
+	}
+
+	want := `{
+		"role": "role"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
