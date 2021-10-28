6ea48d8b0686cb9b8844df5b5c9682d77f60af6f
akshay <akshayrandhave2424@gmail.com>
2021-10-28 13:34:44 +0530

    Add test cases for JSON resource marshaling

--- github/code-scanning_test.go
+++ github/code-scanning_test.go
@@ -480,3 +480,17 @@ func TestTool_Marshal(t *testing.T) {
 
 	testJSONMarshal(t, u, want)
 }
+
+func TestMessage_Marshal(t *testing.T) {
+	testJSONMarshal(t, &Tool{}, "{}")
+
+	u := &Message{
+		Text: String("text")
+	}
+
+	want := `{
+		"text": "text"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
