c6782f5e1f3fcfd313771825fbc1ab8cc9f796b6
akshay <akshayrandhave2424@gmail.com>
2021-10-29 11:34:20 +0530

    fix issue has mentioned

--- github/code-scanning_test.go
+++ github/code-scanning_test.go
@@ -482,7 +482,7 @@ func TestTool_Marshal(t *testing.T) {
 }
 
 func TestMessage_Marshal(t *testing.T) {
-	testJSONMarshal(t, &Tool{}, "{}")
+	testJSONMarshal(t, &Message{}, "{}")
 
 	u := &Message{
 		Text: String("text"),
