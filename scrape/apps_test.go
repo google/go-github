3080d6dc525c6c5956bc213bf1bd795339ee08d0
jlaportebot <jlaportebot@users.noreply.github.com>
2021-11-29 10:50:15 -0500

    Update scrape dependency to v41 (#2212)

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v40/github"
+	"github.com/google/go-github/v41/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
