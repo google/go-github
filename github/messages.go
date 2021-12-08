d024a3429dadd4cf76791d463541696274514e6b
jlaportebot <jlaportebot@users.noreply.github.com>
2021-12-08 22:52:43 +0530

    Fix comment references to webhook types (#2224)

--- github/messages.go
+++ github/messages.go
@@ -255,14 +255,14 @@ func ValidateSignature(signature string, payload, secretToken []byte) error {
 
 // WebHookType returns the event type of webhook request r.
 //
-// GitHub API docs: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos/hooks/#webhook-headers
+// GitHub API docs: https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types
 func WebHookType(r *http.Request) string {
 	return r.Header.Get(EventTypeHeader)
 }
 
 // DeliveryID returns the unique delivery ID of webhook request r.
 //
-// GitHub API docs: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos/hooks/#webhook-headers
+// GitHub API docs: https://docs.github.com/en/developers/webhooks-and-events/events/github-event-types
 func DeliveryID(r *http.Request) string {
 	return r.Header.Get(DeliveryIDHeader)
 }
