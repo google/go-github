d92496fb75dfb0de279106896c0db1f2171a40b1
Craig Gumbley <craiggumbley@gmail.com>
2022-01-17 15:45:58 +0000

    Address PR comments
    Made some minor change to this example that include removing os.Exit(),
    fixing some unnecessary variable declarations and godoc improvements.

--- example/listenvironments/main.go
+++ example/listenvironments/main.go
@@ -1,3 +1,14 @@
+// Copyright 2015 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// listenvironments is an example of how to use ListEnvironments method with EnvironmentListOptions.
+// It's runnable with the following command:
+// 		export GITHUB_TOKEN=your_token
+//		export GITHUB_REPOSITORY_OWNER=your_owner
+//		export GITHUB_REPOSITORY_NAME=your_repo
+//		go run .
 package main
 
 import (
@@ -10,13 +21,7 @@ import (
 	"golang.org/x/oauth2"
 )
 
-// An example of how to use ListEnvironments method with EnvironmentListOptions.
-// It's runnable with the following command:
-// export GITHUB_TOKEN=your_token export GITHUB_REPOSITORY_OWNER=your_owner export GITHUB_REPOSITORY_NAME=your_repo && go run .
 func main() {
-
-	var client *github.Client
-	var ctx = context.Background()
 
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
 	repo := os.Getenv("GITHUB_REPOSITORY_NAME")
@@ -27,8 +32,10 @@ func main() {
 	}
 
 	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+
+	ctx := context.Background()
 	tc := oauth2.NewClient(ctx, ts)
-	client = github.NewClient(tc)
+	client := github.NewClient(tc)
 
 	expectedPageSize := 2
 
@@ -36,12 +43,10 @@ func main() {
 	envResponse, _, err := client.Repositories.ListEnvironments(ctx, owner, repo, opts)
 	if err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 
 	if len(envResponse.Environments) != expectedPageSize {
 		log.Fatal("Unexpected number of environments returned")
-		os.Exit(1)
 	}
 
 	// The number of environments here should be equal to expectedPageSize
