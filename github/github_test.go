0661a478ebbf703eeac178dc5cf88e9732d300d4
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-02-17 21:43:44 -0500

    Improve code testing coverage

--- github/github_test.go
+++ github/github_test.go
@@ -2259,6 +2259,36 @@ func TestBareDo_GoodDebugRequestString(t *testing.T) {
 	}
 }
 
+func TestBareDo_GoodDebugRequestStringButBodyError(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	expectedBody := "Hello from the other side !"
+
+	mux.HandleFunc("/test-url", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, expectedBody)
+	})
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, DebugRequest, "curl")
+	req, err := client.NewRequest("GET", "test-url", nil)
+	if err != nil {
+		t.Fatalf("client.NewRequest returned error: %v", err)
+	}
+	want := "custom error"
+	req.Body = ioutil.NopCloser(iotest.ErrReader(errors.New(want)))
+
+	if _, err = client.BareDo(ctx, req); err == nil {
+		t.Fatal("client.BareDo expected error but got nil")
+	}
+
+	got := err.Error()
+	if !strings.Contains(got, want) {
+		t.Errorf("error = %q, want %q", got, want)
+	}
+}
+
 func TestBareDo_BadDebugRequestString(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
