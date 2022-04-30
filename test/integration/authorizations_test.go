03b5525554d33bcc1a735ab509224599107227a9
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-04-29 20:33:04 -0400

    Bump version of go-github library to v44 (#2351)

--- test/integration/authorizations_test.go
+++ test/integration/authorizations_test.go
@@ -17,7 +17,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/go-github/v43/github"
+	"github.com/google/go-github/v44/github"
 )
 
 const msgEnvMissing = "Skipping test because the required environment variable (%v) is not present."
