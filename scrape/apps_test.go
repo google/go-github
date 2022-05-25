f5f1b66fe29274465239ed07cc30c59346d69a53
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2022-05-25 10:56:49 -0400

    Update scrape dependency to v45
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v44/github"
+	"github.com/google/go-github/v45/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
