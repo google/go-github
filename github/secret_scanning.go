a41be50eb4a1d7814671231afa87b2280678d1ca
Mustafa Abban <mustafaabban@utexas.edu>
2022-09-01 15:42:19 -0500

    Update github/secret_scanning.go
    Co-authored-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- github/secret_scanning.go
+++ github/secret_scanning.go
@@ -62,9 +62,12 @@ type SecretScanningAlertListOptions struct {
 
 	ListCursorOptions
 
-	// List options can vary on the Enterprise type. On Enterprise Cloud, Secret Scan alerts show to support requesting by page number
-	// along with provided a cursor for an after param https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning#list-secret-scanning-alerts-for-an-organization
-	// Whereas on Enterprise Server, it is just by pagination by index https://docs.github.com/en/enterprise-server@3.6/rest/secret-scanning#list-secret-scanning-alerts-for-an-organization
+	// List options can vary on the Enterprise type.
+	// On Enterprise Cloud, Secret Scan alerts support requesting by page number
+	// along with providing a cursor for an "after" param.
+	// See: https://docs.github.com/en/enterprise-cloud@latest/rest/secret-scanning#list-secret-scanning-alerts-for-an-organization
+	// Whereas on Enterprise Server, pagination is by index.
+	// See: https://docs.github.com/en/enterprise-server@3.6/rest/secret-scanning#list-secret-scanning-alerts-for-an-organization
 	ListOptions
 }
 
