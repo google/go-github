5eaffa37bcecdc7ef6352d9cba69da25ca399de3
jlaportebot <jlaportebot@users.noreply.github.com>
2022-11-01 01:00:32 +0530

    Add test case for JSON resource marshaling (#2552)

--- github/repos_tags_test.go
+++ github/repos_tags_test.go
@@ -130,3 +130,19 @@ func TestRepositoriesService_DeleteTagProtection(t *testing.T) {
 		return client.Repositories.DeleteTagProtection(ctx, "o", "r", 1)
 	})
 }
+
+func TestTagProtection_Marshal(t *testing.T) {
+	testJSONMarshal(t, &TagProtection{}, "{}")
+
+	u := &TagProtection{
+		ID:      Int64(1),
+		Pattern: String("pattern"),
+	}
+
+	want := `{
+		"id": 1,
+		"pattern": "pattern"
+	}`
+
+	testJSONMarshal(t, u, want)
+}
