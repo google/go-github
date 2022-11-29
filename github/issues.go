5bd7c232c42560e2cc1853861fc88eadafc261f3
ganeshkumarsv <53483484+ganeshkumarsv@users.noreply.github.com>
2022-11-29 14:39:56 -0500

    Update github/issues.go
    Co-authored-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- github/issues.go
+++ github/issues.go
@@ -28,6 +28,7 @@ type Issue struct {
 	ID                *int64            `json:"id,omitempty"`
 	Number            *int              `json:"number,omitempty"`
 	State             *string           `json:"state,omitempty"`
+	// StateReason can be one of: "completed", "not_planned", "reopened".
 	StateReason       *string           `json:"state_reason,omitempty"`
 	Locked            *bool             `json:"locked,omitempty"`
 	Title             *string           `json:"title,omitempty"`
