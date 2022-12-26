13a1f4b782209fcca99319314407917a52b06e33
VenuManikanta <43684059+VenuManikanta@users.noreply.github.com>
2022-12-26 08:10:54 +0530

    Apply suggestions from code review
    Co-authored-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- github/enterprise_code_security_and_analysis.go
+++ github/enterprise_code_security_and_analysis.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The go-github AUTHORS. All rights reserved.
+// Copyright 2022 The go-github AUTHORS. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
@@ -12,10 +12,10 @@ import (
 
 // EnterpriseSecurityAnalysisSettings represents security analysis settings for an enterprise.
 type EnterpriseSecurityAnalysisSettings struct {
-	AdvancedSecurityEnabledForNewRepositories             bool   `json:"advanced_security_enabled_for_new_repositories,omitempty"`
-	SecretScanningEnabledForNewRepositories               bool   `json:"secret_scanning_enabled_for_new_repositories,omitempty"`
-	SecretScanningPushProtectionEnabledForNewRepositories bool   `json:"secret_scanning_push_protection_enabled_for_new_repositories,omitempty"`
-	SecretScanningPushProtectionCustomLink                string `json:"secret_scanning_push_protection_custom_link,omitempty"`
+	AdvancedSecurityEnabledForNewRepositories             *bool   `json:"advanced_security_enabled_for_new_repositories,omitempty"`
+	SecretScanningEnabledForNewRepositories               *bool   `json:"secret_scanning_enabled_for_new_repositories,omitempty"`
+	SecretScanningPushProtectionEnabledForNewRepositories *bool   `json:"secret_scanning_push_protection_enabled_for_new_repositories,omitempty"`
+	SecretScanningPushProtectionCustomLink                *string `json:"secret_scanning_push_protection_custom_link,omitempty"`
 }
 
 // GetCodeSecurityAndAnalysis gets code security and analysis features for an enterprise.
@@ -29,7 +29,7 @@ func (s *EnterpriseService) GetCodeSecurityAndAnalysis(ctx context.Context, ente
 		return nil, nil, err
 	}
 
-	enterpriseSecurityAnalysisSettings := new(EnterpriseSecurityAnalysisSettings)
+	settings := new(EnterpriseSecurityAnalysisSettings)
 	resp, err := s.client.Do(ctx, req, enterpriseSecurityAnalysisSettings)
 	if err != nil {
 		return nil, resp, err
@@ -41,7 +41,7 @@ func (s *EnterpriseService) GetCodeSecurityAndAnalysis(ctx context.Context, ente
 // UpdateCodeSecurityAndAnalysis updates code security and analysis features for new repositories in an enterprise.
 //
 // GitHub API docs: https://docs.github.com/en/rest/enterprise-admin/code-security-and-analysis?apiVersion=2022-11-28#update-code-security-and-analysis-features-for-an-enterprise
-func (s *EnterpriseService) UpdateCodeSecurityAndAnalysis(ctx context.Context, enterprise string, enterpriseSecurityAnalysisSettings *EnterpriseSecurityAnalysisSettings) (*Response, error) {
+func (s *EnterpriseService) UpdateCodeSecurityAndAnalysis(ctx context.Context, enterprise string, settings *EnterpriseSecurityAnalysisSettings) (*Response, error) {
 	u := fmt.Sprintf("enterprises/%v/code_security_and_analysis", enterprise)
 	req, err := s.client.NewRequest("PATCH", u, enterpriseSecurityAnalysisSettings)
 	if err != nil {
@@ -58,7 +58,7 @@ func (s *EnterpriseService) UpdateCodeSecurityAndAnalysis(ctx context.Context, e
 
 // EnableAdvancedSecurity enables advanced security for all repositories in an enterprise.
 //
-// GitHub API docs:https://docs.github.com/en/enterprise-cloud@latest/rest/enterprise-admin/code-security-and-analysis?apiVersion=2022-11-28#enable-or-disable-a-security-feature
+// GitHub API docs: https://docs.github.com/en/enterprise-cloud@latest/rest/enterprise-admin/code-security-and-analysis?apiVersion=2022-11-28#enable-or-disable-a-security-feature
 func (s *EnterpriseService) EnableAdvancedSecurity(ctx context.Context, enterprise string) (*Response, error) {
 	u := fmt.Sprintf("enterprises/%v/advanced_security/enable_all", enterprise)
 	req, err := s.client.NewRequest("POST", u, nil)
