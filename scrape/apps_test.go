979a2115416f8390f63c80f7936146e255e52623
jlaportebot <jlaportebot@users.noreply.github.com>
2023-01-26 19:17:19 -0500

    Update scrape dependency to v50 (#2648)

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v49/github"
+	"github.com/google/go-github/v50/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
