3f3659daa03962bd94be57fbba28aa307dbd28d0
r-aramizu <rafael.aramizu@mercadolivre.com>
2023-08-15 12:50:28 -0300

    adding omitempty HeadRepo field on struct

--- github/pulls.go
+++ github/pulls.go
@@ -244,7 +244,7 @@ func (s *PullRequestsService) GetRaw(ctx context.Context, owner string, repo str
 type NewPullRequest struct {
 	Title               *string `json:"title,omitempty"`
 	Head                *string `json:"head,omitempty"`
-	HeadRepo            *string `json:"head_repo"`
+	HeadRepo            *string `json:"head_repo,omitempty"`
 	Base                *string `json:"base,omitempty"`
 	Body                *string `json:"body,omitempty"`
 	Issue               *int    `json:"issue,omitempty"`
