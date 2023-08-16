b9027bd8e6f3891c6180a1b60cd692c168f7da2f
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-08-16 12:50:10 -0400

    Add TriggeringActor to WorkflowRun (#2879)
    Fixes: #2874 .

--- github/github_test.go
+++ github/github_test.go
@@ -154,19 +154,19 @@ func testJSONMarshal(t *testing.T, v interface{}, want string) {
 	if err := json.Unmarshal([]byte(want), &u); err != nil {
 		t.Errorf("Unable to unmarshal JSON for %v: %v", want, err)
 	}
-	w, err := json.Marshal(u)
+	w, err := json.MarshalIndent(u, "", "  ")
 	if err != nil {
 		t.Errorf("Unable to marshal JSON for %#v", u)
 	}
 
 	// Marshal the target value.
-	j, err := json.Marshal(v)
+	got, err := json.MarshalIndent(v, "", "  ")
 	if err != nil {
 		t.Errorf("Unable to marshal JSON for %#v", v)
 	}
 
-	if string(w) != string(j) {
-		t.Errorf("json.Marshal(%q) returned %s, want %s", v, j, w)
+	if diff := cmp.Diff(string(w), string(got)); diff != "" {
+		t.Errorf("json.Marshal returned:\n%s\nwant:\n%s\ndiff:\n%v", got, w, diff)
 	}
 }
 
