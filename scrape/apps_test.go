c9f5498d1e98cf10424e27e27e3af1de7c64aab8
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-08-16 14:36:49 -0400

    Bump go-github from v53 to v54 in /scape
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v53/github"
+	"github.com/google/go-github/v54/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
