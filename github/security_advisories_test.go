e53fa9295d44026b2715ed6b1dd87dae3e33f76f
Anish Rajan <anishrajan2708@gmail.com>
2023-08-26 17:22:30 +0530

    added API for Lists repository security advisories for an organization

--- github/security_advisories_test.go
+++ github/security_advisories_test.go
@@ -7,7 +7,9 @@ package github
 
 import (
 	"context"
+	"github.com/google/go-cmp/cmp"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -46,6 +48,130 @@ func TestSecurityAdvisoriesService_RequestCVE(t *testing.T) {
 		resp, err := client.SecurityAdvisories.RequestCVE(ctx, "o", "r", "ghsa_id")
 		if err == nil {
 			t.Errorf("testNewRequestAndDoFailure %v should have return err", methodName)
+		}
+		return resp, err
+	})
+}
+
+func TestSecurityAdvisoriesService_ListRepositorySecurityAdvisoriesForOrg_BadRequest(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/security-advisories", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		http.Error(w, "Bad Request", 400)
+	})
+
+	ctx := context.Background()
+	advisories, resp, err := client.SecurityAdvisories.ListRepositorySecurityAdvisoriesForOrg(ctx, "o", nil)
+	if err == nil {
+		t.Errorf("Expected HTTP 400 response")
+	}
+	if got, want := resp.Response.StatusCode, http.StatusBadRequest; got != want {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return status %d, want %d", got, want)
+	}
+	if advisories != nil {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return %+v, want nil", advisories)
+	}
+}
+
+func TestSecurityAdvisoriesService_ListRepositorySecurityAdvisoriesForOrg_NotFound(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/security-advisories", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		http.NotFound(w, r)
+	})
+
+	ctx := context.Background()
+	advisories, resp, err := client.SecurityAdvisories.ListRepositorySecurityAdvisoriesForOrg(ctx, "o", nil)
+	if err == nil {
+		t.Errorf("Expected HTTP 404 response")
+	}
+	if got, want := resp.Response.StatusCode, http.StatusNotFound; got != want {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return status %d, want %d", got, want)
+	}
+	if advisories != nil {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return %+v, want nil", advisories)
+	}
+}
+
+func TestSecurityAdvisoriesService_ListRepositorySecurityAdvisoriesForOrg_UnmarshalError(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/security-advisories", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`[{"ghsa_id": 12334354}]`))
+	})
+
+	ctx := context.Background()
+	advisories, resp, err := client.SecurityAdvisories.ListRepositorySecurityAdvisoriesForOrg(ctx, "o", nil)
+	if err == nil {
+		t.Errorf("Expected unmarshal error")
+	} else if !strings.Contains(err.Error(), "json: cannot unmarshal number into Go struct field RepoSecurityAdvisory.ghsa_id of type string") {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg returned unexpected error: %v", err)
+	}
+	if got, want := resp.Response.StatusCode, http.StatusOK; got != want {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return status %d, want %d", got, want)
+	}
+	if advisories != nil {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return %+v, want nil", advisories)
+	}
+}
+
+func TestSecurityAdvisoriesService_ListRepositorySecurityAdvisoriesForOrg(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/security-advisories", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`[
+			{
+				"ghsa_id": "GHSA-abcd-1234-efgh",
+    			"cve_id": "CVE-2050-00000"
+  			}
+		]`))
+	})
+
+	ctx := context.Background()
+	advisories, resp, err := client.SecurityAdvisories.ListRepositorySecurityAdvisoriesForOrg(ctx, "o", nil)
+	if err != nil {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg returned error: %v, want nil", err)
+	}
+	if got, want := resp.Response.StatusCode, http.StatusOK; got != want {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg return status %d, want %d", got, want)
+	}
+
+	want := []*RepoSecurityAdvisory{
+		{
+			GHSAID: String("GHSA-abcd-1234-efgh"),
+			CVEID:  String("CVE-2050-00000"),
+		},
+	}
+	if !cmp.Equal(advisories, want) {
+		t.Errorf("ListRepositorySecurityAdvisoriesForOrg returned %+v, want %+v", advisories, want)
+	}
+
+	methodName := "ListRepositorySecurityAdvisoriesForOrg"
+	testBadOptions(t, methodName, func() (err error) {
+		_, _, err = client.SecurityAdvisories.ListRepositorySecurityAdvisoriesForOrg(ctx, "\n", &ListRepositorySecurityAdvisoriesOptions{
+			Sort: "\n",
+		})
+		return err
+	})
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.SecurityAdvisories.ListRepositorySecurityAdvisoriesForOrg(ctx, "o", nil)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
 		}
 		return resp, err
 	})
