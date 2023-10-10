fb8f20f8fc6ef16f06d40a1b730a4abd3cc6caa5
Lachlan Cooper <lachlancooper@gmail.com>
2023-10-11 05:10:51 +1100

    Support options for GetCodeownersErrors (#2953)
    Fixes: #2952.

--- github/repos_codeowners.go
+++ github/repos_codeowners.go
@@ -10,6 +10,14 @@ import (
 	"fmt"
 )
 
+// GetCodeownersErrorsOptions specifies the optional parameters to the
+// RepositoriesService.GetCodeownersErrors method.
+type GetCodeownersErrorsOptions struct {
+	// A branch, tag or commit name used to determine which version of the CODEOWNERS file to use.
+	// Default: the repository's default branch (e.g. main).
+	Ref string `url:"ref,omitempty"`
+}
+
 // CodeownersErrors represents a list of syntax errors detected in the CODEOWNERS file.
 type CodeownersErrors struct {
 	Errors []*CodeownersError `json:"errors"`
@@ -29,8 +37,13 @@ type CodeownersError struct {
 // GetCodeownersErrors lists any syntax errors that are detected in the CODEOWNERS file.
 //
 // GitHub API docs: https://docs.github.com/en/rest/repos/repos#list-codeowners-errors
-func (s *RepositoriesService) GetCodeownersErrors(ctx context.Context, owner, repo string) (*CodeownersErrors, *Response, error) {
+func (s *RepositoriesService) GetCodeownersErrors(ctx context.Context, owner, repo string, opts *GetCodeownersErrorsOptions) (*CodeownersErrors, *Response, error) {
 	u := fmt.Sprintf("repos/%v/%v/codeowners/errors", owner, repo)
+	u, err := addOptions(u, opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	req, err := s.client.NewRequest("GET", u, nil)
 	if err != nil {
 		return nil, nil, err
