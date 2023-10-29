ecc77c200e0bf66bb9d9ebace873d11791fbdbae
Daniel Liao <10663736+liaodaniel@users.noreply.github.com>
2023-10-29 23:16:48 +1100

    Add CreateOrUpdateCustomProperty function

--- github/orgs_properties.go
+++ github/orgs_properties.go
@@ -5,6 +5,11 @@
 
 package github
 
+import (
+	"context"
+	"fmt"
+)
+
 // CustomProperty represents the organization custom property object.
 type CustomProperty struct {
 	PropertyName *string `json:"property_name,omitempty"`
@@ -14,4 +19,24 @@ type CustomProperty struct {
 	DefaultValue  *string  `json:"default_value,omitempty"`
 	Description   *string  `json:"description,omitempty"`
 	AllowedValues []string `json:"allowed_values,omitempty"`
+}
+
+// CreateOrUpdateCustomProperty creates a new or updates an existing custom property that is defined for the specified organization.
+//
+// GitHub API docs: https://docs.github.com/en/rest/orgs/properties#create-or-update-a-custom-property-for-an-organization
+func (s *OrganizationsService) CreateOrUpdateCustomProperty(ctx context.Context, org, name string, property *CustomProperty) (*CustomProperty, *Response, error) {
+	u := fmt.Sprintf("orgs/%v/properties/schema/%v", org, name)
+
+	req, err := s.client.NewRequest("PUT", u, property)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var customProperty *CustomProperty
+	resp, err := s.client.Do(ctx, req, &customProperty)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return customProperty, resp, nil
 }
---
+++ github/orgs_properties_test.go
@@ -0,0 +1,69 @@
+// Copyright 2023 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestOrganizationsService_CreateOrUpdateCustomProperty(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/orgs/o/properties/schema/name", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		fmt.Fprint(w, `{
+		"property_name": "name",
+		"value_type": "single_select",
+		"required": true,
+		"default_value": "production",
+		"description": "Prod or dev environment",
+		"allowed_values":[
+		  "production",
+		  "development"
+		]
+	  }`)
+	})
+
+	ctx := context.Background()
+	property, _, err := client.Organizations.CreateOrUpdateCustomProperty(ctx, "o", "name", &CustomProperty{
+		ValueType:     "single_select",
+		Required:      Bool(true),
+		DefaultValue:  String("production"),
+		Description:   String("Prod or dev environment"),
+		AllowedValues: []string{"production", "development"},
+	})
+	if err != nil {
+		t.Errorf("Organizations.CreateOrUpdateCustomProperty returned error: %v", err)
+	}
+
+	want := &CustomProperty{
+		PropertyName:  String("name"),
+		ValueType:     "single_select",
+		Required:      Bool(true),
+		DefaultValue:  String("production"),
+		Description:   String("Prod or dev environment"),
+		AllowedValues: []string{"production", "development"},
+	}
+	if !cmp.Equal(property, want) {
+		t.Errorf("Organizations.CreateOrUpdateCustomProperty returned %+v, want %+v", property, want)
+	}
+
+	const methodName = "CreateOrUpdateCustomProperty"
+
+	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
+		got, resp, err := client.Organizations.CreateOrUpdateCustomProperty(ctx, "o", "name", nil)
+		if got != nil {
+			t.Errorf("testNewRequestAndDoFailure %v = %#v, want nil", methodName, got)
+		}
+		return resp, err
+	})
+}
