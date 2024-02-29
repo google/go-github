a25989b0298916f638f33c35429ecda5d6c0850b
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-02-29 12:31:09 -0500

    Bump go-github from v59 to v60 in /scrape (#3087)

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v59/github"
+	"github.com/google/go-github/v60/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
