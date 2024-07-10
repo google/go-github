d7769ed10c48fd71e304ad5dbcd55b29b3d5ab53
Romain Arnaud <roming22@gmail.com>
2024-07-10 15:23:30 -0400

    fix: Use 'CallbackURLs'

--- scrape/apps.go
+++ scrape/apps.go
@@ -117,7 +117,7 @@ type AppManifest struct {
 	//Required. The homepage of your GitHub App.
 	URL *string `json:"url,omitempty"`
 	// The full URL of the endpoint to authenticate users via the GitHub App.
-	CallbackURL *string `json:"callback_url,omitempty"`
+	CallbackURLs []string `json:"callback_urls,omitempty"`
 	// Required. The configuration of the GitHub App's webhook.
 	HookAttributes map[string]string `json:"hook_attributes,omitempty"`
 	// The full URL to redirect to after the person installs the GitHub App.
