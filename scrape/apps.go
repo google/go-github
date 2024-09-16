80117c77e4d14ada58d4d5103bdb202c25c3d1f1
jlaportebot <jlaportebot@users.noreply.github.com>
2024-09-16 09:37:20 -0400

    Bump go-github from v64 to v65 in /scrape (#3267)

--- scrape/apps.go
+++ scrape/apps.go
@@ -18,7 +18,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/google/go-github/v64/github"
+	"github.com/google/go-github/v65/github"
 )
 
 // AppRestrictionsEnabled returns whether the specified organization has
