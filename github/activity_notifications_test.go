e0c8cdf6bd95f7754e539233d4d8223d57a5cab3
Daniel Cole <takeitfromthedan@gmail.com>
2024-09-16 19:07:26 -0400

    Fix tests for MarkThreadDone to use int

--- github/activity_notifications_test.go
+++ github/activity_notifications_test.go
@@ -218,19 +218,19 @@ func TestActivityService_MarkThreadDone(t *testing.T) {
 	})
 
 	ctx := context.Background()
-	_, err := client.Activity.MarkThreadDone(ctx, "1")
+	_, err := client.Activity.MarkThreadDone(ctx, 1)
 	if err != nil {
 		t.Errorf("Activity.MarkThreadDone returned error: %v", err)
 	}
 
 	const methodName = "MarkThreadDone"
 	testBadOptions(t, methodName, func() (err error) {
-		_, err = client.Activity.MarkThreadDone(ctx, "\n")
+		_, err = client.Activity.MarkThreadDone(ctx, 0)
 		return err
 	})
 
 	testNewRequestAndDoFailure(t, methodName, client, func() (*Response, error) {
-		return client.Activity.MarkThreadDone(ctx, "1")
+		return client.Activity.MarkThreadDone(ctx, 1)
 	})
 }
 
