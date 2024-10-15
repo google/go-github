65358d51d998a5372f7a18b78387b4df921f9b1a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-10-15 16:14:02 -0400

    Update github/event_types_test.go

--- github/event_types_test.go
+++ github/event_types_test.go
@@ -10064,7 +10064,7 @@ func TestRepositoryRulesetEvent_Marshal(t *testing.T) {
 								MinEntriesToMergeWaitMinutes: 13,
 							},
 						},
-						RequireDeployments: &RepositoryRulesetRequireDeploymentsRule{
+						RequiredDeployments: &RepositoryRulesetRequiredDeploymentsRule{
 							Type: "required_deployments",
 							Parameters: &RequiredDeploymentEnvironmentsRuleParameters{
 								RequiredDeploymentEnvironments: []string{"test"},
