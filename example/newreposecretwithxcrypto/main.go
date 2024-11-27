92383951a7395b26519cb20263b70ebe67a3672a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2024-11-27 09:02:17 -0500

    Bump version of go-github to v67.0.0 (#3360)

--- example/newreposecretwithxcrypto/main.go
+++ example/newreposecretwithxcrypto/main.go
@@ -37,7 +37,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/google/go-github/v66/github"
+	"github.com/google/go-github/v67/github"
 	"golang.org/x/crypto/nacl/box"
 )
 
--- test/fields/fields.go
+++ test/fields/fields.go
@@ -25,7 +25,7 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/google/go-github/v66/github"
+	"github.com/google/go-github/v67/github"
 )
 
 var (
