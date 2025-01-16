dfc158ad08d532f434995e982f2c1844f688234b
Banhidy Krisztian <krisztian@peak.com>
2025-01-16 21:24:51 +0300

    Add  #3436 patch and generate

--- github/github-accessors_test.go
+++ github/github-accessors_test.go
@@ -31884,17 +31884,6 @@ func TestSMTP_GetSupportAddressType(tt *testing.T) {
 	s.GetSupportAddressType()
 }
 
-func TestSMTP_GetUserName(tt *testing.T) {
-	tt.Parallel()
-	var zeroValue string
-	s := &SMTP{UserName: &zeroValue}
-	s.GetUserName()
-	s = &SMTP{}
-	s.GetUserName()
-	s = nil
-	s.GetUserName()
-}
-
 func TestSMTP_GetUsername(tt *testing.T) {
 	tt.Parallel()
 	var zeroValue string
@@ -31904,6 +31893,17 @@ func TestSMTP_GetUsername(tt *testing.T) {
 	s.GetUsername()
 	s = nil
 	s.GetUsername()
+}
+
+func TestSMTP_GetUserName(tt *testing.T) {
+	tt.Parallel()
+	var zeroValue string
+	s := &SMTP{UserName: &zeroValue}
+	s.GetUserName()
+	s = &SMTP{}
+	s.GetUserName()
+	s = nil
+	s.GetUserName()
 }
 
 func TestSNMP_GetCommunity(tt *testing.T) {
