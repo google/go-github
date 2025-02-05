06fa2f47e867b523c4290b1f6d640d5974ace79d
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-02-05 09:41:54 -0500

    Bump go-github from v68 to v69 in /scrape
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scrape/apps.go
+++ scrape/apps.go
@@ -18,7 +18,7 @@ import (
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
-	"github.com/google/go-github/v68/github"
+	"github.com/google/go-github/v69/github"
 )
 
 // AppRestrictionsEnabled returns whether the specified organization has
