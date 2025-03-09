1d4d3ccdf1609610ad0fd20f75e02223a4dda668
Gal Ofri <galmenashofri@gmail.com>
2025-03-09 18:00:30 +0700

    docs: update readme and examples for updated go-github-ratelimit and introduce go-github-pagination

--- example/ratelimit/main.go
+++ example/ratelimit/main.go
@@ -3,16 +3,20 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// The ratelimit command demonstrates using the github_ratelimit.SecondaryRateLimitWaiter.
+// The ratelimit command demonstrates using the github_ratelimit as well as github_pagination.
 // By using the waiter, the client automatically sleeps and retry requests
 // when it hits secondary rate limits.
+// It also prevents the client from abusing the API in case of a primary rate limit.
 package main
 
 import (
 	"context"
 	"fmt"
 
-	"github.com/gofri/go-github-ratelimit/github_ratelimit"
+	"github.com/gofri/go-github-pagination/githubpagination"
+	"github.com/gofri/go-github-ratelimit/v2/github_ratelimit"
+	"github.com/gofri/go-github-ratelimit/v2/github_ratelimit/github_primary_ratelimit"
+	"github.com/gofri/go-github-ratelimit/v2/github_ratelimit/github_secondary_ratelimit"
 	"github.com/google/go-github/v69/github"
 )
 
@@ -21,22 +25,28 @@ func main() {
 	fmt.Print("Enter GitHub username: ")
 	fmt.Scanf("%s", &username)
 
-	rateLimiter, err := github_ratelimit.NewRateLimitWaiterClient(nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	}
+	rateLimiter := github_ratelimit.New(nil,
+		github_primary_ratelimit.WithLimitDetectedCallback(func(ctx *github_primary_ratelimit.CallbackContext) {
+			fmt.Printf("Primary rate limit detected: category %s, reset time: %v\n", ctx.Category, ctx.ResetTime)
+		}),
+		github_secondary_ratelimit.WithLimitDetectedCallback(func(ctx *github_secondary_ratelimit.CallbackContext) {
+			fmt.Printf("Secondary rate limit detected: reset time: %v, total sleep time: %v\n", ctx.ResetTime, ctx.TotalSleepTime)
+		}),
+	)
 
-	client := github.NewClient(rateLimiter)
+	paginator := githubpagination.NewClient(rateLimiter,
+		githubpagination.WithPerPage(100), // default to 100 results per page
+	)
+	client := github.NewClient(paginator)
 
 	// arbitrary usage of the client
-	organizations, _, err := client.Organizations.List(context.Background(), username, nil)
+	repos, _, err := client.Repositories.ListByUser(context.Background(), username, nil)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
-	for i, organization := range organizations {
-		fmt.Printf("%v. %v\n", i+1, organization.GetLogin())
+	for i, repo := range repos {
+		fmt.Printf("%v. %v\n", i+1, repo.GetName())
 	}
 }
