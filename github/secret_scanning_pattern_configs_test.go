65a101781ccc2f2d251eedc1bb96a28aae28288d
dmclink <135415819+dmclink@users.noreply.github.com>
2025-08-10 17:48:25 +0800

    feat: Create files for new secret scanning pattern configs API

---
+++ github/secret_scanning_pattern_configs_test.go
@@ -0,0 +1,6 @@
+// Copyright 2025 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
