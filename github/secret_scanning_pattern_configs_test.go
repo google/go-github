24e740bac0e6aefc0d9c4297efa4f8141e43e6e5
dmclink <135415819+dmclink@users.noreply.github.com>
2025-08-11 10:11:42 +0800

    feat: Add secret scanning pattern config test function stubs

--- github/secret_scanning_pattern_configs_test.go
+++ github/secret_scanning_pattern_configs_test.go
@@ -4,3 +4,60 @@
 // license that can be found in the LICENSE file.
 
 package github
+
+import "testing"
+
+//TODO: implement new SecretScanningPatternConfigs method tests
+func TestSecretScanningService_ListPatternConfigsForEnterprise(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+	//TODO:
+}
+
+func TestSecretScanningService_ListPatternConfigsForOrg(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+	//TODO:
+}
+
+func TestSecretScanningService_UpdatePatternConfigsForEnterprise(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+	//TODO:
+}
+
+func TestSecretScanningService_UpdatePatternConfigsForOrg(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+	//TODO:
+}
+
+func TestSecretScanningPatternConfigs_Marshal(t *testing.T) {
+	t.Parallel()
+	testJSONMarshal(t, &SecretScanningPatternConfigs{}, `{}`)
+	//TODO:
+}
+
+func TestSecretScanningPatternOverride_Marshal(t *testing.T) {
+	t.Parallel()
+	testJSONMarshal(t, &SecretScanningPatternOverride{}, `{}`)
+	//TODO:
+}
+
+func TestSecretScanningPatternConfigsUpdate_Marshal(t *testing.T) {
+	t.Parallel()
+	testJSONMarshal(t, &SecretScanningPatternConfigsUpdate{}, `{}`)
+	//TODO:
+}
+
+func TestSecretScanningPatternConfigsUpdateOptions_Marshal(t *testing.T) {
+	t.Parallel()
+	testJSONMarshal(t, &SecretScanningPatternConfigsUpdateOptions{}, `{}`)
+	//TODO:
+}
+
+func TestSecretScanningProviderPatternSetting_Marshal(t *testing.T) {
+	t.Parallel()
+	testJSONMarshal(t, &SecretScanningProviderPatternSetting{}, `{}`)
+	//TODO:
+}
