afc50525e7bbee943364cd236aa857075847d205
dmclink <135415819+dmclink@users.noreply.github.com>
2025-08-12 23:22:35 +0800

    fix: Missing comments for pattern setting structs

--- github/secret_scanning_pattern_configs.go
+++ github/secret_scanning_pattern_configs.go
@@ -53,6 +53,7 @@ type SecretScanningPatternConfigsUpdateOptions struct {
 	CustomPatternSettings []*SecretScanningCustomPatternSetting `json:"custom_pattern_settings,omitempty"`
 }
 
+// SecretScanningProviderPatternSetting defines an optional pattern setting for provider patterns.
 type SecretScanningProviderPatternSetting struct {
 	// The ID of the pattern to configure.
 	TokenType string `json:"token_type"`
@@ -62,6 +63,7 @@ type SecretScanningProviderPatternSetting struct {
 	PushProtectionSetting string `json:"push_protection_setting"`
 }
 
+// SecretScanningCustomPatternSetting defines an optional pattern setting for custom patterns
 type SecretScanningCustomPatternSetting struct {
 	// The ID of the pattern to configure.
 	TokenType string `json:"token_type"`
