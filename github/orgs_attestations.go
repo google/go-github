225a5c8ac81d01792f48a2e26bd8cb7e0f19f628
jlaportebot <jlaportebot@users.noreply.github.com>
2025-09-25 10:15:18 -0400

    chore: Update openapi_operations.yaml (#3735)

--- github/orgs_attestations.go
+++ github/orgs_attestations.go
@@ -14,7 +14,7 @@ import (
 // with a given subject digest that are associated with repositories
 // owned by an organization.
 //
-// GitHub API docs: https://docs.github.com/rest/orgs/orgs#list-attestations
+// GitHub API docs: https://docs.github.com/rest/orgs/attestations#list-attestations
 //
 //meta:operation GET /orgs/{org}/attestations/{subject_digest}
 func (s *OrganizationsService) ListAttestations(ctx context.Context, org, subjectDigest string, opts *ListOptions) (*AttestationsResponse, *Response, error) {
