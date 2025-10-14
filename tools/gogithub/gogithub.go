1cec393c16ad263b6575c091be02099a5056d3db
zyfy29 <wasuremono127@gmail.com>
2025-10-14 21:42:51 +0900

    copyright

--- tools/gogithub/gogithub.go
+++ tools/gogithub/gogithub.go
@@ -1,3 +1,8 @@
+// Copyright 2025 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /*
 Package gogithub is a custom linter to be used by golangci-lint.
 
