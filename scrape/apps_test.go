79e24b578e73b838534d3ea4edf6277bb33aeb43
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-11-03 21:40:32 -0500

    Bump go-github from v76 to v77 in /scrape
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v76/github"
+	"github.com/google/go-github/v77/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
