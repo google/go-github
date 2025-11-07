65b5cfe09f84606693c13a2808571367b72f8931
Alejandro <elminster.aom@gmail.com>
2025-11-08 00:29:18 +0100

    Initial Enterprise SCIM  schema

---
+++ github/enterprise_scim_test.go
@@ -0,0 +1,14 @@
+// Copyright 2025 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
