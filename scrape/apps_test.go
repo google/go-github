b9e21e3233b46fe82d34329cff9f3b07fe952dfc
jlaportebot <jlaportebot@users.noreply.github.com>
2025-12-04 10:56:25 -0500

    Bump go-github from v79 to v80 in /scrape (#3855)

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v79/github"
+	"github.com/google/go-github/v80/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
