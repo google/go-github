c73b01c4e04034016d347f89ba2a25b422061c3c
Mohamad Al-Zawahreh <merchantmoh@gmail.com>
2026-01-23 19:29:27 -0500

    feat: Add native Go 1.23 Iterator support

---
+++ github/example_iterators_test.go
@@ -0,0 +1,30 @@
+// Copyright 2026 The go-github AUTHORS. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package github_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/v81/github"
+)
+
+func ExampleRepositoriesService_ListByUserIter() {
+	client := github.NewClient(nil)
+	ctx := context.Background()
+
+	// List all repositories for a user using the iterator.
+	// This automatically handles pagination.
+	// Note that if `opts` is `nil`, a new empty `opts` will be created and used within the iterator.
+	opts := &github.RepositoryListByUserOptions{Type: "public"}
+	for repo, err := range client.Repositories.ListByUserIter(ctx, "octocat", opts) {
+		if err != nil {
+			log.Fatalf("Error listing repos: %v", err)
+		}
+		fmt.Println(repo.GetName())
+	}
+}
