02bb4d080eb33f3afcc194af687f4fec44519e4e
Mohamad Al-Zawahreh <merchantmoh@gmail.com>
2026-01-29 15:25:54 -0500

    Update example/otel/main.go
    Co-authored-by: Oleksandr Redko <oleksandr.red+github@gmail.com>

--- example/otel/main.go
+++ example/otel/main.go
@@ -1,4 +1,5 @@
-// Copyright 2026 The go-github Authors. All rights reserved.
+// Copyright 2026 The go-github AUTHORS. All rights reserved.
+//
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
