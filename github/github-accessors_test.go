3861b4a29d4c8f9aefb197a22b7de9fc63cd15c4
Oleksandr Redko <oleksandr.red+github@gmail.com>
2026-02-09 12:53:36 +0200

    fix!: Add `ListLicensesOptions` to `LicensesService.List`

--- github/github-accessors_test.go
+++ github/github-accessors_test.go
@@ -19411,6 +19411,17 @@ func TestListGlobalSecurityAdvisoriesOptions_GetUpdated(tt *testing.T) {
 	l.GetUpdated()
 }
 
+func TestListLicensesOptions_GetFeatured(tt *testing.T) {
+	tt.Parallel()
+	var zeroValue bool
+	l := &ListLicensesOptions{Featured: &zeroValue}
+	l.GetFeatured()
+	l = &ListLicensesOptions{}
+	l.GetFeatured()
+	l = nil
+	l.GetFeatured()
+}
+
 func TestListOrganizations_GetTotalCount(tt *testing.T) {
 	tt.Parallel()
 	var zeroValue int
