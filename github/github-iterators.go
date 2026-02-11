e0c53bf40ff6f7cf0220ac220783e34268de1520
Shivam Kumar <155747305+maishivamhoo123@users.noreply.github.com>
2026-02-12 00:30:59 +0530

    fix!: Fix pagination support for `IssuesService` list methods (#3984)
    BREAKING CHANGE: `ListCursorOptions` is removed from `IssueListOptions`.

--- github/github-iterators.go
+++ github/github-iterators.go
@@ -1312,10 +1312,9 @@ func (s *IssuesService) ListIter(ctx context.Context, all bool, opts *IssueListO
 				}
 			}
 
-			if resp.Cursor == "" && resp.NextPage == 0 {
+			if resp.NextPage == 0 {
 				break
 			}
-			opts.ListCursorOptions.Cursor = resp.Cursor
 			opts.ListOptions.Page = resp.NextPage
 		}
 	}
@@ -1375,10 +1374,9 @@ func (s *IssuesService) ListByOrgIter(ctx context.Context, org string, opts *Iss
 				}
 			}
 
-			if resp.Cursor == "" && resp.NextPage == 0 {
+			if resp.NextPage == 0 {
 				break
 			}
-			opts.ListCursorOptions.Cursor = resp.Cursor
 			opts.ListOptions.Page = resp.NextPage
 		}
 	}
@@ -3767,10 +3765,9 @@ func (s *SubIssueService) ListByIssueIter(ctx context.Context, owner string, rep
 				}
 			}
 
-			if resp.Cursor == "" && resp.NextPage == 0 {
+			if resp.NextPage == 0 {
 				break
 			}
-			opts.ListCursorOptions.Cursor = resp.Cursor
 			opts.ListOptions.Page = resp.NextPage
 		}
 	}
