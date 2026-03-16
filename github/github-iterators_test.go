ca1c68fe3ba4ebd69e80108733995d6de3c78743
Austen Stone <austenstone@github.com>
2026-03-16 18:20:43 -0400

    Add ListRunnerGroupHostedRunners for org runner groups
    Implement GET /orgs/{org}/actions/runner-groups/{runner_group_id}/hosted-runners
    endpoint to list GitHub-hosted runners in an organization runner group.
    
    This was the only missing endpoint from the self-hosted runner groups API.

--- github/github-iterators_test.go
+++ github/github-iterators_test.go
@@ -1383,6 +1383,78 @@ func TestActionsService_ListRepositoryWorkflowRunsIter(t *testing.T) {
 	}
 }
 
+func TestActionsService_ListRunnerGroupHostedRunnersIter(t *testing.T) {
+	t.Parallel()
+	client, mux, _ := setup(t)
+	var callNum int
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		callNum++
+		switch callNum {
+		case 1:
+			w.Header().Set("Link", `<https://api.github.com/?page=1>; rel="next"`)
+			fmt.Fprint(w, `{"runners": [{},{},{}]}`)
+		case 2:
+			fmt.Fprint(w, `{"runners": [{},{},{},{}]}`)
+		case 3:
+			fmt.Fprint(w, `{"runners": [{},{}]}`)
+		case 4:
+			w.WriteHeader(http.StatusNotFound)
+		case 5:
+			fmt.Fprint(w, `{"runners": [{},{}]}`)
+		}
+	})
+
+	iter := client.Actions.ListRunnerGroupHostedRunnersIter(t.Context(), "", 0, nil)
+	var gotItems int
+	for _, err := range iter {
+		gotItems++
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+	}
+	if want := 7; gotItems != want {
+		t.Errorf("client.Actions.ListRunnerGroupHostedRunnersIter call 1 got %v items; want %v", gotItems, want)
+	}
+
+	opts := &ListOptions{}
+	iter = client.Actions.ListRunnerGroupHostedRunnersIter(t.Context(), "", 0, opts)
+	gotItems = 0
+	for _, err := range iter {
+		gotItems++
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+	}
+	if want := 2; gotItems != want {
+		t.Errorf("client.Actions.ListRunnerGroupHostedRunnersIter call 2 got %v items; want %v", gotItems, want)
+	}
+
+	iter = client.Actions.ListRunnerGroupHostedRunnersIter(t.Context(), "", 0, nil)
+	gotItems = 0
+	for _, err := range iter {
+		gotItems++
+		if err == nil {
+			t.Error("expected error; got nil")
+		}
+	}
+	if gotItems != 1 {
+		t.Errorf("client.Actions.ListRunnerGroupHostedRunnersIter call 3 got %v items; want 1 (an error)", gotItems)
+	}
+
+	iter = client.Actions.ListRunnerGroupHostedRunnersIter(t.Context(), "", 0, nil)
+	gotItems = 0
+	iter(func(item *HostedRunner, err error) bool {
+		gotItems++
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+		return false
+	})
+	if gotItems != 1 {
+		t.Errorf("client.Actions.ListRunnerGroupHostedRunnersIter call 4 got %v items; want 1 (an error)", gotItems)
+	}
+}
+
 func TestActionsService_ListRunnerGroupRunnersIter(t *testing.T) {
 	t.Parallel()
 	client, mux, _ := setup(t)
