43b1593f4c69c6d6a89b171c33a49100f67c0663
Danyal Ahmed <58849388+danyalahmed1995@users.noreply.github.com>
2026-05-16 05:19:15 +0500

    github: add enterprise app installation lookup

--- github/apps.go
+++ github/apps.go
@@ -457,6 +457,15 @@ func (s *AppsService) FindOrganizationInstallation(ctx context.Context, org stri
 	return s.getInstallation(ctx, fmt.Sprintf("orgs/%v/installation", org))
 }
 
+// FindEnterpriseInstallation finds the enterprise's installation information.
+//
+// GitHub API docs: https://docs.github.com/enterprise-cloud@latest/rest/apps/apps#get-an-enterprise-installation-for-the-authenticated-app
+//
+//meta:operation GET /enterprises/{enterprise}/installation
+func (s *AppsService) FindEnterpriseInstallation(ctx context.Context, enterprise string) (*Installation, *Response, error) {
+	return s.getInstallation(ctx, fmt.Sprintf("enterprises/%v/installation", enterprise))
+}
+
 // FindRepositoryInstallation finds the repository's installation information.
 //
 // GitHub API docs: https://docs.github.com/rest/apps/apps?apiVersion=2022-11-28#get-a-repository-installation-for-the-authenticated-app
