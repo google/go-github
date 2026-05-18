d9fc4c0f017a9473b61e49e9aa2040fd48b13122
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-05-17 18:49:11 -0700

    chore: Bump go-github from v86 to v87 in /scrape (#4234)

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v86/github"
+	"github.com/google/go-github/v87/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
