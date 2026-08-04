b568aa0a7201ab39ee55f3c8e3736a1b16448a85
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-08-04 14:17:28 -0400

    chore: Bump go-github from v89 to v90 in /scrape
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- scrape/apps_test.go
+++ scrape/apps_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-github/v89/github"
+	"github.com/google/go-github/v90/github"
 )
 
 func Test_AppRestrictionsEnabled(t *testing.T) {
