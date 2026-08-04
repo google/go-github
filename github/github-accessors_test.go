e0645a6dabc939f125e913266d0f7a30f7fd1948
Jam Balaya <jambalaya.pyoncafe@outlook.jp>
2026-08-04 20:28:57 +0900

    refactor!: Pass `UpdateConnectedExternalGroup` request body by value via new `UpdateConnectedExternalGroupRequest` (#4425)
    BREAKING CHANGE: `TeamsService.UpdateConnectedExternalGroup` now takes `UpdateConnectedExternalGroupRequest` (with non-pointer `GroupID`) by value.

--- github/github-accessors_test.go
+++ github/github-accessors_test.go
@@ -53905,6 +53905,14 @@ func TestUpdateCodespaceOptions_GetRecentFolders(tt *testing.T) {
 	u.GetRecentFolders()
 }
 
+func TestUpdateConnectedExternalGroupRequest_GetGroupID(tt *testing.T) {
+	tt.Parallel()
+	u := &UpdateConnectedExternalGroupRequest{}
+	u.GetGroupID()
+	u = nil
+	u.GetGroupID()
+}
+
 func TestUpdateCustomOrgRoleRequest_GetBaseRole(tt *testing.T) {
 	tt.Parallel()
 	var zeroValue string
